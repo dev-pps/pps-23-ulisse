285e294aab239c60bc44a5f9eac01179a23ac0b5
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-07 01:35:26 +0100

    feat: add station edit

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -1,3 +1,5 @@
+import cats.implicits.catsSyntaxEq
+
 final case class Model():
 
   @SuppressWarnings(Array("org.wartremover.warts.Var"))
@@ -5,6 +7,14 @@ final case class Model():
 
   def stationMap: Seq[Station] = _stationMap.stations
   def addStation(station: Station): Unit =
-//    _stationMap = _stationMap.copy(stations = station +: _stationMap.stations)
     _stationMap = StationMap(station +: _stationMap.stations)
     println(_stationMap)
+
+  def removeStation(station: Station): Unit =
+    val updatedStations =
+      _stationMap.stations.filterNot(_.location === station.location)
+    _stationMap = StationMap(updatedStations)
+
+  def updateStation(station: Station): Unit =
+    removeStation(station)
+    addStation(station)
