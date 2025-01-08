1ed6ba7e7f2b55164e78d68c445ac21faa8f02b2
Matteo <matteo.violani@studio.unibo.it>
2025-01-08 09:35:50 +0100

    feat: high speed train entity creation by TDD approach

--- src/main/scala/train/Trains.scala
+++ src/main/scala/train/Trains.scala
@@ -4,26 +4,43 @@ import train.Trains.TrainType.{HIGH_SPEED, NORMAL}
 
 object Trains:
   enum TrainType(val maxSpeed: Int):
-    case NORMAL extends TrainType(130)
+    case NORMAL     extends TrainType(130)
     case HIGH_SPEED extends TrainType(300)
 
-  abstract class Train:
+  trait Train:
+    def trainType: TrainType
+    def maxSpeed: Int
+    val name: String
+    val carriages: Int
+    val capacity: Int
+
+  private abstract class AbstractTrain extends Train:
     def trainType: TrainType
     def maxSpeed: Int = trainType.maxSpeed
     val name: String
     val carriages: Int
     val capacity: Int
 
-  trait NormalSpeed extends Train:
+  private trait NormalSpeed extends AbstractTrain:
     override def trainType: TrainType = NORMAL
 
-  trait HighSpeed extends Train:
+  private trait HighSpeed extends AbstractTrain:
     override def trainType: TrainType = HIGH_SPEED
 
-  class RegionalTrain(override val name: String,
-                      override val carriages: Int,
-                      override val capacity: Int) extends Train with NormalSpeed
+  private class RegionalTrain(
+      override val name: String,
+      override val carriages: Int,
+      override val capacity: Int
+  ) extends AbstractTrain with NormalSpeed
 
-  class HighSpeedTrain(override val name: String,
-                      override val carriages: Int,
-                      override val capacity: Int) extends Train with HighSpeed
+  private class HighSpeedTrain(
+      override val name: String,
+      override val carriages: Int,
+      override val capacity: Int
+  ) extends AbstractTrain with HighSpeed
+
+  def makeRegionalTrain(name: String, carriages: Int, capacity: Int): Train =
+    RegionalTrain(name, carriages, capacity)
+
+  def makeHighSpeedTrain(name: String, carriages: Int, capacity: Int): Train =
+    HighSpeedTrain(name, carriages, capacity)
