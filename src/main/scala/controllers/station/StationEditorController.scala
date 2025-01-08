5d9b81ff839a0ae968cdcab579471cdd88de1352
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-08 17:29:41 +0100

    docs: add scala doc

--- src/main/scala/controllers/station/StationEditorController.scala
+++ src/main/scala/controllers/station/StationEditorController.scala
@@ -6,7 +6,17 @@ import model.station.{Location, Station}
 import views.*
 import views.station.StationEditorView
 
+/** Controller for StationEditorView.
+  *
+  * @constructor
+  *   create a new StationEditorController with a view and model.
+  * @param view
+  *   the related StationEditorView
+  * @param model
+  *   the application model
+  */
 final case class StationEditorController(view: StationEditorView, model: Model):
+
   private def createStation(
       name: String,
       latitude: String,
@@ -19,6 +29,27 @@ final case class StationEditorController(view: StationEditorView, model: Model):
       numberOfTrack.toInt
     )
 
+  /** Handles the click event when the "OK" button is pressed.
+    *
+    * This method is responsible for creating a new station with the provided
+    * information. If an `oldStation` is provided, it removes the old station
+    * before adding the newly created station to the model. If the information
+    * provided is invalid, IllegalArgumentException is thrown.
+    *
+    * @param stationName
+    *   The name of the new station.
+    * @param latitude
+    *   The latitude of the new station's location.
+    * @param longitude
+    *   The longitude of the new station's location.
+    * @param numberOfTrack
+    *   The number of tracks at the new station.
+    * @param oldStation
+    *   An optional existing station that may be removed before adding the new
+    *   station. If no station is provided, no removal occurs.
+    * @throws IllegalArgumentException
+    *   If the information provided is invalid.
+    */
   def onOkClick(
       stationName: String,
       latitude: String,
