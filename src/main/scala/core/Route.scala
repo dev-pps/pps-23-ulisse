fee75bcdcf74f8b5735160e6bf7c569b928004ff
Manuel Buizo <buizomanuel@gmail.com>
2025-01-08 17:59:30 +0100

    feat: compute length route

--- src/main/scala/core/Route.scala
+++ src/main/scala/core/Route.scala
@@ -1,20 +1,25 @@
 package scala.core
 
 import scala.core.Route.{Path, TypeRoute}
+import scala.utils.Points
 
 trait Route:
   def typology: TypeRoute
   def railsCount: Int
   def path: Path
+  def length: Double
 
 object Route:
   def apply(typeRoute: TypeRoute, railsCount: Int, path: Path): Route =
     RouteImpl(typeRoute, railsCount, path)
 
-  type Path = (String, String)
+  type Station = (String, (Double, Double))
+  type Path    = (Station, Station)
 
   enum TypeRoute:
     case Normal, AV
 
   private case class RouteImpl(typology: TypeRoute, railsCount: Int, path: Path)
-      extends Route
+      extends Route:
+    override def length: Double =
+      Points.computePointsDistance(path._1._2, path._2._2)
--- src/test/core/RouteTest.scala
+++ src/test/core/RouteTest.scala
@@ -4,18 +4,27 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
 import scala.core.Route
-import scala.core.Route.{ Path, TypeRoute }
+import scala.core.Route.{ Path, Station, TypeRoute }
+import scala.utils.Points
 
 
 class RouteTest extends AnyFlatSpec with Matchers:
+  val railsCount = 1
+  val riminiStation: Station = ("Cesena", (20d, 20d))
+  val cesenaStation: Station = ("Rimini", (10d, 10d))
+  val path: Path = (riminiStation, cesenaStation)
+  val route: Route = Route(TypeRoute.Normal, railsCount, path)
 
   "create route" should "set core parameters: typology, railsCount, path" in:
-    val railsCount = 1
-    val path: Path = ("Rimini", "Cesena")
-    val route: Route = Route(TypeRoute.Normal, railsCount, path)
     route.typology must be (TypeRoute.Normal)
     route.railsCount must be (railsCount)
     route.path must be (path)
+
+  "compute route length" should "be from path" in:
+    route.length must be (Points.computePointsDistance(riminiStation._2, cesenaStation._2))
+
+
+
 
 
 
