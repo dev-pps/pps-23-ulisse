4e0550631a86ddb1c469dca6f7ac4ec34d1847bf
Manuel Buizo <buizomanuel@gmail.com>
2025-01-09 09:22:52 +0100

    feat: save route in routeBank

---
+++ src/test/scala/application/RouteBankTest.scala
@@ -0,0 +1,29 @@
+package application
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+
+import scala.core.Route
+import scala.core.Route.{ Path, Station, TypeRoute }
+
+import scala.application.RouteBank
+
+class RouteBankTest extends AnyFlatSpec with Matchers:
+  val railsCount = 1
+  val riminiStation: Station = ("Cesena", (20d, 20d))
+  val cesenaStation: Station = ("Rimini", (10d, 10d))
+  val path: Path = (riminiStation, cesenaStation)
+  val route: Route = Route(TypeRoute.Normal, railsCount, path)
+
+  "save route" should "be contains in routeBank" in:
+    val routeBank = RouteBank.empty()
+    val newRouteBank = routeBank.save(route)
+
+    routeBank.contains(route) must be (false)
+    newRouteBank.contains(route) must be (true)
+
+
+
+
+    
+    
