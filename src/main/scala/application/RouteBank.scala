b3b2420ec47425ff886d81f65635ded2555b53a6
Manuel Buizo <buizomanuel@gmail.com>
2025-01-09 09:23:38 +0100

    refactor: routeBank

--- src/main/scala/application/RouteBank.scala
+++ src/main/scala/application/RouteBank.scala
@@ -1,7 +1,7 @@
 package scala.application
 
 import scala.core.Route
-import scala.collection.immutable.{ ArraySeq, List }
+import scala.collection.immutable.{ArraySeq, List}
 
 trait RouteBank:
   def save(route: Route): RouteBank
@@ -9,13 +9,10 @@ trait RouteBank:
 
 object RouteBank:
   def apply(routes: List[Route]): RouteBank = RouteBankImpl(routes)
-  def empty(): RouteBank = RouteBankImpl(List.empty[Route])
-
+  def empty(): RouteBank                    = RouteBankImpl(List.empty[Route])
 
   opaque type Bank = List[Route]
 
   private case class RouteBankImpl(bank: Bank) extends RouteBank:
     override def save(route: Route): RouteBank = RouteBank(bank.appended(route))
     override def contains(route: Route): Boolean = bank.contains(route)
-    
-    
