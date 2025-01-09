aa40931559750bbaccfe6fec5d253dd0e0bd29f2
Matteo <matteo.violani@studio.unibo.it>
2025-01-09 23:35:58 +0100

    feat: created train, carriages and train's technology type entities

--- src/main/scala/train/model/Trains.scala
+++ src/main/scala/train/model/Trains.scala
@@ -1,38 +1,53 @@
 package train.model
 
-import train.model.Trains.TrainType.{HighSpeed, Normal}
-
 object Trains:
 
-  enum TrainType(val name: String, val maxSpeed: Int):
-    case Normal    extends TrainType("Normal", 130)
-    case HighSpeed extends TrainType("HighSpeed", 300)
+  object Carriages:
+    private type Capacity = Int
+    private case class CarriageImpl(use: UseType, capacity: Capacity)
+        extends Carriage
+
+    enum UseType:
+      case Passenger
+      case Other
+
+    trait Carriage:
+      def use: UseType
+      def capacity: Capacity
+
+    object Carriage:
+      def apply(use: UseType, capacity: Capacity): Carriage =
+        CarriageImpl(use, capacity)
+
+  trait TechnologyType:
+    def name: String
+    def maxSpeed: Int
+
+  case class Technology(name: String, maxSpeed: Int) extends TechnologyType
 
   trait Train:
-    def trainType: TrainType
-    def maxSpeed: Int
     val name: String
-    val carriages: Int
-    val capacity: Int
+    val techType: TechnologyType
+    val carriage: Carriages.Carriage
+    val carriageCount: Int
+    def maxSpeed: Int
+    def capacity: Int
+
+  import Carriages.Carriage
+  object Train:
+    def apply(
+        name: String,
+        techType: TechnologyType,
+        carriage: Carriage,
+        carriageCount: Int
+    ): Train =
+      TrainImpl(name, techType, carriage, carriageCount)
 
   private class TrainImpl(
       val name: String,
-      val carriages: Int,
-      val capacity: Int,
-      val trainType: TrainType
+      val techType: TechnologyType,
+      val carriage: Carriage,
+      val carriageCount: Int
   ) extends Train:
-    def maxSpeed: Int = trainType.maxSpeed
-
-  def makeTrain(
-      name: String,
-      trainType: TrainType,
-      carriages: Int,
-      capacity: Int
-  ): Train =
-    TrainImpl(name, carriages, capacity, trainType)
-
-  def makeRegionalTrain(name: String, carriages: Int, capacity: Int): Train =
-    makeTrain(name, Normal, carriages, capacity)
-
-  def makeHighSpeedTrain(name: String, carriages: Int, capacity: Int): Train =
-    makeTrain(name, HighSpeed, carriages, capacity)
+    def maxSpeed: Int = techType.maxSpeed
+    def capacity: Int = carriage.capacity * carriageCount
