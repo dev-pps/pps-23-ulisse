2fe13ba40dfc114d36c55ad01c65d1406070e69e
Matteo <matteo.violani@studio.unibo.it>
2025-01-09 23:35:58 +0100

    feat: created train, carriages and train's technology type entities

--- src/test/scala/train/model/TrainTest.scala
+++ src/test/scala/train/model/TrainTest.scala
@@ -1,35 +1,44 @@
 package train.model
 
 import org.scalatest.*
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers.be
 import org.scalatest.matchers.should.Matchers.should
 import org.scalatest.matchers.{must, should}
-import train.model.Trains.Train
-import train.model.Trains.TrainType.{HighSpeed, Normal}
+import train.model.Trains.{Technology, Train}
+import train.model.Trains.Carriages.{Carriage, UseType}
 
-class TrainTest extends AnyFunSuite:
-  val REGIONAL_TRAIN_SPEED   = 130
-  val HIGH_SPEED_TRAIN_SPEED = 300
+class TrainTest extends AnyFlatSpec:
 
-  test("regionalTrainCreation"):
+  "A Train" should "provide overall capacity and length of train by carriage info" in:
+    val carriageCapacity = 50
+    val carriageCount    = 5
+    val carriageInfo =
+      Carriage(UseType.Passenger, carriageCapacity)
     val train: Train =
-      Trains.makeRegionalTrain(name = "3905", carriages = 5, capacity = 400)
-    train.maxSpeed should be(REGIONAL_TRAIN_SPEED)
-    train.trainType should be(Normal)
-    train.name should be("3905")
-    train.carriages should be(5)
-    train.capacity should be(400)
-
-  test("highSpeedTrainCreation"):
-    val train: Train =
-      Trains.makeHighSpeedTrain(
-        name = "Frecciarossa",
-        carriages = 12,
-        capacity = 900
+      Trains.Train(
+        name = "3905",
+        techType = Technology(name = "HighSpeed", maxSpeed = 300),
+        carriage = carriageInfo,
+        carriageCount = carriageCount
       )
-    train.maxSpeed should be(HIGH_SPEED_TRAIN_SPEED)
-    train.trainType should be(HighSpeed)
-    train.name should be("Frecciarossa")
-    train.carriages should be(12)
-    train.capacity should be(900)
+    train.name should be("3905")
+    train.techType.name should be ("HighSpeed")
+    train.techType.maxSpeed should be (300)
+    train.maxSpeed should be (train.techType.maxSpeed)
+    train.carriage.use should be (UseType.Passenger)
+    train.carriage.capacity should be (carriageCapacity)
+    train.capacity should be (carriageCapacity * carriageCount)
+
+//  test("highSpeedTrainCreation"):
+//    val train: Train =
+//      Trains.makeHighSpeedTrain(
+//        name = "Frecciarossa",
+//        carriages = 12,
+//        capacity = 900
+//      )
+//    train.maxSpeed should be(HIGH_SPEED_TRAIN_SPEED)
+//    train.trainType should be(HighSpeed)
+//    train.name should be("Frecciarossa")
+//    train.carriages should be(12)
+//    train.capacity should be(900)
