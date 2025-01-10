2dedea10526db4993bec745fda1a62e4ae777112
Manuel Buizo <buizomanuel@gmail.com>
2025-01-10 17:52:29 +0100

    feat: set formPanel at vertical center

--- src/main/scala/view/FormPanel.scala
+++ src/main/scala/view/FormPanel.scala
@@ -1,6 +1,7 @@
 package view
 
 import scala.swing.*
+import scala.swing.BorderPanel.Position.{Center, North, South}
 
 trait FormPanel[
     +MP <: Panel,
@@ -12,7 +13,7 @@ trait FormPanel[
 object FormPanel:
 
   def apply[
-      MP <: Panel,
+      MP <: BorderPanel,
       P <: Panel,
       A <: Component,
       B <: Component
@@ -22,7 +23,7 @@ object FormPanel:
   ): FormPanel[_, _, _, _] = FormPanelImpl(panel, pairPanels)
 
   private case class FormPanelImpl[
-      +MP <: Panel,
+      +MP <: BorderPanel,
       +P <: Panel,
       +A <: Component,
       +B <: Component,
@@ -31,8 +32,15 @@ object FormPanel:
       mainPanel: WrapPanel[MP],
       pairPanels: List[PairPanel[P, A, B]]
   ) extends FormPanel[MP, P, A, B]:
-    private val submit = new Button("Submit")
-    pairPanels.foreach(pair => mainPanel.addComponent(pair.panel))
-    mainPanel.addComponent(submit)
+    private val submit             = new Button("Submit")
+    private val boxPanel: BoxPanel = BoxPanel(Orientation.Vertical)
+
+    boxPanel.contents += Swing.Glue
+    boxPanel.contents ++= pairPanels.map(_.panel)
+    boxPanel.contents += Swing.Glue
+
+//    mainPanel.panel.layout(Label("Creation Form")) = North
+    mainPanel.panel.layout(boxPanel) = Center
+    mainPanel.panel.layout(submit) = South
 
     export mainPanel.panel, mainPanel.addComponent, mainPanel.setVisible
