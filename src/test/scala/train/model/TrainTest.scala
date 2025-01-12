1bdf8f612d6887e4d35eefd646975a6e5a7725f6
Matteo <matteo.violani@studio.unibo.it>
2025-01-12 13:37:51 +0100

    feat: add/remove train technology, rename of carriage into wagon

--- src/test/scala/train/model/TrainTest.scala
+++ src/test/scala/train/model/TrainTest.scala
@@ -5,29 +5,31 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers.be
 import org.scalatest.matchers.should.Matchers.should
 import org.scalatest.matchers.{must, should}
-import train.model.Trains.{TechnologyType, Train}
-import train.model.Trains.Carriages.{Carriage, UseType}
+import train.model.domain.Trains.Train
+import train.model.domain.Wagons.{UseType, Wagon}
+import train.model.domain.Technology
+import train.model.domain.Trains
 
 class TrainTest extends AnyFlatSpec:
-  val technology: TechnologyType =
-    TechnologyType(name = "HighSpeed", maxSpeed = 300)
-  val carriageCapacity = 50
-  val carriageCount    = 5
-  val carriageInfo: Carriage =
-    Carriage(UseType.Passenger, carriageCapacity)
+  val technology: Technology =
+    Technology(name = "HighSpeed", maxSpeed = 300)
+  val wagonCapacity = 50
+  val wagonCount    = 5
+  val wagonInfo: Wagon =
+    Wagon(UseType.Passenger, wagonCapacity)
   val train: Train =
     Trains.Train(
       name = "3905",
       techType = technology,
-      carriage = carriageInfo,
-      carriageCount = carriageCount
+      wagon = wagonInfo,
+      wagonCount = wagonCount
     )
 
-  "A Train" should "provide overall capacity and length depending carriage specs" in:
+  "A Train" should "provide overall capacity and length depending wagon specs" in:
     train.name should be("3905")
-    train.carriage.use should be(UseType.Passenger)
-    train.carriage.capacity should be(carriageCapacity)
-    train.capacity should be(carriageCapacity * carriageCount)
+    train.wagon.use should be(UseType.Passenger)
+    train.wagon.capacity should be(wagonCapacity)
+    train.capacity should be(wagonCapacity * wagonCount)
 
   it should "have speed bounded by technology max speed" in:
     train.techType.name should be("HighSpeed")
