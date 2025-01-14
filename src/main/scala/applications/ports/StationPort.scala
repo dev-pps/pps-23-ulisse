ed46c68162d0ccb46b9de269398942b69d41efa4
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-14 01:43:46 +0100

    docs(StationPort): add ScalaDoc

--- src/main/scala/applications/ports/StationPort.scala
+++ src/main/scala/applications/ports/StationPort.scala
@@ -4,14 +4,50 @@ import applications.station.StationMap
 import entities.Location
 import entities.station.Station
 
+/** Defines the `StationPort` object containing inbound operations for managing
+  * a `StationMap`.
+  */
 object StationPort:
+
+  /** Trait representing inbound operations for interacting with a `StationMap`.
+    *
+    * @tparam L
+    *   The type of location used by the stations.
+    */
   trait Inbound[L <: Location]:
 
+    /** Retrieves the current station map.
+      *
+      * @return
+      *   The `StationMap` containing the stations.
+      */
     def stationMap: StationMap[L]
 
+    /** Adds a station to the station map.
+      *
+      * @param station
+      *   The station to be added.
+      * @return
+      *   Either the updated `StationMap` or an `Error` indicating the issue.
+      */
     def addStation(station: Station[L]): Either[StationMap.Error, StationMap[L]]
 
+    /** Removes a station from the station map.
+      *
+      * @param station
+      *   The station to be removed.
+      * @return
+      *   Either the updated `StationMap` or an `Error` indicating the issue.
+      */
     def removeStation(station: Station[L])
         : Either[StationMap.Error, StationMap[L]]
 
+    /** Finds a station at a specified location.
+      *
+      * @param location
+      *   The location to search for a station.
+      * @return
+      *   An `Option` containing the station at the specified location, if it
+      *   exists.
+      */
     def findStationAt(location: L): Option[Station[L]]
