24e2f7124bffc30bcb4bd19d2126de67239ebbbb
Manuel Buizo <buizomanuel@gmail.com>
2025-01-19 00:02:26 +0100

    feat: improv routeManager saving

--- src/main/scala/ulisse/applications/useCases/RouteManager.scala
+++ src/main/scala/ulisse/applications/useCases/RouteManager.scala
@@ -1,5 +1,7 @@
 package ulisse.applications.useCases
 
+import cats.syntax.either.*
+import ulisse.applications.useCases.RouteManager.ErrorSaving
 import ulisse.entities.Route
 import ulisse.entities.Route.Id
 
@@ -8,23 +10,28 @@ import scala.collection.immutable.Map
 trait RouteManager:
   def size: Int
   def route(id: Id): Option[Route]
-  def save(route: Route): RouteManager
+  def save(route: Route): Either[ErrorSaving, RouteManager]
   def contains(route: Route): Boolean
 
 object RouteManager:
   def apply(bank: Bank): RouteManager = RouteManagerImpl(bank)
+
   def fromList(routes: List[Route]): RouteManager =
     RouteManagerImpl(routes.map(route => (route.id, route)).toMap)
+
   def empty(): RouteManager = RouteManagerImpl(Map.empty)
+
+  enum ErrorSaving:
+    case notExist
 
   opaque type Bank = Map[Id, Route]
 
-  private case class RouteManagerImpl(bank: Bank) extends RouteManager:
-    override def size: Int = bank.size
+  private case class RouteManagerImpl(manager: Bank) extends RouteManager:
+    override def size: Int = manager.size
 
-    override def save(route: Route): RouteManager =
-      RouteManager(bank + (route.id -> route))
+    override def save(route: Route): Either[ErrorSaving, RouteManager] =
+      RouteManager(manager + (route.id -> route)).asRight
 
-    override def route(id: Id): Option[Route] = bank.get(id)
+    override def route(id: Id): Option[Route] = manager.get(id)
 
-    override def contains(route: Route): Boolean = bank.contains(route.id)
+    override def contains(route: Route): Boolean = manager.contains(route.id)
--- src/test/scala/ulisse/applications/RouteManagerTest.scala
+++ src/test/scala/ulisse/applications/RouteManagerTest.scala
@@ -14,39 +14,44 @@ class RouteManagerTest extends AnyFlatSpec with Matchers:
   val railsCount             = 1
 
   val route: Route = Route(TypeRoute.Normal, path, pathLength, railsCount)
-  val routeBank    = RouteManager.fromList(List(route))
+  val routeManager = RouteManager.fromList(List(route))
 
   "save new route" should "contains in routeBank" in:
     val routeBank    = RouteManager.empty()
     val newRouteBank = routeBank.save(route)
 
-    routeBank.contains(route) must be(false)
-    newRouteBank.contains(route) must be(true)
+    newRouteBank match
+      case Left(error) => fail(error.productPrefix)
+      case Right(newRouteBank) =>
+        routeBank.contains(route) must be(false)
+        newRouteBank.contains(route) must be(true)
 
   "read route" should "from id" in:
-    val optRoute = routeBank.route(route.id)
+    val optRoute = routeManager.route(route.id)
     optRoute match
       case Some(newRoute) => newRoute must be(route)
       case _              => fail("route is empty")
 
   "save route with same id" should "change route railsCount" in:
-    val sameRoute    = Route(TypeRoute.Normal, path, pathLength, 3)
-    val newRouteBank = routeBank.save(sameRoute)
+    val differentRailsCountRoute = Route(TypeRoute.Normal, path, pathLength, 3)
+    val newRouteBank             = routeManager.save(differentRailsCountRoute)
+    val optOldRoute              = routeManager.route(route.id)
 
-    val optRoute     = routeBank.route(route.id)
-    val optSameRoute = newRouteBank.route(sameRoute.id)
-
-    optRoute match
-      case Some(route) => optSameRoute match
-          case Some(sameRoute) =>
-            route.railsCount must not be sameRoute.railsCount
-          case None => fail("route is empty")
-      case None => fail("route is empty")
+    newRouteBank match
+      case Left(error) => fail(error.productPrefix)
+      case Right(newRouteBank) => newRouteBank.route(differentRailsCountRoute.id) match
+          case Some(route) =>
+            route.railsCount must be(differentRailsCountRoute.railsCount)
+            optOldRoute.map(oldRout => route.railsCount must not be oldRout.railsCount)
+          case None => fail("Route not found")
 
   "save two different route" should "have two element" in:
     val differentRoute = Route(TypeRoute.AV, path, pathLength, railsCount)
-    val newRouteBank   = routeBank.save(differentRoute)
+    val newRouteBank   = routeManager.save(differentRoute)
 
-    newRouteBank.contains(differentRoute) must be(true)
-    val lengthRouteBank = 2
-    newRouteBank.size must be(lengthRouteBank)
+    newRouteBank match
+      case Left(error) => fail(error.productPrefix)
+      case Right(newRouteBank) =>
+        val lengthRouteBank = routeManager.size + 1
+        newRouteBank.contains(differentRoute) must be(true)
+        newRouteBank.size must be(lengthRouteBank)
