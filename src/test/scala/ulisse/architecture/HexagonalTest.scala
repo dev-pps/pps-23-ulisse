6562c7332e25435c0d697a89d761c77c61fa5120
Manuel Buizo <buizomanuel@gmail.com>
2025-01-18 17:59:43 +0100

    refactor: HexagonalTest

--- src/test/scala/ulisse/architecture/HexagonalTest.scala
+++ src/test/scala/ulisse/architecture/HexagonalTest.scala
@@ -8,7 +8,8 @@ import ulisse.architecture.ArchUnits.IMPORT_ONLY_CLASSES_CREATED
 class HexagonalTest extends AnyFlatSpec:
 
   "no classes of the entities package" should "depends on the applications, infrastructures and userInteractions packages" in:
-    val rule = ArchRuleDefinition.noClasses()
+    val rule = ArchRuleDefinition
+      .noClasses
       .that
       .resideInAPackage(Packages.ENTITIES)
       .should.dependOnClassesThat.resideInAnyPackage(
@@ -20,7 +21,8 @@ class HexagonalTest extends AnyFlatSpec:
 
   "no classes of the applications package" should "depend on the infrastructures and userInteractions packages " +
     "and should depend on entities package" in:
-      val rule = ArchRuleDefinition.noClasses()
+      val rule = ArchRuleDefinition
+        .noClasses
         .that
         .resideInAnyPackage(Packages.APPLICATIONS)
         .should.dependOnClassesThat.resideInAnyPackage(Packages.INFRASTRUCTURES, Packages.USER_INTERACTIONS)
@@ -29,7 +31,8 @@ class HexagonalTest extends AnyFlatSpec:
 
   "no classes of the infrastructures package" should "depend on the entities and userInteractions packages " +
     "and should depend on applications package" in:
-      val rule = ArchRuleDefinition.noClasses()
+      val rule = ArchRuleDefinition
+        .noClasses
         .that
         .resideInAnyPackage(Packages.INFRASTRUCTURES)
         .should.dependOnClassesThat.resideInAnyPackage(Packages.ENTITIES, Packages.USER_INTERACTIONS)
@@ -39,7 +42,8 @@ class HexagonalTest extends AnyFlatSpec:
 
   "no classes of the userInteractions package" should "depend on the entities and infrastructures packages " +
     "and should depend on application package" in:
-      val rule = ArchRuleDefinition.noClasses()
+      val rule = ArchRuleDefinition
+        .noClasses
         .that
         .resideInAnyPackage(Packages.USER_INTERACTIONS)
         .should.dependOnClassesThat.resideInAnyPackage(Packages.ENTITIES, Packages.INFRASTRUCTURES)
@@ -49,7 +53,8 @@ class HexagonalTest extends AnyFlatSpec:
       rule.check(IMPORT_ONLY_CLASSES_CREATED)
 
   "hexagonal architecture" should "be entities -> applications -> infrastructures/userInteractions" in:
-    val rule = Architectures.onionArchitecture()
+    val rule = Architectures
+      .onionArchitecture
       .domainModels(Packages.ENTITIES)
       .applicationServices(Packages.APPLICATIONS)
       .adapter("infrastructures", Packages.INFRASTRUCTURES)
