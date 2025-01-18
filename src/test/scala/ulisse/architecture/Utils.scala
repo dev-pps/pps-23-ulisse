2f991c60aaf363cbafbf90c24062a24d8b0de380
Manuel Buizo <buizomanuel@gmail.com>
2025-01-18 18:43:15 +0100

    test: check name of applications.adapters package

--- src/test/scala/ulisse/architecture/Utils.scala
+++ src/test/scala/ulisse/architecture/Utils.scala
@@ -3,14 +3,20 @@ package ulisse.architecture
 import com.tngtech.archunit.core.importer.{ClassFileImporter, Location}
 
 object Packages:
+  private val applications    = "applications"
+  private val infrastructures = "infrastructures"
+  private val adapters        = "adapters"
+
   val PROJECT           = "..ulisse.."
   val ENTITIES          = "..entities.."
-  val APPLICATIONS      = "..applications.."
-  val INFRASTRUCTURES   = "..infrastructures.."
+  val APPLICATIONS      = ".." + applications + ".."
+  val INFRASTRUCTURES   = ".." + infrastructures + ".."
   val USER_INTERACTIONS = "..userInteractions.."
 
-  val USE_CASES = "..useCases.."
-  val PORTS     = "..ports"
+  val PORTS          = "..ports.."
+  val USE_CASES      = "..useCases.."
+  val INPUT_ADAPTER  = ".." + applications + "." + adapters + ".."
+  val OUTPUT_ADAPTER = ".." + infrastructures + "." + adapters + ".."
 
 object ArchUnits:
   private val DO_NOT_INCLUDE_SCALA_COMPILED_FILE: Location => Boolean = !_.contains("$")
