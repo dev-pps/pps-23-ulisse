19859de6051809f5cb02e49606ce59c0ae35f8ce
Matteo Violani <matteo.violani@studio.unibo.it>
2025-01-19 15:54:02 +0100

    docs: fixed base url in config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    base: '/report',
+    base: '/pps-23-ulisse/report',
     title: "PPS-23-Ulisse",
     description: "Ulisse an Train Infrastructure Similator",
     themeConfig: {
