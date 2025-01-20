b629af74780384c0961e4f821b20fbb5b28b6af3
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-20 19:17:00 +0100

    feat(StationMap): implement unchecked StationMap and use as default constructor

--- src/main/scala/ulisse/applications/useCases/StationManager.scala
+++ src/main/scala/ulisse/applications/useCases/StationManager.scala
@@ -2,18 +2,20 @@ package ulisse.applications.useCases
 
 import ulisse.applications.ports.StationPorts
 import ulisse.applications.station.StationMap
+import ulisse.applications.station.StationMap.CheckedStationMap
 import ulisse.entities.Coordinates.Coordinate
 import ulisse.entities.station.Station
 
 final case class StationManager[N: Numeric, C <: Coordinate[N]](uiPort: StationPorts.Output):
   @SuppressWarnings(Array("org.wartremover.warts.Var"))
-  private var _stationMap: StationMap[N, C] = StationMap[N, C]()
+  private var _stationMap: CheckedStationMap[N, C] = StationMap.createCheckedStationMap[N, C]()
 
   def stationMap: StationMap[N, C] = _stationMap
 
   def addStation(station: Station[N, C]): Either[StationMap.Error, StationMap[N, C]] =
-    _stationMap.addStation(station).map: updatedMap =>
+    _stationMap.addStation(station).map(updatedMap =>
       _stationMap = updatedMap; updatedMap
+    )
 
   def removeStation(station: Station[N, C]): Either[StationMap.Error, StationMap[N, C]] =
     _stationMap.removeStation(station).map: updatedMap =>
