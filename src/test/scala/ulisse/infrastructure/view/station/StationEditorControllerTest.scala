f2276588a08f7a0c429624d882625bd92877d759
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-21 16:25:04 +0100

    test(StationEditorControllerTest): restore tests

--- src/test/scala/ulisse/infrastructure/view/station/StationEditorControllerTest.scala
+++ src/test/scala/ulisse/infrastructure/view/station/StationEditorControllerTest.scala
@@ -12,95 +12,88 @@ import ulisse.infrastructures.view.station.StationEditorController
 import ulisse.infrastructures.view.station.StationEditorController.given
 import org.scalatestplus.mockito.MockitoSugar.mock
 import ulisse.applications.station.StationMap
+import ulisse.entities.station.Station.CheckedStation
 import ulisse.utils.Errors.BaseError
 
 class StationEditorControllerTest extends AnyWordSpec with Matchers:
 
+  private type N = Int
+  private type C = Coordinate[N]
   private val outputPort    = mock[StationPorts.Output]
   private val stationName   = "New Station"
   private val x             = 1
   private val y             = 1
-  private val numberOfTrack = "1"
+  private val numberOfTrack = 1
+  private val station       = Station(stationName, Coordinate(x, y), numberOfTrack)
+
   "StationEditorController" when:
     "onOkClick is invoked" should:
       "add a new station when inputs are valid and oldStation is None" in:
         val inputPort  = StationPortInputAdapter[Int, Coordinate[Int]](StationManager(outputPort))
         val controller = StationEditorController[Int, Coordinate[Int]](inputPort)
-        val station    = Station(stationName, Coordinate(x, y), 1)
+        val station    = Station(stationName, Coordinate(x, y), numberOfTrack)
 
         controller.onOkClick(
           stationName,
           x.toString,
           y.toString,
-          numberOfTrack,
+          numberOfTrack.toString,
           None
         ) shouldBe Right(StationMap(station))
 
         controller.findStationAt(Coordinate(x, y)) shouldBe Some(station)
 
-////      "replace the station when inputs are valid and oldStation is Some(station)" in:
-////        val model      = Manger()
-////        val controller = StationEditorController(view, model)
-////
-////        val oldStationName          = "Old Station"
-////        val oldStationLatitude      = "1.0"
-////        val oldStationLongitude     = "1.0"
-////        val oldStationNumberOfTrack = "1"
-////
-////        val oldStation = Station(
-////          oldStationName,
-////          Location(oldStationLatitude.toDouble, oldStationLongitude.toDouble),
-////          oldStationNumberOfTrack.toInt
-////        )
-////        controller.onOkClick(
-////          oldStationName,
-////          oldStationLatitude,
-////          oldStationLongitude,
-////          oldStationNumberOfTrack,
-////          None
-////        )
-////
-////        val stationName   = "New Station"
-////        val latitude      = "2.0"
-////        val longitude     = "2.0"
-////        val numberOfTrack = "2"
-////
-////        val station = Station(
-////          stationName,
-////          Location(latitude.toDouble, longitude.toDouble),
-////          numberOfTrack.toInt
-////        )
-////        controller.onOkClick(
-////          stationName,
-////          latitude,
-////          longitude,
-////          numberOfTrack,
-////          Some(oldStation)
-////        )
-////
-////        controller.findStationAt(Location(
-////          latitude.toDouble,
-////          longitude.toDouble
-////        )) shouldEqual Some(station)
-////        controller.findStationAt(Location(
-////          oldStationLatitude.toDouble,
-////          oldStationLongitude.toDouble
-////        )) shouldEqual None
-////
-////      "throw an IllegalArgumentException when inputs are invalid" in:
-////        val model      = Manger()
-////        val controller = StationEditorController(view, model)
-////
-////        val stationName   = "New Station"
-////        val latitude      = "1.0"
-////        val longitude     = "1.0"
-////        val numberOfTrack = "1"
-////
-////        intercept[IllegalArgumentException]:
-////          controller.onOkClick(stationName, "a", longitude, numberOfTrack, None)
-////
-////        intercept[IllegalArgumentException]:
-////          controller.onOkClick(stationName, latitude, "a", numberOfTrack, None)
-////
-////        intercept[IllegalArgumentException]:
-////          controller.onOkClick(stationName, latitude, longitude, "a", None)
+      "replace the station when inputs are valid and oldStation is Some(station)" in:
+        val inputPort  = StationPortInputAdapter[Int, Coordinate[Int]](StationManager(outputPort))
+        val controller = StationEditorController[Int, Coordinate[Int]](inputPort)
+        val oldStation = Station(stationName, Coordinate(x, y), numberOfTrack)
+        val newStation = Station(stationName, Coordinate(x + 1, y + 1), numberOfTrack + 1)
+
+        controller.onOkClick(
+          stationName,
+          x.toString,
+          y.toString,
+          numberOfTrack.toString,
+          None
+        ) shouldBe Right(StationMap(oldStation))
+
+        controller.findStationAt(Coordinate(x, y)) shouldBe Some(station)
+
+        controller.onOkClick(
+          stationName,
+          (x + 1).toString,
+          (y + 1).toString,
+          (numberOfTrack + 1).toString,
+          Some(oldStation)
+        ) shouldBe Right(StationMap(newStation))
+
+        controller.findStationAt(Coordinate(x, y)) shouldBe None
+        controller.findStationAt(Coordinate(x + 1, y + 1)) shouldBe Some(newStation)
+
+      "returns error when input are not valid" in:
+        val inputPort  = StationPortInputAdapter[Int, Coordinate[Int]](StationManager(outputPort))
+        val controller = StationEditorController[Int, Coordinate[Int]](inputPort)
+
+        controller.onOkClick(
+          stationName,
+          "a",
+          y.toString,
+          numberOfTrack.toString,
+          None
+        ) shouldBe Left(StationEditorController.Error.InvalidRowFormat)
+
+        controller.onOkClick(
+          stationName,
+          x.toString,
+          "a",
+          numberOfTrack.toString,
+          None
+        ) shouldBe Left(StationEditorController.Error.InvalidColumnFormat)
+
+        controller.onOkClick(
+          stationName,
+          x.toString,
+          y.toString,
+          "a",
+          None
+        ) shouldBe Left(StationEditorController.Error.InvalidNumberOfTrackFormat)
