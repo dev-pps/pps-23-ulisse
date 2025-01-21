7c07ee92831a43c4b435ee70df5480cd61279686
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-21 19:39:25 +0100

    fix: show stationEditorView when start the app

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,23 +12,14 @@ final case class Setting():
   type N = Int
   type C = Grid
   type S = CheckedStation[N, C]
-  lazy val outputAdapter: StationPortOutputAdapter[N, C, S]          = StationPortOutputAdapter(stationEditorController)
-  lazy val stationManager: StationManager[N, C, S]                   = StationManager(outputAdapter)
-  lazy val inputAdapter: StationPortInputAdapter[N, C, S]            = StationPortInputAdapter(stationManager)
-  lazy val stationEditorController: StationEditorController[N, C, S] = StationEditorController(inputAdapter)
-  val stationEditorView: StationEditorView                           = StationEditorView(stationEditorController)
+  lazy val outputAdapter: StationPortOutputAdapter[N, C, S]     = StationPortOutputAdapter(stationEditorController)
+  lazy val stationManager: StationManager[N, C, S]              = StationManager(outputAdapter)
+  lazy val inputAdapter: StationPortInputAdapter[N, C, S]       = StationPortInputAdapter(stationManager)
+  val stationEditorController: StationEditorController[N, C, S] = StationEditorController(inputAdapter)
+  val stationEditorView: StationEditorView                      = StationEditorView(stationEditorController)
 
 @main def main(): Unit =
   val app      = AppFrame()
   val settings = Setting()
   app.contents = settings.stationEditorView
   app.open()
-
-trait Map {
-  def findElement(key: String): Int
-}
-
-// Trait for CheckedMap (returns Either[String, Int])
-trait CheckedMap {
-  def findElement(key: String): Either[String, Int]
-}
