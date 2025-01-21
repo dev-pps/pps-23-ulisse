cb2ab77d53a617765c56dcd762d3bfe92c3431fa
Manuel Buizo <buizomanuel@gmail.com>
2025-01-21 23:34:35 +0100

    feat: create wrap for immutable UI

--- src/main/scala/ulisse/infrastructures/view/common/JComponent.scala
+++ src/main/scala/ulisse/infrastructures/view/common/JComponent.scala
@@ -1,25 +1,47 @@
 package ulisse.infrastructures.view.common
 
-import scala.swing.{Button, ComboBox, Label, TextField}
+import scala.swing.*
 
 object JComponent:
 
-  trait JLabel(label: String)
   object JLabel:
-    def apply(string: String): JLabel = JLabelImpl(string)
-    private case class JLabelImpl(label: String) extends Label(label) with JLabel(label)
+    def apply(text: String): JBLabel = JBLabel(Label(text), text)
+    case class JBLabel(label: Label, text: String)
 
-  trait JTextField(string: String)
   object JTextField:
-    def apply(string: String): JTextField = JTextFieldImpl(string)
-    private case class JTextFieldImpl(string: String) extends TextField(string) with JTextField(string)
+    def apply(text: String): JBTextField = JBTextField(TextField(text), text)
+    case class JBTextField(textField: TextField, text: String)
 
-  trait JComoBox[T](objs: Seq[T])
   object JComoBox:
-    def apply[T](objs: Seq[T]): JComoBox[T] = JComoBoxImpl(objs)
-    private case class JComoBoxImpl[T](objs: Seq[T]) extends ComboBox(objs) with JComoBox[T](objs)
+    def apply[T](items: Seq[T]): JBComoBox[T] = JBComoBox(ComboBox(items), items)
+    case class JBComoBox[T](comboBox: ComboBox[T], items: Seq[T])
 
-  trait JButton(label: String)
   object JButton:
-    def apply(string: String): JButton = JButtonImpl(string)
-    private case class JButtonImpl(label: String) extends Button(label) with JButton(label)
+    def apply(text: String): JBButton = JBButton(new Button(text), text)
+    case class JBButton(button: Button, label: String)
+
+  object JPanel:
+
+    object JFlowPanel:
+      def apply(items: Component*): JFlowPanel = JFlowPanel(FlowPanel(items: _*), items: _*)
+      case class JFlowPanel(flowPanel: FlowPanel, items: Component*)
+
+    trait JBoxPanel()
+    object JBoxPanel:
+      def apply(): JBoxPanel = BoxPanelImpl()
+      private case class BoxPanelImpl() extends BoxPanel(Orientation.Vertical) with JBoxPanel
+
+    trait JBorderPanel
+    object JBorderPanel:
+      def apply(): JBorderPanel = BorderPanelImpl()
+      private case class BorderPanelImpl() extends BorderPanel with JBorderPanel
+
+    trait JGridPanel
+    object JGridPanel:
+      def apply(): JGridPanel = GridPanelImpl()
+      private case class GridPanelImpl() extends GridPanel(1, 1) with JGridPanel
+
+    trait JGridBagPanel
+    object JGridBagPanel:
+      def apply(): JGridBagPanel = GridBagPanelImpl()
+      private case class GridBagPanelImpl() extends GridBagPanel with JGridBagPanel
