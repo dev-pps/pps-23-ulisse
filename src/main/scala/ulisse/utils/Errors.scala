9eec9648e9fcc61ade768ee6f5f4dc89f21852ab
Matteo <matteo.violani@studio.unibo.it>
2025-01-22 18:50:34 +0100

    refactor: changed name of ErrorNotExists

--- src/main/scala/ulisse/utils/Errors.scala
+++ src/main/scala/ulisse/utils/Errors.scala
@@ -4,9 +4,8 @@ package ulisse.utils
 object Errors:
 
   trait BaseError
-
   trait ErrorMessage(val msg: String) extends BaseError
 
   class ErrorNotFound(private val text: String)   extends ErrorMessage(s"[NOT-FOUND] $text")
-  class ErrorValidation(private val text: String) extends ErrorMessage(s"[NOT-VALIDATE] $text")
-  class ErrorExist(private val text: String)      extends ErrorMessage(s"[NOT-EXIST] $text")
+  class ErrorValidation(private val text: String) extends ErrorMessage(s"[NOT-VALID] $text")
+  class ErrorNotExist(private val text: String)   extends ErrorMessage(s"[NOT-EXIST] $text")
