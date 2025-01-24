36e6aa3b97640d9e7f005d6e9584277f3c3eaa2f
Manuel Buizo <buizomanuel@gmail.com>
2025-01-24 16:23:00 +0100

    feat: finish possible methods to create loop

--- src/main/scala/ulisse/applications/adapters/RouteAdapter.scala
+++ src/main/scala/ulisse/applications/adapters/RouteAdapter.scala
@@ -4,16 +4,41 @@ import cats.syntax.either.*
 import ulisse.applications.ports.RoutePorts
 import ulisse.applications.useCases.RouteManager
 import ulisse.applications.useCases.RouteManager.ErrorSaving
-import ulisse.entities.Route
+import ulisse.entities.{Coordinates, Route}
+
+import java.util.concurrent.LinkedBlockingQueue
+import scala.concurrent.{Future, Promise}
 
 object RouteAdapter:
 
   object UIAdapter:
-    def apply(manager: RouteManager): RoutePorts.UIPort = IUAdapter(manager)
+    def apply(queue: LinkedBlockingQueue[RouteManager => RouteManager]): RoutePorts.UIPort =
+      IUAdapter(queue)
 
-    private case class IUAdapter(manager: RouteManager) extends RoutePorts.UIPort():
-      export manager.*
+    private case class IUAdapter(queue: LinkedBlockingQueue[RouteManager => RouteManager]) extends RoutePorts.UIPort():
 
-      override def save(optRoute: Option[Route]): Either[RouteManager.ErrorSaving, RoutePorts.UIPort] =
-        optRoute.map(route => manager.save(route).map(newManager => UIAdapter(newManager)))
-          .getOrElse(Left(ErrorSaving.creation))
+      override def save(optRoute: Option[Route]): Future[Either[RouteManager.ErrorSaving, List[Route]]] =
+        val promise = Promise[Either[RouteManager.ErrorSaving, List[Route]]]()
+        queue.offer(manager => {
+          val either: Either[ErrorSaving, RouteManager] =
+            optRoute.map(route => manager.save(route)).getOrElse(Left(ErrorSaving.creation))
+          either match
+            case Left(error) =>
+              promise.success(error.asLeft)
+              manager
+            case Right(newManager) =>
+              promise.success(newManager.routes.asRight)
+              newManager
+        })
+        promise.future
+
+//        val responsePort = optRoute.map(route => manager.save(route).map(manager => copy(manager = manager)))
+//          .getOrElse(Left(ErrorSaving.creation))
+//
+//        val newManager: RouteManager => RouteManager = manager => optRoute.map(route => manager.save(route))
+//        val newManager: RouteManager => RouteManager = manager => optRoute.map(route => manager.save(route))
+//        queue.put(newManager)
+
+//        queue.put(manager => manager.save(optRoute).map(manager => copy(queue = queue)))
+//        optRoute.map(route => manager.save(route).map(manager => copy(manager = manager)))
+//          .getOrElse(Left(ErrorSaving.creation))
