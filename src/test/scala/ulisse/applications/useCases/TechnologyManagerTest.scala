d05af63f8cda98069911336b750c6cdfac0e7574
Matteo <matteo.violani@studio.unibo.it>
2025-01-25 14:54:05 +0100

    refactor: train technology improved, train port changed

--- src/test/scala/ulisse/applications/useCases/TechnologyManagerTest.scala
+++ src/test/scala/ulisse/applications/useCases/TechnologyManagerTest.scala
@@ -1,19 +1,19 @@
-package ulisse.applications.useCases.train
+package ulisse.applications.useCases
 
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.must.Matchers.be
 import org.scalatest.matchers.should.Matchers.should
-import ulisse.entities.train.Technology
-import TechnologyManagers.TechnologyManager
 import TechnologyManagers.TechErrors.{InvalidSpeed, TechnologyAlreadyExists}
+import TechnologyManagers.TechnologyManager
+import ulisse.entities.Technology
 
 class TechnologyManagerTest extends AnyFeatureSpec with GivenWhenThen:
-  val emptyManager: TechnologyManager = TechnologyManager(List.empty)
-  val technology: Technology          = Technology("Standard", 130, 1.0, 0.5)
+  val emptyManager: TechnologyManager[Technology] = TechnologyManager(List.empty)
+  val technology: Technology                      = Technology("Standard", 130)
 
-  Feature("Managing train technologies (add, remove)"):
-    Scenario("Add new train technology"):
+  Feature("Managing technologies (add, remove)"):
+    Scenario("Add new technology"):
       Given("there are no technologies saved yet")
       emptyManager.technologiesList.size should be(0)
       When("I add technology with a given name and max speed value")
@@ -23,7 +23,7 @@ class TechnologyManagerTest extends AnyFeatureSpec with GivenWhenThen:
 
     Scenario("Try to add technology with already existing one (same name)"):
       Given("some technologies are saved")
-      val technology = Technology("Standard", 130, 1.0, 0.5)
+      val technology = Technology("Standard", 130)
       When("I add technology twice (with same name)")
       val res =
         for
@@ -48,7 +48,17 @@ class TechnologyManagerTest extends AnyFeatureSpec with GivenWhenThen:
       Given("that no technologies are saved")
       When("I add new technology with negative max speed")
       val negativeSpeed     = -100
-      val invalidTechnology = Technology("TechNotValid", negativeSpeed, 1.0, 0.5)
+      val invalidTechnology = Technology("TechNotValid", negativeSpeed)
       val res               = emptyManager.add(invalidTechnology)
       Then("it should be returned an InvalidValue error")
       res should be(Left(InvalidSpeed(negativeSpeed)))
+
+    Scenario("Get technology providing name"):
+      Given("There is technology that i need saved")
+      When("I ask manager to get it")
+      Then("technology should be returned")
+
+    Scenario("Try get technology that not exists")
+    Given("There is technology that i need saved")
+    When("I ask manager to get it")
+    Then("it should be returned and error")
