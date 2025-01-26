fda799a63bd3ce9c35d687cfa01c7c777a78bdcd
Matteo <matteo.violani@studio.unibo.it>
2025-01-26 12:41:47 +0100

    feat: added train editor demo launcher

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -33,3 +33,32 @@ object StationTypes:
   type N = Int
   type C = Grid
   type S = CheckedStation[N, C]
+
+@main def trainDemoMain(): Unit =
+  import ulisse.applications.adapters.TrainServiceManagerAdapter
+  import ulisse.applications.useCases.TrainManagers.TrainManager
+  import ulisse.applications.useCases.TechnologyManagers.TechnologyManager
+  import ulisse.applications.ports.TrainPorts
+  import ulisse.entities.train.Trains.TrainTechnology
+  import ulisse.infrastructures.view.train.TrainEditorView
+
+  type AppState = (TrainManager, TechnologyManager[TrainTechnology])
+  val stateEventQueue = LinkedBlockingQueue[AppState => AppState]
+
+  // Train Fleet init
+  val trainPort: TrainPorts.Input = TrainServiceManagerAdapter(stateEventQueue)
+  val trainView                   = TrainEditorView(trainPort)
+  trainView.open()
+
+  // Init Managers state
+  val trainManager     = TrainManager(List.empty)
+  val technologies     = List(TrainTechnology("AV", 300, 2.0, 1.0), TrainTechnology("Normal", 160, 1.0, 0.5))
+  val trainTechManager = TechnologyManager[TrainTechnology](technologies)
+
+  // App state init
+  val initialState: AppState = (trainManager, trainTechManager)
+  LazyList.continually(stateEventQueue.take()).scanLeft(initialState)((state, event) =>
+    event(state)
+  ).foreach((appState: AppState) =>
+    println(s"Stations: ${appState._1.trains}")
+  )
