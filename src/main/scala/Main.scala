0de20a5fbc730d2d44144a3961e49038d03431b5
Matteo <matteo.violani@studio.unibo.it>
2025-01-26 12:41:47 +0100

    feat: added train editor demo launcher

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,11 @@
-import ulisse.applications.adapters.StationPortInputAdapter
+import ulisse.applications.adapters.{StationPortInputAdapter, TrainServiceManagerAdapter}
 import ulisse.applications.useCases.StationManager
 import ulisse.entities.Coordinates.Grid
 import ulisse.infrastructures.view.AppFrame
 import ulisse.infrastructures.view.adapter.StationPortOutputAdapter
 import ulisse.infrastructures.view.station.{StationEditorController, StationEditorView}
+
+import java.util.concurrent.LinkedBlockingQueue
 
 final case class Setting():
   lazy val outputAdapter: StationPortOutputAdapter =
@@ -19,3 +21,31 @@ final case class Setting():
   val settings = Setting()
   app.contents = settings.stationEditorView
   app.open()
+
+@main def trainDemoMain(): Unit =
+  import ulisse.applications.useCases.TrainManagers.TrainManager
+  import ulisse.applications.useCases.TechnologyManagers.TechnologyManager
+  import ulisse.applications.ports.TrainPorts
+  import ulisse.entities.train.Trains.TrainTechnology
+  import ulisse.infrastructures.view.train.TrainEditorView
+
+  type AppState = (TrainManager, TechnologyManager[TrainTechnology])
+  val stateEventQueue = LinkedBlockingQueue[AppState => AppState]
+
+  // Train Fleet init
+  val trainPort: TrainPorts.Input = TrainServiceManagerAdapter(stateEventQueue)
+  val trainView                   = TrainEditorView(trainPort)
+  trainView.open()
+
+  // Init Managers state
+  val trainManager     = TrainManager(List.empty)
+  val technologies     = List(TrainTechnology("AV", 300, 2.0, 1.0), TrainTechnology("Normal", 160, 1.0, 0.5))
+  val trainTechManager = TechnologyManager[TrainTechnology](technologies)
+
+  // App state init
+  val initialState: AppState = (trainManager, trainTechManager)
+  LazyList.continually(stateEventQueue.take()).scanLeft(initialState)((state, event) =>
+    event(state)
+  ).foreach((appState: AppState) =>
+    println(s"Stations: ${appState._1.trains}")
+  )
