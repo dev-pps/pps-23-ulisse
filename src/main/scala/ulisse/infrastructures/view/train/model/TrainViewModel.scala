a76fd1924ffa30c35f0d2d9330b8bb257c052224
Matteo <matteo.violani@studio.unibo.it>
2025-01-27 15:29:34 +0100

    refactor: improved list view, show errors, deselect train, fix update refresh

--- src/main/scala/ulisse/infrastructures/view/train/model/TrainViewModel.scala
+++ src/main/scala/ulisse/infrastructures/view/train/model/TrainViewModel.scala
@@ -1,5 +1,8 @@
 package ulisse.infrastructures.view.train.model
 
+import ulisse.entities.train.Trains.{Train, TrainTechnology}
+import ulisse.entities.train.Wagons.UseType
+
 object TrainViewModel:
 
   case class TrainData(
@@ -30,3 +33,25 @@ object TrainViewModel:
 
   case class TechType(name: String, maxSpeed: Int, acc: Double, dec: Double)
   case class WagonName(useName: String)
+
+  extension (l: List[Train])
+    def toTrainDatas: List[TrainData] =
+      l.map(t =>
+        TrainData(
+          name = Some(t.name),
+          technologyName = Some(t.techType.name),
+          technologyMaxSpeed = Some(t.techType.maxSpeed),
+          technologyAcc = Some(t.techType.acceleration),
+          technologyDec = Some(t.techType.deceleration),
+          wagonNameType = Some(t.wagon.use.name),
+          wagonCapacity = Some(t.wagon.capacity),
+          wagonCount = Some(t.length)
+        )
+      )
+
+  extension (t: List[TrainTechnology])
+    def toTechType: List[TechType] =
+      t.map(tk => TechType(tk.name, tk.maxSpeed, tk.acceleration, tk.deceleration))
+
+  extension (w: List[UseType])
+    def toWagonNames: List[WagonName] = w.map(w => WagonName(w.name))
