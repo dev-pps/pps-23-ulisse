277c7a2ec6850b816ba32053eb585b6e9777bee0
Manuel Buizo <buizomanuel@gmail.com>
2025-01-28 11:57:55 +0100

    feat: create color palette methods

--- src/main/scala/ulisse/infrastructures/view/components/JColorPalette.scala
+++ src/main/scala/ulisse/infrastructures/view/components/JColorPalette.scala
@@ -6,4 +6,12 @@ object JColorPalette:
 
   def apply(background: Color, click: Color, hover: Color): JPalette = JPalette(background, click, hover)
 
-  case class JPalette(background: Color, click: Color, hover: Color)
+  case class JPalette(background: Color, click: Color, hover: Color):
+    def setAll(newBackground: Color, newClick: Color, newHover: Color): JPalette =
+      copy(background = newBackground, click = newClick, hover = newHover)
+
+    def setBackground(newBackground: Color): JPalette = copy(background = newBackground)
+
+    def setClick(newClick: Color): JPalette = copy(click = newClick)
+
+    def setHover(newHover: Color): JPalette = copy(hover = newHover)
--- src/test/scala/ulisse/infrastructure/view/components/JColorPaletteTest.scala
+++ src/test/scala/ulisse/infrastructure/view/components/JColorPaletteTest.scala
@@ -7,9 +7,39 @@ import ulisse.infrastructures.view.components.JColorPalette
 import java.awt.Color
 
 class JColorPaletteTest extends AnyFlatSpec with Matchers:
+  val paletteColor = JColorPalette(Color.BLACK, Color.RED, Color.BLUE)
 
   "palette color" should "have background, click and hover color" in:
-    val paletteColor = JColorPalette(Color.BLACK, Color.RED, Color.BLUE)
     paletteColor.background must be(Color.BLACK)
     paletteColor.click must be(Color.RED)
     paletteColor.hover must be(Color.BLUE)
+
+  "change background color" should "have new background color" in:
+    val newBackground   = Color.GREEN
+    val newPaletteColor = paletteColor.setBackground(newBackground)
+    newPaletteColor.background must be(newBackground)
+    newPaletteColor.click must be(Color.RED)
+    newPaletteColor.hover must be(Color.BLUE)
+
+  "change click color" should "have new click color" in:
+    val newClick        = Color.GREEN
+    val newPaletteColor = paletteColor.setClick(newClick)
+    newPaletteColor.background must be(Color.BLACK)
+    newPaletteColor.click must be(newClick)
+    newPaletteColor.hover must be(Color.BLUE)
+
+  "change hover color" should "have new hover color" in:
+    val newHover        = Color.GREEN
+    val newPaletteColor = paletteColor.setHover(newHover)
+    newPaletteColor.background must be(Color.BLACK)
+    newPaletteColor.click must be(Color.RED)
+    newPaletteColor.hover must be(newHover)
+
+  "change all colors" should "have new colors" in:
+    val newBackground   = Color.GREEN
+    val newClick        = Color.YELLOW
+    val newHover        = Color.CYAN
+    val newPaletteColor = paletteColor.setAll(newBackground, newClick, newHover)
+    newPaletteColor.background must be(newBackground)
+    newPaletteColor.click must be(newClick)
+    newPaletteColor.hover must be(newHover)
