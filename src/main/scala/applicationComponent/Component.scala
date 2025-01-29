7dc714042ec02a586fb62503269ed33045a94550
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-29 17:00:29 +0100

    feat: add mvc cake pattern with constructor requirements instead of self types

--- src/main/scala/applicationComponent/Component.scala
+++ src/main/scala/applicationComponent/Component.scala
@@ -2,6 +2,8 @@ package applicationComponent
 
 import applicationComponent.ApplicationComponents.{Controller, DriverRequirement}
 import applicationComponent.ApplicationComponents3.Dependency.Application.userFactory
+import applicationComponent.ApplicationComponents4.Application.StationPort
+import applicationComponent.ApplicationComponents4.Application.UseCases.StationManagerImpl
 import applicationComponent.ControllerModule.ControllerImpl
 
 object ApplicationComponents:
@@ -201,3 +203,160 @@ object MVC extends ModelModule.Interface
 
   @main def main(): Unit =
     view.show(1)
+
+object ApplicationComponents4:
+
+  object Application:
+    object StationPort:
+      trait InputProvider:
+        val inputPort: Input
+
+      trait Input:
+        def metodoInput(): Unit
+
+      trait OutputProvider:
+        val outputPort: Output
+
+      trait Output:
+        def metodoOutput(): Unit
+
+    object UseCases:
+      trait StationManagerProvider:
+        val stationManager: StationManager
+
+      trait StationManager extends StationPort.Input:
+        val outputPort: StationPort.Output
+        def metodoManager(): Unit
+
+      final case class StationManagerImpl(outputPort: StationPort.Output) extends StationManager:
+        def metodoInput(): Unit   = println("StationManagerImpl.metodoInput")
+        def metodoManager(): Unit = println("StationManagerImpl.metodoManager")
+
+  object Swing:
+    object StationEditor:
+      trait ViewProvider:
+        val view: StationEditorView
+
+      trait StationEditorView:
+        val controller: StationEditorController
+        def metodoView(): Unit
+
+      final case class StationEditorViewImpl(controller: StationEditorController) extends StationEditorView:
+        def metodoView(): Unit = println("StationEditorViewImpl.metodoView")
+
+      trait ControllerProvider:
+        val controller: StationEditorController
+
+      trait StationEditorController extends Application.StationPort.Output:
+        val inputPort: Application.StationPort.Input
+        val view: StationEditorView
+        def metodoController(): Unit
+
+        final case class StationEditorControllerImpl(inputPort: Application.StationPort.Input, view: StationEditorView)
+            extends StationEditorController:
+          def metodoOutput(): Unit     = println("StationEditorControllerImpl.metodoOutput")
+          def metodoController(): Unit = println("StationEditorControllerImpl.metodoController")
+
+//    final case class StationEditorComponent() extends Application.StationPort.InputProvider with Swing.StationEditor.ViewProvider with Swing.StationEditor.ControllerProvider:
+//      override val inputPort: StationManager = StationManagerImpl()
+//      override val controller: StationEditorController = StationEditorControllerImpl(inputPort, view)
+//      override val view: StationEditorView = StationEditorViewImpl(controller)
+//  @main def main4(): Unit =
+//    println(Component().view.show())
+
+object ApplicationComponents5:
+  trait Model[S <: Model.State]:
+    def state: S
+    def update(f: S => S): Unit
+
+  object Model:
+    trait State
+    def apply[S <: State](initialState: S): Model[S] =
+      new Model[S]:
+        @SuppressWarnings(Array("org.wartremover.warts.Var"))
+        private var _state                   = initialState
+        override def state: S                = _state
+        override def update(f: S => S): Unit = _state = f(_state)
+
+    trait Requirements[S <: State]
+
+    trait Provider[S <: State]:
+      def model: Model[S]
+
+  trait Controller[State <: Model.State]:
+    def state: State
+
+  object Controller:
+    type Factory[V <: View[?], C <: Controller[?], S <: Model.State] = Requirements[V, S] => C
+    trait Requirements[V <: View[?], S <: Model.State] extends Model.Provider[S] with View.Provider[V]
+
+    trait Dependencies[V <: View[S], S <: Model.State](requirements: Requirements[V, S]) extends Controller[S]:
+      protected def view: V         = requirements.view
+      protected def model: Model[S] = requirements.model
+
+    trait Provider[C <: Controller[?]]:
+      def controller: C
+
+  abstract class BaseController[V <: View[S], S <: Model.State](requirements: Controller.Requirements[V, S])
+      extends Controller[S]
+      with Controller.Dependencies(requirements):
+
+    override def state: S = model.state
+
+  class EmptyController[State <: Model.State, V <: View[State]](requirements: Controller.Requirements[V, State])
+      extends BaseController(requirements)
+
+  trait View[State <: Model.State]:
+    def state: State
+    def show(): Unit
+    def hide(): Unit
+    def displayMessage(message: String): Unit
+    def updateState(state: State): Unit = ()
+
+  object View:
+    type Factory[C <: Controller[?], V <: View[?]] = Requirements[C] => V
+    trait Requirements[C <: Controller[?]] extends Controller.Provider[C]
+    trait Dependencies[C <: Controller[?]](requirements: Requirements[C]) extends View[?]:
+      protected def controller: C = requirements.controller
+    trait Provider[V <: View[?]]:
+      def view: V
+
+  abstract class BaseView[State <: Model.State, C <: Controller[State]](requirements: View.Requirements[C])
+      extends View[State]
+      with View.Dependencies(requirements):
+
+    override def state: State = _state
+    @SuppressWarnings(Array("org.wartremover.warts.Var"))
+    private var _state: State = controller.state
+    override def updateState(state: State): Unit =
+      _state = state
+
+  final case class ApplicationState(game: Option[String]) extends Model.State
+
+  object ApplicationState:
+    def apply(): ApplicationState = ApplicationState(Option.empty)
+
+  trait HomeController extends Controller[ApplicationState]
+
+  object HomeController:
+    def apply(requirements: Controller.Requirements[HomeView, ApplicationState]): HomeController =
+      new EmptyController(requirements) with HomeController
+
+  trait HomeView extends View[ApplicationState]
+  case class HomeViewImpl(requirements: View.Requirements[HomeController])
+      extends BaseView[ApplicationState, HomeController](requirements) with HomeView:
+    override def show(): Unit                          = println("HomeViewImpl.show")
+    override def hide(): Unit                          = println("HomeViewImpl.hide")
+    override def displayMessage(message: String): Unit = println("HomeViewImpl.displayMessage")
+
+  case class ApplicationPage[S <: Model.State, C <: Controller[?], V <: View[?]](
+      override val model: Model[S],
+      viewFactory: View.Factory[C, V],
+      controllerFactory: Controller.Factory[V, C, S]
+  ) extends Model.Requirements[S]
+      with View.Requirements[C]
+      with Controller.Requirements[V, S]:
+    override lazy val view: V       = viewFactory(this)
+    override lazy val controller: C = controllerFactory(this)
+
+  val page = ApplicationPage(Model(ApplicationState()), HomeViewImpl.apply, HomeController.apply)
