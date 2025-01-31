4d6416b82a5fb27c2ec81796a3751ac43181c5b8
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-31 18:26:50 +0100

    feat(SideMenu): setup

--- src/main/scala/ulisse/infrastructures/view/components/ComponentUtils.scala
+++ src/main/scala/ulisse/infrastructures/view/components/ComponentUtils.scala
@@ -1,7 +1,22 @@
 package ulisse.infrastructures.view.components
 
+import java.awt.image.BufferedImage
+import java.awt.{BorderLayout, Color}
+import javax.swing.JToggleButton
+import javax.swing.JToggleButton.ToggleButtonModel
+import javax.swing.border.LineBorder
 import scala.swing.event.MouseClicked
-import scala.swing.{Component, Dimension, UIElement}
+import scala.swing.{
+  AbstractButton,
+  ButtonGroup,
+  Component,
+  Dimension,
+  Graphics2D,
+  Publisher,
+  RadioButton,
+  ToggleButton,
+  UIElement
+}
 
 object ComponentUtils:
   extension [E <: UIElement](element: E)
@@ -18,3 +33,25 @@ object ComponentUtils:
         case _: MouseClicked => action()
       }
       component
+
+    def opaque(value: Boolean): C =
+      component.opaque = value
+      component
+
+    def defaultBorder(): C =
+      component.border = new LineBorder(Color.BLACK, 2)
+      component
+
+    def fListenTo(ps: Publisher*): C =
+      component.listenTo(ps*)
+      component
+
+    def makeSelectable(): AbstractButton =
+      new AbstractButton():
+        peer.setModel(new ToggleButtonModel)
+        peer.add(component.peer, BorderLayout.CENTER)
+
+  extension [B <: AbstractButton](button: B)
+    def addToGroup(buttonGroup: ButtonGroup): B =
+      buttonGroup.peer.add(button.peer)
+      button
