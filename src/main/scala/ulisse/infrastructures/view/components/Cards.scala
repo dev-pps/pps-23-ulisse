edb96dc48fccb06d7894ec040c37a417dcf85e9d
Federico Bravetti <federicobravetti802@gmail.com>
2025-01-31 12:57:06 +0100

    feat(Card): add ImageCard

--- src/main/scala/ulisse/infrastructures/view/components/Cards.scala
+++ src/main/scala/ulisse/infrastructures/view/components/Cards.scala
@@ -3,7 +3,10 @@ package ulisse.infrastructures.view.components
 import com.formdev.flatlaf.extras.FlatSVGIcon
 import com.formdev.flatlaf.extras.FlatSVGIcon.ColorFilter
 
-import java.awt.Color
+import java.awt.image.BufferedImage
+import java.awt.{Color, Image}
+import java.io.File
+import javax.imageio.ImageIO
 import javax.swing.border.LineBorder
 import scala.swing.{BoxPanel, Component, Graphics2D, Label, Orientation, Panel, Swing, UIElement}
 
@@ -11,6 +14,7 @@ object Cards:
   trait Card extends Component
 
   object Card:
+    def createImageCard(image: String, text: Option[String]): Card                         = ImageCard(image, text)
     def createSVGCard(svg: String, text: Option[String], color: Color): Card               = SVGCard(svg, color, text)
     def createDrawnCard(icon: (UIElement, Graphics2D) => Unit, text: Option[String]): Card = DrawnCard(icon, text)
 
@@ -20,15 +24,26 @@ object Cards:
       contents += new Label(t)
       contents += Swing.HGlue
 
+  private final case class ImageCard(imageUrl: String, text: Option[String])
+      extends BoxPanel(Orientation.Vertical) with Card:
+    private val image = ImageIO.read(ClassLoader.getSystemResource(imageUrl))
+    contents += new Panel:
+      override def paint(g: Graphics2D): Unit =
+        val size = math.min(peer.getWidth, peer.getHeight) / 2
+        g.drawImage(image, (peer.getWidth - size) / 2, (peer.getHeight - size) / 2, size, size, peer)
+    for dc <- buildDescriptionContainer(text) do contents += dc
+
   private final case class SVGCard(svg: String, color: Color, text: Option[String])
       extends BoxPanel(Orientation.Vertical) with Card:
     border = new LineBorder(color, 2)
+
     contents += new Panel:
       val rawIcon = new FlatSVGIcon(svg)
       rawIcon.setColorFilter(ColorFilter(color => Color.GREEN))
       override def paint(g: Graphics2D): Unit =
-        val icon = rawIcon.derive(size.width / 2, size.height / 2)
-        icon.paintIcon(peer, g, (size.width - icon.getWidth) / 2, (size.height - icon.getHeight) / 2)
+        val size = math.min(peer.getWidth, peer.getHeight) / 2
+        val icon = rawIcon.derive(size, size)
+        icon.paintIcon(peer, g, (peer.getWidth - icon.getWidth) / 2, (peer.getHeight - icon.getHeight) / 2)
     for dc <- buildDescriptionContainer(text) do contents += dc
 
   private final case class DrawnCard(icon: (UIElement, Graphics2D) => Unit, text: Option[String])
@@ -53,5 +68,6 @@ object Cards:
       g.fillRect(center._1 - length / 2, center._2 - thickness / 2, length, thickness)
       g.setColor(oldColor)
 
+    val imageCardExample: Card = Cards.Card.createImageCard("icon/logo.jpg", Some("Logo"))
     val svgCardExample: Card   = Cards.Card.createSVGCard("icon/mapIcon.svg", Some("Map"), Color.RED)
     val drawnCardExample: Card = Cards.Card.createDrawnCard(drawCross(20, 2, Color.BLUE), Some("Cross"))
