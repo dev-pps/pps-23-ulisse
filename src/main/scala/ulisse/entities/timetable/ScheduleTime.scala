b7bd50722797984a188f2b19fe5b252d438a3d60
Matteo <matteo.violani@studio.unibo.it>
2025-02-01 17:21:25 +0100

    feat: new entity ClockTime (TDD), adapting TrainTimetable

---
+++ src/main/scala/ulisse/entities/timetable/ScheduleTime.scala
@@ -0,0 +1,37 @@
+package ulisse.entities.timetable
+
+import ulisse.utils.Times.ClockTime
+import ulisse.utils.Times.FluentDeclaration.h
+
+private type WaitTime = Int
+
+trait ScheduleTime:
+  def arriving: Option[ClockTime]
+  def waitTime: Option[WaitTime]
+  def departure: Option[ClockTime]
+
+object ScheduleTime:
+  def apply(arriving: Option[ClockTime], waitTime: Option[WaitTime], departure: Option[ClockTime]): ScheduleTime =
+    ScheduleTimeImpl(arriving, waitTime, departure)
+
+  private case class ScheduleTimeImpl(
+      arriving: Option[ClockTime],
+      waitTime: Option[WaitTime],
+      departure: Option[ClockTime]
+  ) extends ScheduleTime
+
+  def StartScheduleTime(departure: Option[ClockTime]): ScheduleTime =
+    ScheduleTime(arriving = None, waitTime = None, departure = departure)
+
+  def EndScheduleTime(arriving: Option[ClockTime]): ScheduleTime =
+    ScheduleTime(arriving = arriving, waitTime = None, departure = None)
+
+  def AutoScheduleTime(arriving: Option[ClockTime], waitTime: Option[WaitTime]): ScheduleTime =
+    import ulisse.utils.Times.+
+    import ulisse.utils.Times.FluentDeclaration
+    val departure =
+      for
+        a <- arriving
+        u <- (a ++ h(0).m(waitTime.getOrElse(0))).toOption
+      yield u
+    ScheduleTime(arriving = arriving, waitTime = waitTime, departure = departure)
---
+++ src/main/scala/ulisse/utils/Times.scala
@@ -0,0 +1,55 @@
+package ulisse.utils
+
+import ulisse.utils.Errors.ErrorMessage
+import scala.annotation.targetName
+
+object Times:
+
+  sealed trait ClockTimeErrors
+  final case class InvalidHours()   extends ClockTimeErrors with ErrorMessage("hours not in range [0,24]")
+  final case class InvalidMinutes() extends ClockTimeErrors with ErrorMessage("minutes not in range [0,59]")
+
+  trait ClockTime:
+    def h: Int
+    def m: Int
+
+  object ClockTime:
+    def apply(h: Int, m: Int): Either[ClockTimeErrors, ClockTime] =
+      for
+        h <- ValidationUtils.validateRange(h, 0, 23, InvalidHours())
+        m <- ValidationUtils.validateRange(m, 0, 59, InvalidMinutes())
+      yield ClockTimeImpl(h, m)
+
+    private case class ClockTimeImpl(h: Int, m: Int) extends ClockTime
+
+  object FluentDeclaration:
+
+    case class HoursBuilder(hours: Int)
+    infix def h(h: Int): HoursBuilder = HoursBuilder(h)
+
+    extension (hb: HoursBuilder)
+      def m(minutes: Int): Either[ClockTimeErrors, ClockTime] = ClockTime(hb.hours, minutes)
+
+  extension (time: Either[ClockTimeErrors, ClockTime])
+    @targetName("add")
+    def +(time2: Either[ClockTimeErrors, ClockTime]): Either[ClockTimeErrors, ClockTime] =
+      for
+        t   <- time
+        t2  <- time2
+        sum <- calculateSum(t, t2)
+      yield sum
+
+  extension (time: ClockTime)
+    @targetName("add")
+    def ++(time2: Either[ClockTimeErrors, ClockTime]): Either[ClockTimeErrors, ClockTime] =
+      for
+        t2  <- time2
+        sum <- calculateSum(time, t2)
+      yield sum
+
+  private def calculateSum(t: ClockTime, t2: ClockTime): Either[ClockTimeErrors, ClockTime] =
+    val totalMinutes = t.m + t2.m
+    val extraHours   = totalMinutes / 60
+    val minutes      = totalMinutes              % 60
+    val hours        = (t.h + t2.h + extraHours) % 24
+    ClockTime(hours, minutes)
