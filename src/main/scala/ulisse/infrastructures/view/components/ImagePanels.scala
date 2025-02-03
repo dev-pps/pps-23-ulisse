2b2b04e02ab6a30db9e3becd54a3b66caf8a8e55
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-03 18:22:25 +0100

    refactor(ImagePanels): derive image panels from cards

--- src/main/scala/ulisse/infrastructures/view/components/ImagePanels.scala
+++ src/main/scala/ulisse/infrastructures/view/components/ImagePanels.scala
@@ -5,14 +5,13 @@ import com.formdev.flatlaf.extras.FlatSVGIcon.ColorFilter
 
 import java.awt.Color
 import javax.imageio.ImageIO
-import scala.swing.{Graphics2D, Panel, UIElement}
 
 object ImagePanels:
   trait ImagePanel extends Panel
 
   object ImagePanel:
     def createImagePanel(imagePath: String): ImagePanel                           = ImagePanelImpl(imagePath)
-    def createSVGPanel(svgPath: String, color: Color): SVGPanel                   = SVGPanel(svgPath, color)
+    def createSVGPanel(svgPath: String, color: Color): SVGPanel = SVGPanel(svgPath, color)
     def createDrawnPanel(iconDrawer: (UIElement, Graphics2D) => Unit): ImagePanel = DrawnPanel(iconDrawer)
 
   private final case class ImagePanelImpl(imagePath: String)
@@ -38,11 +37,10 @@ object ImagePanels:
       val size = math.min(peer.getWidth, peer.getHeight)
       val icon = rawIcon.derive(size, size)
       icon.paintIcon(peer, g, (peer.getWidth - icon.getWidth) / 2, (peer.getHeight - icon.getHeight) / 2)
-      super.paintComponent(g)
 
   private final case class DrawnPanel(iconDrawer: (UIElement, Graphics2D) => Unit)
       extends ImagePanel:
-    override def paintComponent(g: Graphics2D): Unit = iconDrawer(this, g)
+    override def paint(g: Graphics2D): Unit = iconDrawer(this, g)
 
   object Example:
     def drawCross(preferredLength: Int, preferredThickness: Int, color: Color)(
