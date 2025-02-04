61f9f3a002b09dcb7fd31e76a336d1fd43fc55c4
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-04 20:43:29 +0100

    feat(MenuCard): align content to the left

--- src/main/scala/ulisse/infrastructures/view/dashboard/SideMenu.scala
+++ src/main/scala/ulisse/infrastructures/view/dashboard/SideMenu.scala
@@ -32,41 +32,42 @@ object SideMenu:
       component.opaque(false).makeSelectable().addToGroup(buttonGroup)
 
     private def position[C <: Component](component: C): Unit =
-      peer.add(component.peer)
+      contents += component
 
     val openMenu: ImagePanel = ImagePanel.createSVGPanel("icon/keyboard_double_arrow_right.svg", Color.BLACK)
       .genericClickReaction(() =>
         SideMenuImpl.this.preferredSize = new Dimension(200, 400)
-        peer.remove(openMenu.peer)
-        peer.add(closeMenu.peer, 0)
-        SideMenuImpl.this.revalidate()
-        SideMenuImpl.this.repaint()
+        openMenu.visible = false
+        closeMenu.visible = true
       )
     val closeMenu: AbstractButton =
       configure(ImagePanel.createSVGPanel("icon/keyboard_double_arrow_left.svg", Color.BLACK)
         .genericClickReaction(() =>
           SideMenuImpl.this.preferredSize = new Dimension(58, 400)
-          peer.remove(closeMenu.peer)
-          peer.add(openMenu.peer, 0)
-          SideMenuImpl.this.revalidate()
-          SideMenuImpl.this.repaint()
+          openMenu.visible = true
+          closeMenu.visible = false
         ))
-    private val imagePanels = List(
-      openMenu,
+    closeMenu.visible = false
+
+    private val menuCards = List(
       MenuCard(ImagePanel.createSVGPanel("icon/simulation.svg", Color.BLACK), "Simulation"),
-      MenuCard(ImagePanel.createSVGPanel("icon/map.svg", Color.BLACK), "Editors").genericClickReaction(() =>
-        println("map")
-      ),
+      MenuCard(ImagePanel.createSVGPanel("icon/map.svg", Color.BLACK), "Editors"),
       MenuCard(ImagePanel.createSVGPanel("icon/train.svg", Color.BLACK), "Trains"),
       MenuCard(ImagePanel.createSVGPanel("icon/settings.svg", Color.BLACK), "Settings")
     ).map(configure)
-    private val (topMenu, bottomMenu) = imagePanels.splitAt(4)
+
+    private val maxCardWidth = menuCards.foldLeft(0)((m, c) => math.max(m,c.preferredSize.width))
+    menuCards.foreach:
+      card => card.fixedSize(maxCardWidth, card.preferredSize.height)
+
+    position(openMenu)
+    private val (topMenu, bottomMenu) = menuCards.splitAt(3)
     topMenu.foreach(position)
     position(Swing.VGlue)
     bottomMenu.foreach(position)
 
 final case class MenuCard(imagePanel: ImagePanel, text: String) extends BoxPanel(Orientation.Horizontal):
-  imagePanel.fixedSize(50, 50)
-  peer.add(imagePanel.peer)
-  peer.add(new Label(text).peer)
+  contents += imagePanel.fixedSize(50, 50)
+  contents += new Label(text)
+  contents += Swing.HGlue
   def toggleLabel(): Unit = peer.getComponent(1).setVisible(!peer.getComponent(1).isVisible)
