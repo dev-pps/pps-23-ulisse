85e5c2d8a8ba3c668bf166465b313e83b77a8c56
Manuel Buizo <buizomanuel@gmail.com>
2025-02-05 09:28:35 +0100

    feat: change architecture and relative tests
    changed disposition and name of packages

--- src/test/scala/ulisse/architecture/HexagonalTest.scala
+++ src/test/scala/ulisse/architecture/HexagonalTest.scala
@@ -1,5 +1,7 @@
 package ulisse.architecture
 
+import com.tngtech.archunit.base.DescribedPredicate
+import com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage
 import com.tngtech.archunit.lang.ArchRule
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition
 import com.tngtech.archunit.library.Architectures
@@ -17,37 +19,49 @@ class HexagonalTest extends AnyFlatSpec:
       .andShould.dependOnClassesThat.resideInAnyPackage(depend: _*)
       .allowEmptyShould(true)
 
-  "no classes of the entities package" should "depends on the applications, infrastructures and userInteractions packages" in:
+  "no classes of the entities package" should "depends on the applications, adapters and infrastructures packages" in:
     val rule = dependenciesRuleOfPackage(Packages.ENTITIES)(
       Packages.APPLICATIONS,
+      Packages.ADAPTERS,
       Packages.INFRASTRUCTURES
     )()
 
     rule.check(IMPORT_ONLY_CLASSES_CREATED)
 
-  "no classes of the applications package" should "depend on the infrastructures and userInteractions packages " +
+  "no classes of the applications package" should "depend on the adapters and infrastructures packages " +
     "and should depend on entities package" in:
-      val rule = dependenciesRuleOfPackage(Packages.APPLICATIONS)(Packages.INFRASTRUCTURES)(
+      val rule = dependenciesRuleOfPackage(Packages.APPLICATIONS)(Packages.ADAPTERS, Packages.INFRASTRUCTURES)(
         Packages.ENTITIES
       )
 
       rule.check(IMPORT_ONLY_CLASSES_CREATED)
 
-  "no classes of the infrastructures package" should "depend on the entities and userInteractions packages " +
-    "and should depend on applications package" in:
-      val rule = dependenciesRuleOfPackage(Packages.INFRASTRUCTURES)()(
-        Packages.APPLICATIONS,
-        Packages.ENTITIES
-      )
+  "classes of the adapters package" should "depend on the entities, applications and infrastructures packages" in:
+    val rule = dependenciesRuleOfPackage(Packages.ADAPTERS)()(
+      Packages.ENTITIES,
+      Packages.APPLICATIONS,
+      Packages.INFRASTRUCTURES
+    )
 
-      rule.check(IMPORT_ONLY_CLASSES_CREATED)
+    rule.check(IMPORT_ONLY_CLASSES_CREATED)
 
-  "hexagonal architecture" should "be entities -> applications -> infrastructures/userInteractions" in:
+  "classes of the infrastructures package" should "depend on the entities, applications and adapters packages" in:
+    val rule = dependenciesRuleOfPackage(Packages.INFRASTRUCTURES)()(
+      Packages.ENTITIES,
+      Packages.APPLICATIONS,
+      Packages.ADAPTERS
+    )
+
+    rule.check(IMPORT_ONLY_CLASSES_CREATED)
+
+  "hexagonal architecture" should "be entities -> applications -> adapters" in:
     val rule = Architectures
       .onionArchitecture
       .domainModels(Packages.ENTITIES)
       .applicationServices(Packages.APPLICATIONS)
-      .adapter("infrastructures", Packages.INFRASTRUCTURES)
+      .adapter("adapters", Packages.ADAPTERS)
+      .ignoreDependency(DescribedPredicate.alwaysTrue(), resideInAPackage(Packages.INFRASTRUCTURES))
+      .ignoreDependency(resideInAPackage(Packages.INFRASTRUCTURES), DescribedPredicate.alwaysTrue())
       .allowEmptyShould(true)
 
     rule.check(IMPORT_ONLY_CLASSES_CREATED)
