38e568a5dd56b1b69aa06cdbde7de93b1654e648
Manuel Buizo <buizomanuel@gmail.com>
2025-02-07 16:05:00 +0100

    feat: create map items colletion to mapPanel

--- src/main/scala/ulisse/infrastructures/view/components/JImages.scala
+++ src/main/scala/ulisse/infrastructures/view/components/JImages.scala
@@ -27,11 +27,19 @@ object JImages:
     def drawSilhouette(image: JImage, scale: Float, color: Color, observer: ImageObserver): Unit =
       image.drawSilhouette(g, scale, color, observer)
 
+  extension (point: java.awt.Point)
+    def hasCollided(item: JImage): Boolean =
+      val x          = point.x
+      val y          = point.y
+      val itemX      = item.center.width
+      val itemY      = item.center.height
+      val itemWidth  = item.dimension.width
+      val itemHeight = item.dimension.height
+      x >= itemX && x <= itemX + itemWidth && y >= itemY && y <= itemY + itemHeight
+
   trait JImage:
     val center: Position
     val dimension: Dimension
-
-    def createSilhouette(color: Color, observer: ImageObserver): BufferedImage
 
     def draw(g: Graphics2D, observer: ImageObserver): Unit
     def drawSilhouette(g: Graphics2D, scale: Float, color: Color, observer: ImageObserver): Unit
@@ -43,30 +51,27 @@ object JImages:
       create(path, position, dimension)
 
     sealed private case class JImageImpl(path: String, position: Position, dimension: Dimension) extends JImage:
-      private val url   = ClassLoader.getSystemResource(path)
-      private val image = ImageIO.read(url)
+      private val url        = ClassLoader.getSystemResource(path)
+      private val image      = ImageIO.read(url)
+      private val silhouette = BufferedImage(image.getWidth, image.getHeight, BufferedImage.TYPE_INT_ARGB)
 
       override val center: Position =
         JStyler.Dimension2D(position.width - (dimension.width / 2), position.height - (dimension.height / 2))
 
-      override def createSilhouette(color: Color, observer: ImageObserver): BufferedImage =
-        val silhouette    = new BufferedImage(image.getWidth, image.getHeight, BufferedImage.TYPE_INT_ARGB)
-        val imageGraphics = silhouette.createGraphics()
-
-        imageGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        imageGraphics.drawImage(image, 0, 0, observer)
-        imageGraphics.setComposite(java.awt.AlphaComposite.SrcAtop)
-        imageGraphics.setColor(color)
-        imageGraphics.fillRect(0, 0, image.getWidth, image.getHeight)
-        imageGraphics.dispose()
-
-        silhouette
+      private def setupSilhouette(color: Color, observer: ImageObserver): Unit =
+        val graphics = silhouette.createGraphics()
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        graphics.drawImage(image, 0, 0, observer)
+        graphics.setComposite(java.awt.AlphaComposite.SrcAtop)
+        graphics.setColor(color)
+        graphics.fillRect(0, 0, image.getWidth, image.getHeight)
+        graphics.dispose()
 
       override def draw(g: Graphics2D, observer: ImageObserver): Unit =
         g.drawImage(image, center.width, center.height, dimension.width, dimension.height, observer)
 
       override def drawSilhouette(g: Graphics2D, scale: Float, color: Color, observer: ImageObserver): Unit =
-        val silhouette = createSilhouette(color, observer)
+        setupSilhouette(color, observer)
 
         val scaleSize     = createDimension((dimension.width * scale).toInt, (dimension.height * scale).toInt)
         val evenSize      = scaleSize.plus(createDimension(scaleSize.width % 2, scaleSize.height % 2))
