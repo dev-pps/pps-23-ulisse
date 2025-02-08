4672463fb97c7ebba84641b5f0f7a9284ac1632c
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-08 18:00:53 +0100

    chore(SimulationPage): build simulation input side

--- src/main/scala/ulisse/adapters/output/SimulationNotificationAdapter.scala
+++ src/main/scala/ulisse/adapters/output/SimulationNotificationAdapter.scala
@@ -5,7 +5,7 @@ import ulisse.entities.Coordinates.Coordinate
 import ulisse.entities.station.Station
 import ulisse.infrastructures.view.simulation.SimulationPage
 
-final case class SimulationPageAdapter[N: Numeric, C <: Coordinate[N], S <: Station[N, C]](
+final case class SimulationNotificationAdapter[N: Numeric, C <: Coordinate[N], S <: Station[N, C]](
     simulationPage: SimulationPage
 ) extends SimulationPorts.Output:
   override def stepNotification(): Unit = simulationPage.updateData()
