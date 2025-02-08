724e71b5608b10da94b674320c3e46d99cbd1f00
Manuel Buizo <buizomanuel@gmail.com>
2025-02-08 17:37:53 +0100

    feat: init map service

--- src/main/scala/ulisse/infrastructures/view/map/EditorsView.scala
+++ src/main/scala/ulisse/infrastructures/view/map/EditorsView.scala
@@ -1,7 +1,8 @@
 package ulisse.infrastructures.view.map
 
+import ulisse.infrastructures.view.common.CentralController
 import ulisse.infrastructures.view.components.LayeredContainers.JLayeredPane
-import ulisse.infrastructures.view.form.CentralController
+import ulisse.infrastructures.view.map.MapPanel
 import ulisse.infrastructures.view.menu.Menu
 
 import java.awt.BorderLayout
@@ -9,8 +10,8 @@ import scala.swing.{BorderPanel, Component, Dimension}
 import scala.swing.BorderPanel.Position.{Center, East}
 
 final case class EditorsView() extends JLayeredPane:
-  private val mapPark: MapPanel = MapPanel.empty()
-  private val mapController     = CentralController.createMap()
+  private val mapPark       = MapPanel.empty()
+  private val mapController = CentralController.createMap()
 
   mainPane.peer.add(mapPark.peer)
   glassPane.peer.add(mapController.component.peer, BorderLayout.EAST)
