6ae83f8b46cd6c331a4a668f24c1d9cef64a11b9
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-10 12:48:30 +0100

    chore(SimulationService): setup service

--- src/main/scala/ulisse/applications/useCases/SimulationService.scala
+++ src/main/scala/ulisse/applications/useCases/SimulationService.scala
@@ -1,8 +1,25 @@
 package ulisse.applications.useCases
 
+import ulisse.applications.AppState
 import ulisse.applications.ports.SimulationPorts
+import ulisse.entities.Coordinates.Coordinate
+import ulisse.entities.station.Station
 
-final case class SimulationService() extends SimulationPorts.Input:
-  override def start(): Unit = println("Simulation started")
-  override def stop(): Unit  = println("Simulation stopped")
-  override def reset(): Unit = println("Simulation reset")
+import java.util.concurrent.LinkedBlockingQueue
+import scala.concurrent.{Future, Promise}
+
+final case class SimulationService[N: Numeric, C <: Coordinate[N], S <: Station[N, C]](
+    eventQueue: LinkedBlockingQueue[AppState[N, C, S] => AppState[N, C, S]]
+) extends SimulationPorts.Input:
+  override def start(): Future[Unit] =
+    val p = Promise[Unit]()
+    p.success(println("Simulation started"))
+    p.future
+  override def stop(): Future[Unit] =
+    val p = Promise[Unit]()
+    p.success(println("Simulation stopped"))
+    p.future
+  override def reset(): Future[Unit] =
+    val p = Promise[Unit]()
+    p.success(println("Simulation reset"))
+    p.future
