a3298475027dda700979034a5590d434645bd7ae
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-10 13:57:44 +0100

    feat(SimulationManager): working simulation loop

--- src/main/scala/ulisse/applications/managers/SimulationManager.scala
+++ src/main/scala/ulisse/applications/managers/SimulationManager.scala
@@ -3,7 +3,6 @@ package ulisse.applications.managers
 import ulisse.applications.SimulationState
 
 import java.util.concurrent.LinkedBlockingQueue
-import scala.concurrent.{Future, Promise}
 
 trait SimulationManager:
   val running: Boolean
@@ -11,12 +10,27 @@ trait SimulationManager:
   def start(): SimulationManager
   def stop(): SimulationManager
   def reset(): SimulationManager
+  def doStep(): SimulationManager
 
 object SimulationManager:
-  def apply(simulationEvents: LinkedBlockingQueue[SimulationState => SimulationState]): SimulationManager = SimulationManagerImpl(simulationEvents, false, 0)
-  private case class SimulationManagerImpl(simulationEvents: LinkedBlockingQueue[SimulationState => SimulationState], running: Boolean, step: Int) extends SimulationManager:
-    override def start(): SimulationManager = copy(running = true)
-    override def stop(): SimulationManager  = copy(running = false)
+  def apply(simulationEvents: LinkedBlockingQueue[SimulationState => SimulationState]): SimulationManager =
+    SimulationManagerImpl(simulationEvents, false, 0)
+  private case class SimulationManagerImpl(
+      simulationEvents: LinkedBlockingQueue[SimulationState => SimulationState],
+      running: Boolean,
+      step: Int
+  ) extends SimulationManager:
+    override def start(): SimulationManager =
+      doStep(); copy(running = true)
+    override def stop(): SimulationManager =
+      copy(running = false)
     override def reset(): SimulationManager = copy(running = false, step = 0)
-    private def doStep(): Unit =
-      simula
+
+    override def doStep(): SimulationManager =
+      simulationEvents.offer((state: SimulationState) => {
+        if state.simulationManager.running then
+          state.copy(simulationManager = state.simulationManager.doStep())
+        else
+          state
+      })
+      copy(step = step + 1)
