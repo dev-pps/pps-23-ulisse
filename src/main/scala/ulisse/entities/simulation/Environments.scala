db6c5d07c04334b96a566f8987d606082fb71c92
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-11 22:30:24 +0100

    refactor(Simulation): move classes

--- src/main/scala/ulisse/entities/simulation/Environments.scala
+++ src/main/scala/ulisse/entities/simulation/Environments.scala
@@ -1,8 +1,24 @@
 package ulisse.entities.simulation
 
+import ulisse.entities.simulation.Agents.SimulationAgent
 import ulisse.entities.station.Station
 
 object Environments:
-  final case class SimulationEnvironment(
-      stationMap: Seq[Station]
-  )
+  trait SimulationEnvironment:
+    def stations: Seq[Station]
+    def agents: Seq[SimulationAgent]
+    def stations_=(newStations: Seq[Station]): SimulationEnvironment
+    def agents_=(newAgents: Seq[SimulationAgent]): SimulationEnvironment
+
+  object SimulationEnvironment:
+    def apply(): SimulationEnvironment =
+      SimulationEnvironmentImpl(Seq[Station](), Seq[SimulationAgent]())
+
+    private final case class SimulationEnvironmentImpl(
+        stations: Seq[Station],
+        agents: Seq[SimulationAgent]
+    ) extends SimulationEnvironment:
+      def stations_=(newStations: Seq[Station]): SimulationEnvironment =
+        copy(stations = newStations)
+      def agents_=(newAgents: Seq[SimulationAgent]): SimulationEnvironment =
+        copy(agents = newAgents)
