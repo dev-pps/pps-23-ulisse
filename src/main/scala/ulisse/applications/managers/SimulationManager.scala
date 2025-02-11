01dfa99359eebf79a4f998013f0bc1ba07905760
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-11 19:26:35 +0100

    chore(SimulationManager): delta time idea

--- src/main/scala/ulisse/applications/managers/SimulationManager.scala
+++ src/main/scala/ulisse/applications/managers/SimulationManager.scala
@@ -2,6 +2,7 @@ package ulisse.applications.managers
 
 import ulisse.applications.SimulationState
 import ulisse.applications.ports.SimulationPorts
+import ulisse.applications.useCases.{SimulationAgent, SimulationData}
 import ulisse.entities.simulation.Environments.SimulationEnvironment
 import ulisse.entities.station.Station
 
@@ -18,17 +19,35 @@ trait SimulationManager:
 object SimulationManager:
   def apply(notificationService: SimulationPorts.Output): SimulationManager =
     SimulationManagerImpl(false, 0, SimulationEnvironment(Seq[Station]()), notificationService)
+    SimulationManagerImpl(
+      false,
+      0,
+      SimulationEnvironment(Seq[Station]()),
+      notificationService,
+      0.0,
+      0,
+      SimulationAgent(1, 0, 5)
+    )
   private case class SimulationManagerImpl(
       running: Boolean,
       step: Int,
       environment: SimulationEnvironment,
-      notificationService: SimulationPorts.Output
+      notificationService: SimulationPorts.Output,
+      secondElapsed: Double,
+      lastUpdate: Long,
+      simulationAgent: SimulationAgent
   ) extends SimulationManager:
     override def start(environment: SimulationEnvironment): SimulationManager =
-      copy(running = true, environment = environment)
-    override def stop(): SimulationManager  = copy(running = false)
-    override def reset(): SimulationManager = copy(running = false, step = 0)
+      copy(running = true, environment = environment, lastUpdate = System.currentTimeMillis())
+    override def stop(): SimulationManager = copy(running = false)
+    override def reset(): SimulationManager =
+      copy(running = false, step = 0, secondElapsed = 0, lastUpdate = 0, simulationAgent = SimulationAgent(1, 0, 5))
     override def doStep(): SimulationManager =
-      val updatedStep = step + 1
-      notificationService.stepNotification(updatedStep)
-      copy(step = updatedStep)
+      val updatedStep          = step + 1
+      val lu                   = System.currentTimeMillis()
+      val deltaElapsed         = lu - lastUpdate
+      val updatedAgent         = simulationAgent.update(deltaElapsed / 1000.0)
+      val updatedSecondElapsed = secondElapsed + deltaElapsed / 1000.0
+      println(s"[SimulationManager]: Step $lastUpdate, $deltaElapsed")
+      notificationService.stepNotification(SimulationData(updatedStep, updatedSecondElapsed, updatedAgent))
+      copy(step = updatedStep, secondElapsed = updatedSecondElapsed, lastUpdate = lu, simulationAgent = updatedAgent)
