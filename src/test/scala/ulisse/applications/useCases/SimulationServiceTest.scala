2538e364bc08a58be24b75c2c6a7acc86fc9344b
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-12 18:39:44 +0100

    chore(SimulationService): take control of simulation event processing

--- src/test/scala/ulisse/applications/useCases/SimulationServiceTest.scala
+++ src/test/scala/ulisse/applications/useCases/SimulationServiceTest.scala
@@ -4,8 +4,8 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar.mock
 import ulisse.Runner.runAll
-import ulisse.applications.AppState
-import ulisse.applications.managers.StationManager
+import ulisse.applications.{AppState, SimulationState}
+import ulisse.applications.managers.{SimulationManager, StationManager}
 import ulisse.applications.ports.{SimulationPorts, UtilityPorts}
 import ulisse.entities.simulation.Simulations.EngineState
 import ulisse.entities.station.Station
@@ -17,12 +17,18 @@ import scala.concurrent.duration.Duration
 class SimulationServiceTest extends AnyWordSpec with Matchers:
 
   private type S = Station[?]
-  private val notificationService = mock[SimulationPorts.Output]
-  private val timeProviderService = mock[UtilityPorts.Output.TimeProviderPort]
-  private val initialState        = AppState[S](StationManager.createCheckedStationManager())
-  private val eventQueue          = LinkedBlockingQueue[AppState[S] => AppState[S]]()
-  private val simulationService   = SimulationService(eventQueue, notificationService, timeProviderService)
-  private def updateState()       = runAll(initialState, eventQueue)
+  private val notificationService     = mock[SimulationPorts.Output]
+  private val timeProviderService     = mock[UtilityPorts.Output.TimeProviderPort]
+  private val initialState            = AppState[S](StationManager.createCheckedStationManager())
+  private val eventQueue              = LinkedBlockingQueue[AppState[S] => AppState[S]]()
+  private val initialSimulationState  = SimulationState(SimulationManager.empty(timeProviderService))
+  private val simulationQueue         = LinkedBlockingQueue[SimulationState => SimulationState]()
+  private val simulationService       = SimulationService(eventQueue, simulationQueue, notificationService)
+  private def updateAppState()        = runAll(initialState, eventQueue)
+  private def updateSimulationState() = runAll(initialSimulationState, simulationQueue)
+  private def updateState() =
+    updateAppState()
+    updateSimulationState()
 
   "SimulationService" should:
     "start simulation" in:
