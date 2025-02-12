98717937536513d25a472e2963d44492cfdcb1cd
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-12 16:15:47 +0100

    feat(TimeProvider): setup time provider system

---
+++ src/main/scala/ulisse/infrastructures/commons/TimeProviders.scala
@@ -0,0 +1,11 @@
+package ulisse.infrastructures.commons
+
+object TimeProviders:
+  trait TimeProvider:
+    def currentTimeMillis: Long
+
+    object TimeProvider:
+      def systemTimeProvider(): TimeProvider = SystemTimeProvider()
+
+      private final case class SystemTimeProvider() extends TimeProvider:
+        override def currentTimeMillis: Long = System.currentTimeMillis()
