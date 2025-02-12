798b7ba204a67a0eccd1b567fdfdc5418769f499
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-12 17:16:18 +0100

    chore(SimulationServiceTest): setup basic testing

--- src/main/scala/ulisse/adapters/input/SimulationPageAdapter.scala
+++ src/main/scala/ulisse/adapters/input/SimulationPageAdapter.scala
@@ -1,10 +1,11 @@
 package ulisse.adapters.input
 
 import ulisse.applications.ports.SimulationPorts
+import ulisse.entities.simulation.Simulations.EngineState
 
 import scala.concurrent.Future
 
 final case class SimulationPageAdapter(simulationPort: SimulationPorts.Input):
-  def start(): Future[Unit] = simulationPort.start()
-  def stop(): Future[Unit]  = simulationPort.stop()
-  def reset(): Future[Unit] = simulationPort.reset()
+  def start(): Future[EngineState] = simulationPort.start()
+  def stop(): Future[EngineState]  = simulationPort.stop()
+  def reset(): Future[EngineState] = simulationPort.reset()
