6bc132791e50fc06cc4d63d75f8389867040c7f2
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-13 04:32:40 +0100

    chore(SimulationManagerTest): add little dsl for comparison and enanche testing

--- src/test/scala/ulisse/applications/managers/SimulationManagerTest.scala
+++ src/test/scala/ulisse/applications/managers/SimulationManagerTest.scala
@@ -3,10 +3,15 @@ package ulisse.applications.managers
 import org.mockito.Mockito.when
 import org.mockito.invocation.InvocationOnMock
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.should.Matchers.shouldBe
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar.mock
+import ulisse.Utils.MatchersUtils.shouldBeBoolean
 import ulisse.applications.ports.UtilityPorts
 import ulisse.entities.simulation.Environments.SimulationEnvironment
+import ulisse.entities.simulation.Simulations.EngineState
+import ulisse.entities.simulation.Simulations.EngineState.{ComparisonBuilder, Field}
+import EngineState.*
 
 class SimulationManagerTest extends AnyWordSpec with Matchers:
 
@@ -39,8 +44,14 @@ class SimulationManagerTest extends AnyWordSpec with Matchers:
       SimulationManager.emptyBatchManager(timeProvider).start().reset().engineState.running shouldBe false
       SimulationManager.emptyBatchManager(timeProvider).start().stop().reset().engineState.running shouldBe false
 
-    "preserve state on pause" in:
-      val manager = SimulationManager.emptyBatchManager(timeProvider).start().doStep()
+    "preserve state on stop" in:
+      val manager       = SimulationManager.emptyBatchManager(timeProvider).start().doStep()
       val pausedManager = manager.stop()
-      pausedManager.engineState shouldBe manager.engineState
+      pausedManager.engineState compareTo manager.engineState ignoring Field.Running shouldBeBoolean true
       pausedManager.simulationData shouldBe manager.simulationData
+
+    "clear state on reset" in:
+      val manager      = SimulationManager.emptyBatchManager(timeProvider)
+      val resetManager = manager.start().doStep().reset()
+      resetManager.engineState shouldBe manager.engineState
+      resetManager.simulationData shouldBe manager.simulationData
