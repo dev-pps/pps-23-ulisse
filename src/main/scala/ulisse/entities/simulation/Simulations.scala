ecf36925d2935b06b3e36e3fb569268c627ec2b4
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-13 04:32:40 +0100

    chore(SimulationManagerTest): add little dsl for comparison and enanche testing

--- src/main/scala/ulisse/entities/simulation/Simulations.scala
+++ src/main/scala/ulisse/entities/simulation/Simulations.scala
@@ -6,7 +6,38 @@ import ulisse.entities.simulation.Environments.SimulationEnvironment
 object Simulations:
   object EngineState:
     def empty(): EngineState = EngineState(false, None, None, 0, 0)
+
+    enum Field:
+      case Running, CyclesPerSecond, LastUpdate, LastDelta, ElapsedCycleTime
+
+    given Conversion[ComparisonBuilder, Boolean] with
+      def apply(builder: ComparisonBuilder): Boolean =
+        builder.compare
+
+    case class ComparisonBuilder(engines: List[EngineState], ignoredFields: Seq[Field]):
+      def ignoring(fields: Field*): ComparisonBuilder =
+        copy(ignoredFields = ignoredFields ++ fields)
+
+      def andTo(nextEngine: EngineState): ComparisonBuilder =
+        copy(engines = nextEngine +: engines)
+
+      def compare: Boolean =
+        val fieldsToCompare = Field.values.filterNot(ignoredFields.contains)
+        engines match
+          case Nil => true
+          case firstEngine :: tail => tail.forall { otherEngine =>
+              fieldsToCompare.forall {
+                case Field.Running          => firstEngine.running == otherEngine.running
+                case Field.CyclesPerSecond  => firstEngine.cyclesPerSecond == otherEngine.cyclesPerSecond
+                case Field.LastUpdate       => firstEngine.lastUpdate == otherEngine.lastUpdate
+                case Field.LastDelta        => firstEngine.lastDelta == otherEngine.lastDelta
+                case Field.ElapsedCycleTime => firstEngine.elapsedCycleTime == otherEngine.elapsedCycleTime
+              }
+            }
+
     extension (engineState: EngineState)
+      def compareTo(otherEngineState: EngineState): ComparisonBuilder =
+        ComparisonBuilder(List(engineState, otherEngineState), Seq[Field]())
       def decreaseElapsedCycleTimeBy(delta: Double): EngineState =
         engineState.copy(elapsedCycleTime = engineState.elapsedCycleTime - delta)
 
@@ -17,6 +48,7 @@ object Simulations:
       lastDelta: Double,
       elapsedCycleTime: Double
   ):
+    def reset(): EngineState = EngineState(false, cyclesPerSecond, None, 0, 0)
     def update(currentUpdate: Double): EngineState =
       lastUpdate match
         case Some(lastUpdate) =>
@@ -41,5 +73,5 @@ object Simulations:
       secondElapsed: Double,
       simulationEnvironment: SimulationEnvironment
   ):
-    def clear(): SimulationData =
+    def reset(): SimulationData =
       copy(step = 0, secondElapsed = 0, simulationEnvironment = SimulationEnvironment.empty())
