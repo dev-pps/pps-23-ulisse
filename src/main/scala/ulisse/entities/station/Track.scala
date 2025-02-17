b6fccfe351b8f65b6e634a6a6ca30ca0a1175ac6
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-17 17:23:09 +0100

    chore(Track): improve implementation

--- src/main/scala/ulisse/entities/station/Track.scala
+++ src/main/scala/ulisse/entities/station/Track.scala
@@ -6,19 +6,27 @@ import ulisse.entities.train.Trains.Train
 import ulisse.utils.Errors.BaseError
 import ulisse.utils.ValidationUtils.validatePositive
 
+/** Defines a track in a station. */
 trait Track:
   val platform: Int
   val train: Option[Train]
 
+/** Factory for [[Track]] instances. */
 object Track:
 
-  def apply(platformNumber: Int): Either[NonEmptyChain[Error], Track] =
-    validatePositive(platformNumber, Error.InvalidPlatformNumber).toValidatedNec.toEither.map(TrackImpl(_, None))
+  /** Creates a `Track` instance. If platform number is not positive it is set to 1 */
+  def apply(platformNumber: Int): Track = TrackImpl(math.max(1, platformNumber), None)
 
+  /** Creates a `Track` instance with validation. If platform number is not positive an error is returned */
+  def createCheckedTrack(platformNumber: Int): Either[NonEmptyChain[Error], Track] =
+    validatePositive(platformNumber, Error.InvalidPlatformNumber).toValidatedNec.toEither.map(Track(_))
+
+  /** Creates a List of `Track` instance. If the specified numberOfTracks is not positive an empty List is returned */
   def generateSequentialTracks(numberOfTracks: Int): List[Track] =
     val step: Int => Int = _ + 1
     List.tabulate(numberOfTracks)(i => TrackImpl(step(i), None))
 
+  /** Represents errors that can occur during `Track` creation. */
   enum Error extends BaseError:
     case InvalidPlatformNumber
 
