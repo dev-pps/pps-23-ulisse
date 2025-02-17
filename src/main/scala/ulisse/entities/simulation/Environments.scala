7c1335429b2a2dbe85055249268a9f4ee65ff20f
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-17 03:21:36 +0100

    chore(SimulationEnvironment): start setup step update

--- src/main/scala/ulisse/entities/simulation/Environments.scala
+++ src/main/scala/ulisse/entities/simulation/Environments.scala
@@ -37,11 +37,32 @@ object Environments:
         val stepActions = agents.map(a => a -> a.doStep(dt, this)).toMap
         stepActions.foldLeft(this.copy(agents = Seq())) {
           case (env, (agent: TrainAgent, Actions.MoveBy(d))) =>
-            env.copy(agents = agents ++ Seq(agent.updateTravelDistance(d)))
+            if agent.travelDistance > 0 then
+              findRoute(agent).map(route =>
+                if agent.travelDistance + d >= route.length then
+                  env.copy(agents = agents ++ Seq(agent.travelDistance = 0)).arriveToDestination(route, agent)
+                else
+                  env.copy(agents = agents ++ Seq(agent.updateTravelDistance(d)))
+              ).getOrElse(env)
+            else
+              env.copy(agents = agents ++ Seq(agent.updateTravelDistance(d)))
           case (env, (agent, _)) => env.copy(agents = agents ++ Seq(agent))
         }
       private def findRoute(agent: TrainAgent): Option[RouteEnvironmentElement] =
-        routes.find(_.id == agent.routeId)
+        routes.find(_.trains.exists(_.contains(agent)))
+      private def findStation(agent: TrainAgent): Option[StationEnvironmentElement] =
+        stations.find(_.tracks.contains(agent))
+//      private def leaveStation(station: StationEnvironmentElement, train: TrainAgent): SimulationEnvironment =
+//        this.copy(stations = stations.map(s => if s == station then s.updateTrack(station.tracks.find(_.train.contains(train)).get, None) else s))
+      private def arriveToDestination(route: Route, train: TrainAgent): SimulationEnvironmentImpl =
+        this.copy(stations =
+          stations.map(s =>
+            if s.coordinate == route.arrival.coordinate then
+              s.firstAvailableTrack.map(track => s.updateTrack(track, Some(train))).getOrElse(s)
+            else s
+          )
+        )
+
       def stations_=(newStations: Seq[StationEnvironmentElement]): SimulationEnvironment =
         copy(stations = newStations)
       def routes_=(newRoutes: Seq[RouteEnvironmentElement]): SimulationEnvironment =
