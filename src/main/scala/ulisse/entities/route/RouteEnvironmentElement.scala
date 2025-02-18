87eaba4837de1e00eb32d036714140dcf75dadaa
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-18 17:47:37 +0100

    chore(StationEnvironmentElement): make station arrival and departure use option

--- src/main/scala/ulisse/entities/route/RouteEnvironmentElement.scala
+++ src/main/scala/ulisse/entities/route/RouteEnvironmentElement.scala
@@ -2,6 +2,7 @@ package ulisse.entities.route
 
 import ulisse.entities.route.Routes.Route
 import ulisse.entities.simulation.Environments.EnvironmentElement
+import ulisse.entities.station.StationEnvironmentElement
 import ulisse.entities.train.Trains.Train
 
 trait RouteEnvironmentElement extends Route with EnvironmentElement:
@@ -10,5 +11,12 @@ trait RouteEnvironmentElement extends Route with EnvironmentElement:
 object RouteEnvironmentElement:
 
   extension (train: Train)
+    def take(route: RouteEnvironmentElement): Option[RouteEnvironmentElement] =
+      route.firstAvailableTrack.map(track => route.updateTrack(track, Some(train)))
+      station.firstAvailableTrack.map(track => station.updateTrack(track, Some(train)))
+
+    def leave(route: RouteEnvironmentElement): Option[RouteEnvironmentElement] =
+      station.tracks.find(_.train.contains(train)).map(track => station.updateTrack(track, None))
+      
     def findInRoutes(routes: Seq[RouteEnvironmentElement]): Option[RouteEnvironmentElement] =
       routes.find(_.trains.exists(_.contains(train)))
