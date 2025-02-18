65cba25c686611e408653fff2f5ffa1abd98ab0a
Matteo <matteo.violani@studio.unibo.it>
2025-02-18 20:41:05 +0100

    docs: doc comments in Timetables

--- src/main/scala/ulisse/entities/timetable/Timetables.scala
+++ src/main/scala/ulisse/entities/timetable/Timetables.scala
@@ -12,36 +12,52 @@ import scala.collection.immutable.ListMap
 /** Object containing traits and utility method for creation and use of TrainTimetable */
 object Timetables:
 
-  type Length           = Double
+  private type Length   = Double
   private type Time     = ScheduleTime
   private type WaitTime = Int
 
+  /** Rail info `length` and [[TypeRoute]] used to estimate `ScheduleTime` of station. */
   case class RailInfo(length: Length, typeRoute: TypeRoute)
 
   extension (i: Int)
     def toWaitTime: WaitTime = i
 
   extension (t: TrainTimetable)
+    /** Returns list of [[Station]] where train stops */
     def stopStations: List[Station] = t.table.filter(_._2.waitTime.nonEmpty).keys.toList
+
+    /** Returns list of [[Station]] where train just transit (doesn't stop) */
     def transitStations: List[Station] =
       t.table.filter(_._2.waitTime.isEmpty).keys.toList.filterNot(s =>
         s.equals(t.arrivingStation) || s.equals(t.startStation)
       )
+
+    /** Returns list of nearest stations pair */
     def routes: List[(Station, Station)] = t.table.keys.zip(t.table.keys.drop(1)).toList
 
+  /** Basic timetable */
   trait Timetable:
     def train: Train
     def startStation: Station
     def departureTime: ClockTime
     def table: ListMap[Station, Time]
 
+  /** Complete timetable containing arriving station and time */
   trait TrainTimetable extends Timetable:
     def arrivingStation: Station
     def arrivingTime: Option[ClockTime]
 
+  /** ETA estimator strategy.
+    * Method `ETA` requires previous station [[Time]], [[RailInfo]] and [[Train]] in order
+    * to calculate Estimated Time of Arrival.
+    */
   trait TimeEstimator:
+    /** Return optionally arrival ClockTime to travel rail length ([[railInfo.length]]) */
     def ETA(lastTime: Option[Time], railInfo: RailInfo, train: Train): Option[ClockTime]
 
+  /** Default implementation of [[TimeEstimator]]
+    * It uses minimum speed between train and rail ones; it doesn't consider train's acceleration and deceleration specs
+    */
   private object UnrealTimeEstimator extends TimeEstimator:
     def ETA(lastTime: Option[Time], railInfo: RailInfo, train: Train): Option[ClockTime] =
       for
@@ -51,12 +67,15 @@ object Timetables:
         arrivingTime    <- h(travelStartTime.h).m(travelStartTime.m + travelMinutes.toInt).toOption
       yield arrivingTime
 
-  /** Default time estimator just considers distance between last station and the given one and train speed. No
-    * acceleration and acceleration are considered.
+  /** Default time estimator just considers given distance between stations ([[RailInfo.length]]),
+    * rail technology speed ([[RailInfo.typeRoute.technology]]) and train speed.
+    * No acceleration and acceleration are considered.
     */
   given defaultTimeEstimator: TimeEstimator = UnrealTimeEstimator
 
-  /** Train timetable to be defined */
+  /** Partial train timetable to be defined.
+    * Methods allows to define station where train stops, transit or arrives.
+    */
   trait PartialTimetable extends Timetable:
     def stopsIn(station: Station, waitTime: WaitTime)(railInfo: RailInfo): PartialTimetable
     def transitIn(station: Station)(railInfo: RailInfo): PartialTimetable
@@ -64,6 +83,9 @@ object Timetables:
 
   /** Factory of PartialTimetable */
   object PartialTimetable:
+    /** Returns `PartialTimetable` given train, startStation (where train departs) and departureTime from startStation.
+      * If `departureTime` is invalid is returned an [[ClockTimeErrors]]
+      */
     def apply(
         train: Train,
         startStation: Station,
