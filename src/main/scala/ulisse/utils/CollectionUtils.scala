4f03aff491b6accc7a72ac0d4172d7c0297df46f
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-18 23:32:58 +0100

    chore(RouteEnvironment): setup utility functions

--- src/main/scala/ulisse/utils/CollectionUtils.scala
+++ src/main/scala/ulisse/utils/CollectionUtils.scala
@@ -1,8 +1,19 @@
 package ulisse.utils
 
-import cats.Functor
+import cats.data.State
+import cats.syntax.all.*
+import cats.{Functor, Traverse}
 
 object CollectionUtils:
   extension [F[_], A](collection: F[A])(using F: Functor[F])
     def updateWhen(condition: A => Boolean)(update: A => A): F[A] =
       F.map(collection)(item => if condition(item) then update(item) else item)
+  extension [F[_], A](collection: F[A])(using F: Traverse[F])
+    // maybe a fold approach is more readable
+    def updateFirstWhen(condition: A => Boolean)(update: A => A): F[A] =
+      collection.traverse(item =>
+        State[Boolean, A] { hasUpdated =>
+          if condition(item) && !hasUpdated then (true, update(item))
+          else (hasUpdated, item)
+        }
+      ).runA(false).value
