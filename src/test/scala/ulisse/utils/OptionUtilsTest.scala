b4c8ab80da47d20a0cfaffd969861068cd78e1ab
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-19 00:55:24 +0100

    chore(Utils): test option/collection utils

---
+++ src/test/scala/ulisse/utils/OptionUtilsTest.scala
@@ -0,0 +1,14 @@
+package ulisse.utils
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import ulisse.utils.OptionUtils.when
+
+class OptionUtilsTest extends AnyWordSpec with Matchers:
+
+  "when" should:
+    "return Some if the condition is true" in:
+      1 when true shouldBe Some(1)
+
+    "return None if the condition is false" in:
+      1 when false shouldBe None
