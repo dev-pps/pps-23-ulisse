b5de5aca37188702a3c3345e5a1506b381abdfa2
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-18 23:32:58 +0100

    chore(RouteEnvironment): setup utility functions

--- src/main/scala/ulisse/entities/train/TrainAgent.scala
+++ src/main/scala/ulisse/entities/train/TrainAgent.scala
@@ -1,12 +1,22 @@
 package ulisse.entities.train
 
-import ulisse.entities.simulation.SimulationAgent
+import ulisse.entities.simulation.Environments.RailwayEnvironment
+import ulisse.entities.simulation.Simulations.Actions
+import ulisse.entities.simulation.{Environments, SimulationAgent}
 import ulisse.entities.station.StationEnvironmentElement
 import ulisse.entities.train.Trains.Train
 
 trait TrainAgent extends Train with SimulationAgent:
   def distanceTravelled: Double
-  def distanceTravelled_=(newTravelDistance: Double): TrainAgent
+  def distanceTravelled_=(newDistanceTravelled: Double): TrainAgent
   def resetDistanceTravelled(): TrainAgent                       = distanceTravelled = 0
   def updateDistanceTravelled(distanceDelta: Double): TrainAgent = distanceTravelled += distanceDelta
   def isOnRoute: Boolean                                         = distanceTravelled > 0
+
+object TrainAgent:
+  def createTrainAgent(train: Train): TrainAgent = TrainAgentImpl(train, 0.0)
+  private final case class TrainAgentImpl(train: Train, distanceTravelled: Double) extends TrainAgent:
+    export train.*
+    def distanceTravelled_=(newDistanceTravelled: Double): TrainAgent = copy(distanceTravelled = newDistanceTravelled)
+
+    override def doStep(dt: Int, simulationEnvironment: RailwayEnvironment): Option[Actions.SimulationAction] = None
