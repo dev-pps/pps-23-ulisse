c3dfa57d0b18cb5041785912a5175246daed5ef5
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-19 00:55:24 +0100

    chore(Utils): test option/collection utils

--- src/main/scala/ulisse/utils/OptionUtils.scala
+++ src/main/scala/ulisse/utils/OptionUtils.scala
@@ -1,6 +1,7 @@
 package ulisse.utils
 
-object OptionUtilities:
+object OptionUtils:
   extension [A](optionalResult: => A)
+    /** Compute and returns an `Option` containing the result if the condition is `true`, otherwise `None`. */
     def when(condition: Boolean): Option[A] =
       if condition then Some(optionalResult) else None
