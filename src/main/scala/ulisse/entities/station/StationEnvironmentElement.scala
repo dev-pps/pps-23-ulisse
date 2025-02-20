170fb77417a6d8d9407c632d4cfc05113fdf4334
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-20 12:32:19 +0100

    chore(StationEnviroment): improve implementation

--- src/main/scala/ulisse/entities/station/StationEnvironmentElement.scala
+++ src/main/scala/ulisse/entities/station/StationEnvironmentElement.scala
@@ -3,11 +3,12 @@ package ulisse.entities.station
 import ulisse.entities.simulation.Environments.EnvironmentElement
 import ulisse.entities.train.Trains.Train
 import ulisse.utils.CollectionUtils.*
+import ulisse.utils.OptionUtils.when
 
 trait StationEnvironmentElement extends Station with EnvironmentElement:
   val tracks: List[Track]
   def firstAvailableTrack: Option[Track] = tracks.find(_.train.isEmpty)
-  def updateTrack(track: Track, train: Option[Train]): StationEnvironmentElement
+  def updateTrack(track: Track, train: Option[Train]): Option[StationEnvironmentElement]
 
 object StationEnvironmentElement:
   def createStationEnvironmentElement(station: Station): StationEnvironmentElement =
@@ -15,17 +16,20 @@ object StationEnvironmentElement:
 
   extension (train: Train)
     def arriveAt(station: StationEnvironmentElement): Option[StationEnvironmentElement] =
-      station.firstAvailableTrack.map(track => station.updateTrack(track, Some(train)))
+      station.firstAvailableTrack.flatMap(track => station.updateTrack(track, Some(train)))
 
     def leave(station: StationEnvironmentElement): Option[StationEnvironmentElement] =
-      station.tracks.find(_.train.contains(train)).map(track => station.updateTrack(track, None))
+      station.tracks.find(_.train.contains(train)).flatMap(track => station.updateTrack(track, None))
 
     def findInStation(stations: Seq[StationEnvironmentElement]): Option[StationEnvironmentElement] =
-      stations.find(_.tracks.contains(train))
+      // TODO check impl
+      stations.find(_.tracks.exists(_.train.map(_.name).contains(train.name)))
 
   private final case class StationEnvironmentElementImpl(station: Station, tracks: List[Track])
       extends StationEnvironmentElement:
     export station.*
 
-    def updateTrack(track: Track, train: Option[Train]): StationEnvironmentElement =
-      copy(tracks = tracks.updateWhen(_ == track)(_.withTrain(train)))
+    def updateTrack(track: Track, train: Option[Train]): Option[StationEnvironmentElement] =
+      copy(tracks = tracks.updateWhen(_ == track)(_.withTrain(train))) when !tracks.exists(
+        train.isDefined && _.train == train
+      )
