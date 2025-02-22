ed244bb47f5305a4bbfa990f5fc40639b9a30da8
Manuel Buizo <buizomanuel@gmail.com>
2025-02-22 16:49:21 +0100

    feat: common trait to extended of swing components

--- src/main/scala/ulisse/infrastructures/view/components/ExtendedSwing.scala
+++ src/main/scala/ulisse/infrastructures/view/components/ExtendedSwing.scala
@@ -6,22 +6,21 @@ import java.awt.FlowLayout
 import scala.swing.*
 
 object ExtendedSwing:
-  trait ExtendedSwing
-  
-  case class JBorderPanelItem() extends BorderPanel with ShapeEffect with FontEffect
+  trait ExtendedSwingStyle extends ShapeEffect with FontEffect with BorderEffect
 
-  case class JFlowPanelItem() extends FlowPanel with ShapeEffect with FontEffect:
+  case class JBorderPanelItem() extends BorderPanel with ExtendedSwingStyle
+
+  case class JFlowPanelItem() extends FlowPanel with ExtendedSwingStyle:
     private val layout = new FlowLayout(FlowLayout.CENTER, 0, 0)
     peer.setLayout(layout)
     export layout._
 
-  case class JBoxPanelItem(orientation: Orientation.Value) extends BoxPanel(orientation)
-      with ShapeEffect with FontEffect with BorderEffect
+  case class JBoxPanelItem(orientation: Orientation.Value) extends BoxPanel(orientation) with ExtendedSwingStyle
 
-  case class JPanelItem() extends Panel with ShapeEffect with FontEffect
+  case class JPanelItem() extends Panel with ExtendedSwingStyle
 
-  case class JButtonItem(label: String) extends Button(label) with ShapeEffect with FontEffect
+  case class JButtonItem(label: String) extends Button(label) with ExtendedSwingStyle
 
-  case class JLabelItem(label: String) extends Label(label) with ShapeEffect with FontEffect
+  case class JLabelItem(label: String) extends Label(label) with ExtendedSwingStyle
 
-  case class JTextFieldItem(colum: Int) extends TextField(colum) with ShapeEffect with FontEffect
+  case class JTextFieldItem(colum: Int) extends TextField(colum) with ExtendedSwingStyle
