aa3b99c53d739d5697b2c7a7a57bdc82a5173060
Manuel Buizo <buizomanuel@gmail.com>
2025-02-22 18:19:25 +0100

    feat: create source image

---
+++ src/test/scala/ulisse/infrastructures/view/components/images/SourceImageTest.scala
@@ -0,0 +1,23 @@
+package ulisse.infrastructures.view.components.images
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import ulisse.infrastructures.view.components.ui.decorators.Images.SourceImage
+
+object SourceImageTest:
+  val rightPath = "icons/route.svg"
+  val wrongPath = "icons/test"
+
+  val rightSourceImage = SourceImage(rightPath)
+  val wrongSourceImage = SourceImage(wrongPath)
+
+class SourceImageTest extends AnyFlatSpec with Matchers:
+  import SourceImageTest.*
+
+  "create source with right path" should "return a source image" in:
+    rightSourceImage.path mustBe rightPath
+    rightSourceImage.bufferImage must not be None
+
+  "create source with wrong path" should "return a empty image" in:
+    wrongSourceImage.path mustBe wrongPath
+    wrongSourceImage.bufferImage mustBe None
