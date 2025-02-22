12a7ac90551ec196d702d3c61abadd7e0f0b9ebf
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-22 18:20:50 +0100

    refactor(Track): make minPermittedDistanceBetweenTrains dependsOn context

--- src/main/scala/ulisse/entities/route/Track.scala
+++ src/main/scala/ulisse/entities/route/Track.scala
@@ -20,22 +20,22 @@ trait Track:
   export trains.{contains, exists, isEmpty}
 
 object Track:
-  def apply(minPermittedDistanceBetweenTrains: Double, trains: TrainAgent*): Track =
-    TrackImpl(trains.distinctBy(_.name), minPermittedDistanceBetweenTrains)
+  def apply(trains: TrainAgent*)(using minPermittedDistanceBetweenTrains: Double, trackLength: Double): Track =
+    TrackImpl(trains.distinctBy(_.name))
   def createCheckedTrack(
-      minPermittedDistanceBetweenTrains: Double,
       trains: TrainAgent*
-  ): Either[NonEmptyChain[Track.Errors], Track] =
-    validateUniqueItems(trains.map(_.name), Errors.DuplicateTrains).toValidatedNec
-      .map(_ => Track(minPermittedDistanceBetweenTrains, trains*))
+  )(using minPermittedDistanceBetweenTrains: Double, trackLength: Double): Either[NonEmptyChain[Track.Errors], Track] =
+    (validateUniqueItems(trains.map(_.name), Errors.DuplicateTrains).toValidatedNec,
+      validate
+      .map(_ => Track(trains*))
       .toEither
-  private final case class TrackImpl(trains: Seq[TrainAgent], minPermittedDistanceBetweenTrains: Double) extends Track:
+  private final case class TrackImpl(trains: Seq[TrainAgent])(using val minPermittedDistanceBetweenTrains: Double) extends Track:
     @targetName("appendedTrain")
     override def :+(train: TrainAgent): Either[NonEmptyChain[Track.Errors], Track] =
-      Track.createCheckedTrack(minPermittedDistanceBetweenTrains, trains :+ train: _*)
+      Track.createCheckedTrack(trains :+ train*)
     override def updateWhen(p: TrainAgent => Boolean)(f: TrainAgent => TrainAgent)
         : Either[NonEmptyChain[Track.Errors], Track] =
-      Track.createCheckedTrack(minPermittedDistanceBetweenTrains, trains.updateWhen(p)(f): _*)
+      Track.createCheckedTrack(trains.updateWhen(p)(f)*)
     override def filterNot(p: TrainAgent => Boolean): Track = copy(trains = trains.filterNot(p))
 
   enum Errors extends BaseError:
