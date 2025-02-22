8d39212816eb991ed81c9280c3dede4a0934a33e
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-22 17:38:23 +0100

    refactor(OptionUtils): restore when functionality adding implicit flatten

--- src/main/scala/ulisse/utils/OptionUtils.scala
+++ src/main/scala/ulisse/utils/OptionUtils.scala
@@ -6,3 +6,5 @@ object OptionUtils:
     /** Compute and returns an `Option` containing the result if the condition is `true`, otherwise `None`. */
     def when(condition: Boolean): Option[A] =
       if condition then Some(optionalResult) else None
+
+  given [A]: Conversion[Option[Option[A]], Option[A]] = _.flatten
