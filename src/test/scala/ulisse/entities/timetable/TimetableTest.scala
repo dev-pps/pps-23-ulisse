0351129a13777add27a823daa0a36b698aea1929
Matteo <matteo.violani@studio.unibo.it>
2025-02-23 12:08:09 +0100

    refactor: added default ClockTime method and strategy, new Time entity, Timetable gets ClockTime

--- src/test/scala/ulisse/entities/timetable/TimetableTest.scala
+++ src/test/scala/ulisse/entities/timetable/TimetableTest.scala
@@ -18,60 +18,48 @@ import scala.language.postfixOps
 
 class TimetableTest extends AnyFlatSpec:
 
-  val partialTimetable: Either[ClockTimeErrors, PartialTimetable] =
-    PartialTimetable(train = AV1000Train, startStation = stationA, departureTime = h(9).m(0))
+  val partialTimetable: PartialTimetable =
+    PartialTimetable(train = AV1000Train, startStation = stationA, departureTime = h(9).m(0).getOrDefault)
 
-  val AV1000TimeTable: Either[ClockTimeErrors, TrainTimetable] =
-    partialTimetable.map:
-      _.stopsIn(stationB, waitTime = 5)(railAV_10)
-        .transitIn(stationC)(railAV_10)
-        .stopsIn(stationD, waitTime = 10)(railAV_10)
-        .arrivesTo(stationF)(railAV_10)
-
-  "When create new Timetable" should "be returned an error if departure time is not valid" in:
-    val invalidDepartureTime = h(50).m(0)
-    val invalidTimetable: Either[ClockTimeErrors, PartialTimetable] =
-      PartialTimetable(train = AV1000Train, startStation = stationA, invalidDepartureTime)
-    import ulisse.utils.Times.InvalidHours
-    invalidTimetable should be(Left(InvalidHours()))
+  val AV1000TimeTable: TrainTimetable =
+    partialTimetable.stopsIn(stationB, waitTime = 5)(railAV_10)
+      .transitIn(stationC)(railAV_10)
+      .stopsIn(stationD, waitTime = 10)(railAV_10)
+      .arrivesTo(stationF)(railAV_10)
 
   "TrainTimetable" should "provide list of stations where train stops and where it only transits" in:
-    AV1000TimeTable in: t =>
-      t.stopStations should be(List(stationB, stationD))
-      t.transitStations should be(List(stationC))
+    AV1000TimeTable.stopStations should be(List(stationB, stationD))
+    AV1000TimeTable.transitStations should be(List(stationC))
 
   "TrainTimetable" should "calculate arriving and departure time of each station where train arrives" in:
     val timeTableWithStops =
-      partialTimetable.map:
-        _.stopsIn(stationB, 5)(RailInfo(length = 10, typeRoute = AV))
-          .arrivesTo(stationC)(RailInfo(length = 15, typeRoute = AV))
+      partialTimetable
+        .stopsIn(stationB, 5)(RailInfo(length = 10, typeRoute = AV))
+        .arrivesTo(stationC)(RailInfo(length = 15, typeRoute = AV))
 
-    timeTableWithStops in: t =>
-      t.table should be(ListMap(
-        stationA -> StartScheduleTime(h(9).m(0).toOption),
-        stationB -> AutoScheduleTime(arriving = h(9).m(2).toOption, waitTime = Some(5.toWaitTime)),
-        stationC -> EndScheduleTime(arriving = h(9).m(10).toOption)
-      ))
+    timeTableWithStops.table should be(ListMap(
+      stationA -> StartScheduleTime(h(9).m(0).toOption),
+      stationB -> AutoScheduleTime(arriving = h(9).m(2).toOption, waitTime = Some(5.toWaitTime)),
+      stationC -> EndScheduleTime(arriving = h(9).m(10).toOption)
+    ))
 
   "TrainTimetable" should "consider also station of transit in estimation of arriving time" in:
-    val timetableWithTransits =
-      partialTimetable.map:                                           // 9:00
-        _.transitIn(stationB)(RailInfo(length = 10, typeRoute = AV))  // 2 min from A = 9:02
-          .transitIn(stationC)(RailInfo(length = 15, typeRoute = AV)) // 3 min from B = 9:05
-          .transitIn(stationD)(RailInfo(length = 25, typeRoute = AV)) // 5 min from C = 9:10
-          .arrivesTo(stationF)(RailInfo(length = 5, typeRoute = AV))  // 1 min from D = 9:11
-    timetableWithTransits.in(_.arrivingTime should be(ClockTime(9, 11).toOption))
+    val timetableWithTransits = // 9:00
+      partialTimetable.transitIn(stationB)(RailInfo(length = 10, typeRoute = AV)) // 2 min from A = 9:02
+        .transitIn(stationC)(RailInfo(length = 15, typeRoute = AV))               // 3 min from B = 9:05
+        .transitIn(stationD)(RailInfo(length = 25, typeRoute = AV))               // 5 min from C = 9:10
+        .arrivesTo(stationF)(RailInfo(length = 5, typeRoute = AV))                // 1 min from D = 9:11
+    timetableWithTransits.arrivingTime should be(ClockTime(9, 11).toOption)
 
   "TrainTimetable" should "provide couple with nearest stations names" in:
     val AV1000TimeTable =
-      partialTimetable.map:
-        _.stopsIn(stationB, waitTime = 5)(railAV_10)
-          .stopsIn(stationC, waitTime = 5)(railAV_10)
-          .arrivesTo(stationD)(railAV_10)
+      partialTimetable
+        .stopsIn(stationB, waitTime = 5)(railAV_10)
+        .stopsIn(stationC, waitTime = 5)(railAV_10)
+        .arrivesTo(stationD)(railAV_10)
 
-    AV1000TimeTable in:
-      _.routes should be(List(
-        (stationA, stationB),
-        (stationB, stationC),
-        (stationC, stationD)
-      ))
+    AV1000TimeTable.routes should be(List(
+      (stationA, stationB),
+      (stationB, stationC),
+      (stationC, stationD)
+    ))
---
+++ src/test/scala/ulisse/utils/TimesTest.scala
@@ -0,0 +1,56 @@
+package ulisse.utils
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import ulisse.TestUtility.in
+import ulisse.utils.Times.*
+import ulisse.utils.Times.FluentDeclaration.h
+
+import scala.language.postfixOps
+
+class TimesTest extends AnyWordSpec with Matchers:
+  "ClockTime" should:
+    "validate non in range values of hours and minutes" in:
+      ClockTime(h = -12, m = 30) should be(Left(InvalidHours(Time(-12, 30))))
+      ClockTime(h = 30, m = 30) should be(Left(InvalidHours(Time(30, 30))))
+      ClockTime(h = 10, m = -30) should be(Left(InvalidMinutes(Time(10, -30))))
+      ClockTime(h = 10, m = 60) should be(Left(InvalidMinutes(Time(10, 60))))
+
+    "be added to another ClockTimes" in:
+      h(10).m(45) + h(2).m(30) should be(h(13).m(15))
+      h(10).m(59) + h(0).m(1) should be(h(11).m(0))
+      h(0).m(59) + h(0).m(59) should be(h(1).m(58))
+      h(23).m(59) + h(0).m(1) should be(h(0).m(0))
+
+    "comparable to other ClockTime" in:
+      h(10).m(45) greaterEqThan h(10).m(45) should be(true)
+      h(10).m(45) greaterEqThan h(2).m(30) should be(true)
+      h(2).m(30) greaterEqThan h(10).m(45) should be(false)
+      h(2).m(30) sameAs h(10).m(45) should be(false)
+      h(2).m(30) greaterThan h(10).m(45) should be(false)
+
+    "be created with default value in case of invalid ClockTime" in:
+      val invalidHour    = 25
+      val invalidMinutes = 60
+      val defaultTime    = Time(0, 0)
+      val clockTime      = ClockTime.withDefault(invalidHour, invalidMinutes)
+      clockTime.asTime shouldBe Time(0, 0)
+
+    "return user clock time when hours and minutes values are valid using withDefault method" in:
+      val validHours   = 12
+      val validMinutes = 35
+      val clockTime    = ClockTime.withDefault(validHours, validMinutes)
+      clockTime.asTime shouldBe Time(validHours, validMinutes)
+
+    "returns a default time that depends on context strategy" in:
+      import ulisse.utils.Times.ClockTime.DefaultTimeStrategy
+      val invalidClockTime      = ClockTime(50, 70)
+      given DefaultTimeStrategy = t => Time(t.h % 24, t.m % 60)
+      val defaultClockTime      = invalidClockTime.getOrDefault
+      defaultClockTime.asTime shouldBe Time(2, 10)
+//
+//    "be subtracted to another ClockTimes" in :
+//      ClockTime(10, 25) - ClockTime(1, 25) should be(ClockTime(11, 50))
+//      ClockTime(10, 59) - ClockTime(0, 1) should be(ClockTime(11, 0))
+//      ClockTime(24, 59) - ClockTime(0, 1) should be(ClockTime(1, 0))
+//      ClockTime(24, 59) - ClockTime(0, 1) should be(ClockTime(0, 0))
