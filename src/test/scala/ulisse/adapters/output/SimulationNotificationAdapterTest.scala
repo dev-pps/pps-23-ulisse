4702ae06e0a25ed9d64747a905614cfa0dbcb367
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-24 23:03:52 +0100

    chore(SimulationNotificationAdapter): basic testing

---
+++ src/test/scala/ulisse/adapters/output/SimulationNotificationAdapterTest.scala
@@ -0,0 +1,19 @@
+package ulisse.adapters.output
+
+import org.mockito.Mockito.verify
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar.mock
+import ulisse.entities.simulation.Simulations.SimulationData
+import ulisse.infrastructures.view.simulation.SimulationPage
+
+class SimulationNotificationAdapterTest extends AnyWordSpec with Matchers:
+
+  private val simulationData                = mock[SimulationData]
+  private val mockedSimulationPage          = mock[SimulationPage]
+  private val simulationNotificationAdapter = SimulationNotificationAdapter(mockedSimulationPage)
+
+  "SimulationNotificationAdapter" should:
+    "call updateData on SimulationPage when stepNotification is triggered" in:
+      simulationNotificationAdapter.stepNotification(simulationData)
+      verify(mockedSimulationPage).updateData(simulationData)
---
+++ src/test/scala/ulisse/adapters/output/TimeProviderAdapterTest.scala
@@ -0,0 +1,5 @@
+package ulisse.adapters.output
+
+class TimeProviderAdapterTest {
+
+}
