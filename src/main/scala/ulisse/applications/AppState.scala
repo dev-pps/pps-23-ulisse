33095ddf1db2d0402970a1985c636de242f0fb8d
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-24 23:37:11 +0100

    refactor(AppState): unify SimulationState and use only one queue

--- src/main/scala/ulisse/applications/AppState.scala
+++ src/main/scala/ulisse/applications/AppState.scala
@@ -1,20 +1,28 @@
 package ulisse.applications
 
+import ulisse.adapters.output.UtilityAdapters.TimeProviderAdapter
 import ulisse.applications.managers.RouteManagers.RouteManager
 import ulisse.applications.managers.TechnologyManagers.TechnologyManager
 import ulisse.applications.managers.TrainManagers.TrainManager
 import ulisse.applications.managers.{SimulationManager, StationManager}
 import ulisse.entities.station.Station
 import ulisse.entities.train.Trains.TrainTechnology
+import ulisse.infrastructures.commons.TimeProviders.TimeProvider
 
 object AppState:
   def default(): AppState =
-    AppState(StationManager(), RouteManager.empty(), TrainManager(List.empty), TechnologyManager(List.empty))
+    AppState(
+      StationManager(),
+      RouteManager.empty(),
+      TrainManager(List.empty),
+      TechnologyManager(List.empty),
+      SimulationManager.emptyBatchManager(TimeProviderAdapter(TimeProvider.systemTimeProvider()))
+    )
 
 final case class AppState(
     stationManager: StationManager,
     routeManager: RouteManager,
     trainManager: TrainManager,
-    technologyManager: TechnologyManager[TrainTechnology]
+    technologyManager: TechnologyManager[TrainTechnology],
+    simulationManager: SimulationManager
 )
-final case class SimulationState(simulationManager: SimulationManager)
--- src/main/scala/ulisse/applications/useCases/SimulationInfoService.scala
+++ src/main/scala/ulisse/applications/useCases/SimulationInfoService.scala
@@ -1,6 +1,5 @@
 package ulisse.applications.useCases
 
-import ulisse.applications.SimulationState
 import ulisse.applications.ports.SimulationInfoPorts
 import ulisse.entities.route.Routes.Route
 import ulisse.entities.route.{RouteEnvironmentElement, Routes}
@@ -9,17 +8,17 @@ import ulisse.entities.simulation.SimulationAgent
 import ulisse.entities.station.{Station, StationEnvironmentElement}
 import ulisse.entities.train.Trains.Train
 import ulisse.entities.train.{TrainAgent, Trains}
-
+import ulisse.applications.AppState
 import java.util.concurrent.LinkedBlockingQueue
 import scala.concurrent.{Future, Promise}
 
 final case class SimulationInfoService(
-    simulationEvents: LinkedBlockingQueue[SimulationState => SimulationState]
+    eventQueue: LinkedBlockingQueue[AppState => AppState]
 ) extends SimulationInfoPorts.Input:
   // TODO is best a future that fails or a future that return an option?
   override def stationInfo(s: Station): Future[Option[StationEnvironmentElement]] =
     val p = Promise[Option[StationEnvironmentElement]]
-    simulationEvents.add((state: SimulationState) => {
+    eventQueue.add((state: AppState) => {
       p.success(state.simulationManager.simulationData.simulationEnvironment.stations.find(_.name == s.name))
       state
     })
@@ -27,7 +26,7 @@ final case class SimulationInfoService(
 
   override def routeInfo(r: Route): Future[Option[RouteEnvironmentElement]] =
     val p = Promise[Option[RouteEnvironmentElement]]
-    simulationEvents.add((state: SimulationState) => {
+    eventQueue.add((state: AppState) => {
       p.success(state.simulationManager.simulationData.simulationEnvironment.routes.find(_.id == r.id))
       state
     })
@@ -35,7 +34,7 @@ final case class SimulationInfoService(
 
   override def trainInfo(t: Train): Future[Option[TrainAgent]] =
     val p = Promise[Option[TrainAgent]]
-    simulationEvents.add((state: SimulationState) => {
+    eventQueue.add((state: AppState) => {
       p.success(state.simulationManager.simulationData.simulationEnvironment.agents.collect({ case t: TrainAgent => t }
       ).find(_.name == t.name))
       state
