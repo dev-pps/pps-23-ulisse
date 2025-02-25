1f5efb19ae2263cb1e76386c491afd5f382d397c
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-25 16:04:58 +0100

    feat(SimulationService): add api for setup

--- src/main/scala/ulisse/applications/ports/SimulationPorts.scala
+++ src/main/scala/ulisse/applications/ports/SimulationPorts.scala
@@ -1,6 +1,7 @@
 package ulisse.applications.ports
 
 import ulisse.entities.simulation.Simulations.{EngineState, SimulationData}
+import ulisse.utils.Times.{ClockTime, Time}
 
 import scala.concurrent.Future
 
@@ -10,6 +11,8 @@ object SimulationPorts:
     def stepNotification(data: SimulationData): Unit
 
   trait Input:
+    def initSimulation(): Future[EngineState]
+    def setupEngine(stepSize: Time, cyclesPerSecond: Option[Int]): Future[EngineState]
     def start(): Future[EngineState]
     def stop(): Future[EngineState]
     def reset(): Future[EngineState]
