5e5b3110b87ea1b745c536c87182570a376925dd
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-25 02:11:59 +0100

    chore(Utils): update test and collectionUtils

--- src/main/scala/ulisse/utils/OptionUtils.scala
+++ src/main/scala/ulisse/utils/OptionUtils.scala
@@ -5,6 +5,6 @@ object OptionUtils:
   extension [A](optionalResult: => A)
     /** Compute and returns an `Option` containing the result if the condition is `true`, otherwise `None`. */
     def when(condition: Boolean): Option[A] =
-      if condition then Some(optionalResult) else None
+      Option.when(condition)(optionalResult)
 
   given [A]: Conversion[Option[Option[A]], Option[A]] = _.flatten
