c8f8c6fa135e3f906cdfa9d188ee9db8575d4343
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-25 02:11:59 +0100

    chore(Utils): update test and collectionUtils

--- src/main/scala/ulisse/utils/CollectionUtils.scala
+++ src/main/scala/ulisse/utils/CollectionUtils.scala
@@ -10,22 +10,10 @@ object CollectionUtils:
   private def wrappedUpdate[A](update: A => A)(in: A): Id[A] = Id(update(in))
 
   extension [F[_]: Traverse, A](collection: F[A])
-    /** Update all elements in the collection that satisfy the condition, handling effects. */
+    /** Update all elements in the collection that satisfy the condition. */
     def updateWhen(condition: A => Boolean)(update: A => A): F[A] =
       collection.updateWhenWithEffects(condition)(wrappedUpdate(update))
+
+    /** Update all elements in the collection that satisfy the condition, handling effects. */
     def updateWhenWithEffects[W[_]: Monad](condition: A => Boolean)(update: A => W[A]): W[F[A]] =
       collection.traverse(item => if condition(item) then update(item) else item.pure[W])
-
-  extension [F[_]: Traverse, A](collection: F[A])
-    /** Update the first element in the collection that satisfies the condition. */
-    def updateFirstWhen(condition: A => Boolean)(update: A => A): F[A] =
-      collection.updateFirstWhenWithEffects(condition)(wrappedUpdate(update))
-    def updateFirstWhenWithEffects[W[_]: Monad](condition: A => Boolean)(update: A => W[A]): W[F[A]] =
-      collection.traverse { item =>
-        StateT[W, Boolean, A] { hasUpdated =>
-          if condition(item) && !hasUpdated then
-            update(item).map(updated => (true, updated))
-          else
-            (hasUpdated, item).pure[W]
-        }
-      }.runA(false)
--- src/test/scala/ulisse/Utils.scala
+++ src/test/scala/ulisse/Utils.scala
@@ -1,7 +1,20 @@
 package ulisse
+import org.scalatest.exceptions.TestFailedException
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import ulisse.Utils.MatchersUtils.shouldBeBoolean
+
 object Utils:
   object MatchersUtils extends Matchers:
     extension (b1: Boolean)
       def shouldBeBoolean(b2: Boolean): Unit =
         b1 shouldBe b2
+
+class UtilsTest extends AnyWordSpec with Matchers:
+  "shouldBeBoolean" should:
+    "correctly compare two boolean values" in:
+      true shouldBeBoolean true
+      false shouldBeBoolean false
+      intercept[TestFailedException]:
+        false shouldBeBoolean true
+        true shouldBeBoolean false
