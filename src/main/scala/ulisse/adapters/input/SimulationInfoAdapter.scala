b6760fa82ad5e46be67eb49b79009cba38640abb
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-25 16:21:25 +0100

    chore(SimulationInfoService): return also the list of timetables related to the trainAgent

--- src/main/scala/ulisse/adapters/input/SimulationInfoAdapter.scala
+++ src/main/scala/ulisse/adapters/input/SimulationInfoAdapter.scala
@@ -4,7 +4,7 @@ import ulisse.applications.ports.SimulationInfoPorts
 import ulisse.entities.route.RouteEnvironmentElement
 import ulisse.entities.route.Routes.Route
 import ulisse.entities.station.{Station, StationEnvironmentElement}
-import ulisse.entities.train.TrainAgents.TrainAgent
+import ulisse.entities.train.TrainAgents.{TrainAgent, TrainAgentInfo}
 import ulisse.entities.train.Trains.Train
 
 import scala.concurrent.Future
@@ -12,4 +12,4 @@ import scala.concurrent.Future
 final case class SimulationInfoAdapter(private val simulationInfoPort: SimulationInfoPorts.Input):
   def stationInfo(s: Station): Future[Option[StationEnvironmentElement]] = simulationInfoPort.stationInfo(s)
   def routeInfo(r: Route): Future[Option[RouteEnvironmentElement]]       = simulationInfoPort.routeInfo(r)
-  def trainInfo(t: Train): Future[Option[TrainAgent]]                    = simulationInfoPort.trainInfo(t)
+  def trainInfo(t: Train): Future[Option[TrainAgentInfo]]                = simulationInfoPort.trainInfo(t)
