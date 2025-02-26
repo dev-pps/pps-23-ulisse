bef05b971fd9c1f0fcd779bfe3f954289c6b079e
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-26 19:56:13 +0100

    refactor(TrainAgentContainer): remove direction from the base trait

--- src/main/scala/ulisse/entities/station/Platform.scala
+++ src/main/scala/ulisse/entities/station/Platform.scala
@@ -2,25 +2,23 @@ package ulisse.entities.station
 
 import cats.data.NonEmptyChain
 import cats.syntax.all.*
-import ulisse.entities.simulation.EnvironmentElements.{TrainAgentsContainer, TrainAgentsDirection}
+import ulisse.entities.simulation.EnvironmentElements.TrainAgentsContainer
 import ulisse.entities.train.TrainAgents.TrainAgent
 import ulisse.utils.Errors.BaseError
 import ulisse.utils.OptionUtils.when
 import ulisse.utils.ValidationUtils.validatePositive
 
-trait Platform2 extends Platform
-
 /** Defines a track in a station. */
-trait Platform extends TrainAgentsContainer:
+trait Platform extends TrainAgentsContainer[Platform]:
   val currentTrain: Option[TrainAgent]
-  override def trains: Seq[TrainAgent]                   = currentTrain.toList
-  override def minPermittedDistanceBetweenTrains: Double = if isEmpty then 0.0 else Double.MaxValue
+  override def trains: Seq[TrainAgent] = currentTrain.toList
+  def putTrain(train: TrainAgent): Option[Platform]
 
 /** Factory for [[Platform]] instances. */
 object Platform:
 
   /** Creates a `Track` instance. If platform number is not positive it is set to 1 */
-  def apply(platformNumber: Int): Platform = PlatformImpl(math.max(1, platformNumber), None, None)
+  def apply(platformNumber: Int): Platform = PlatformImpl(math.max(1, platformNumber), None)
 
   /** Creates a `Track` instance with validation. If platform number is not positive an error is returned */
   def createCheckedPlatform(platformNumber: Int): Either[NonEmptyChain[Errors], Platform] =
@@ -32,11 +30,10 @@ object Platform:
 
   private final case class PlatformImpl(
       id: Int,
-      currentTrain: Option[TrainAgent],
-      currentDirection: Option[TrainAgentsDirection]
+      currentTrain: Option[TrainAgent]
   ) extends Platform:
     // TODO evaluate if could be nice to introduce a control to check if the train is entering the station or is already moved
-    override def putTrain(train: TrainAgent, direction: TrainAgentsDirection): Option[Platform] =
+    override def putTrain(train: TrainAgent): Option[Platform] =
       copy(currentTrain = Some(train)) when isAvailable
     override def updateTrain(train: TrainAgent): Option[Platform] =
       copy(currentTrain = Some(train)) when contains(train)
