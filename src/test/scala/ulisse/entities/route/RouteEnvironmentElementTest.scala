dcae07f181d8fceae92f0673a92aec6109b30821
Federico Bravetti <federicobravetti802@gmail.com>
2025-02-27 20:50:11 +0100

    chore(ValidationUtils): add validatedUniqueItemsBy

--- src/test/scala/ulisse/entities/route/RouteEnvironmentElementTest.scala
+++ src/test/scala/ulisse/entities/route/RouteEnvironmentElementTest.scala
@@ -13,137 +13,137 @@ import ulisse.entities.train.Wagons.{UseType, Wagon}
 class RouteEnvironmentElementTest extends AnyWordSpec with Matchers:
   given minPermittedDistanceBetweenTrains: Double = 100.0
   private val defaultTechnology                   = TrainTechnology("HighSpeed", 300, 1.0, 0.5)
-  private val defaultWagon                        = Wagon(UseType.Passenger, 50)
-  private val defaultWagonNumber                  = 5
-  private val train3905 =
-    TrainAgent.apply(Train("3905", defaultTechnology, defaultWagon, defaultWagonNumber))
-  private val train3906 =
-    TrainAgent.apply(Train("3906", defaultTechnology, defaultWagon, defaultWagonNumber))
-  private val train3907 =
-    TrainAgent.apply(Train("3907", defaultTechnology, defaultWagon, defaultWagonNumber))
-
-  def route: RouteEnvironmentElement =
-    // Create a route with 2 tracks and a length of 200.0 + trainLength, min distance between two train is 100.0 with this factory method
-    validateRoute.flatMap(_.withLength(2 * minPermittedDistanceBetweenTrains + train3905.length)) match
-      case Left(errors) => fail()
-      case Right(route) => RouteEnvironmentElement(route, minPermittedDistanceBetweenTrains)
-
-  "RouteEnvironmentElement" should:
-    "have two empty tracks" in:
-      route.containers.size shouldBe 2
-      route.containers.forall(_.isEmpty) shouldBe true
-
-    "have a default minPermittedDistanceBetweenTrains of 100.0" in:
-      route.containers.forall(_.minPermittedDistanceBetweenTrains == 100.0) shouldBe true
-
-    "have a length greater or equal than minPermittedDistanceBetweenTrains + train3905 length" in:
-      route.containers.forall(
-        route.length >= _.minPermittedDistanceBetweenTrains + train3905.length
-      ) shouldBe true
-
-  "A trainAgent" when:
-    "take a route" should:
-      "be place in a track if it empty" in:
-        route.putTrain(train3905, Forward) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers.find(_.contains(train3905)) shouldBe Some(Track(1, train3905))
-            updatedRoute.containers shouldBe Seq(Track(1, train3905), Track(2))
-          case None => fail()
-
-      "not be place in a track if not available" in:
-        route.putTrain(train3905, Forward).flatMap(_.putTrain(train3906, Forward)) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers.find(_.contains(train3906)) shouldBe Some(Track(2, train3906))
-            updatedRoute.containers shouldBe Seq(Track(1, train3905), Track(2, train3906))
-            updatedRoute.putTrain(train3907, Forward) shouldBe None
-          case None => fail()
-
-      "not be place if it's already in the route" in:
-        route.putTrain(train3905, Forward).flatMap(_.putTrain(train3905, Forward)) shouldBe None
-        route.putTrain(train3905, Forward).flatMap(
-          _.updateTrain(train3905.updateDistanceTravelled(100.0 + train3905.length))
-        ).flatMap(_.putTrain(train3905, Forward)) shouldBe None
-
-      "be place in a track if available" in:
-        val train3905Updated = train3905.updateDistanceTravelled(100.0 + train3905.length)
-        route.putTrain(train3905, Forward).flatMap(_.updateTrain(train3905Updated)).flatMap(_.putTrain(
-          train3906,
-          Forward
-        )) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers.find(_.contains(train3906)) shouldBe Some(Track(1, train3905Updated, train3906))
-          case None => fail()
-
-    "put in a route" should:
-      "be placed in the first available track" in:
-        route.putTrain(train3905, Forward) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers shouldBe Seq(
-              Track(1, train3905),
-              Track(2)
-            )
-          case None => fail()
-
-        route.putTrain(train3905, Forward).flatMap(_.putTrain(
-          train3906,
-          Forward
-        )) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers shouldBe Seq(
-              Track(1, train3905),
-              Track(2, train3906)
-            )
-          case None => fail()
-
-      "not be placed if it's already in the route" in:
-        route.putTrain(train3905, Forward).flatMap(_.putTrain(
-          train3905,
-          Forward
-        )) shouldBe None
-
-      "not be placed if it's not available" in:
-        route.putTrain(train3905, Forward).flatMap(_.putTrain(
-          train3906,
-          Forward
-        )).flatMap(_.putTrain(train3907, Forward)) shouldBe None
-
-      "be place behind if it is possible" in:
-        val train3905Updated = train3905.updateDistanceTravelled(100.0 + train3905.length)
-        route.putTrain(train3905, Forward).flatMap(_.updateTrain(train3905Updated)).flatMap(
-          _.putTrain(train3906, Forward)
-        ) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers shouldBe Seq(
-              Track(1, train3905Updated, train3906),
-              Track(2)
-            )
-          case None => fail()
-
-    "update in a route" should:
-      "be updated if present" in:
-        val updatedTrain3905 = train3905.updateDistanceTravelled(1)
-        route.putTrain(train3905, Forward).flatMap(_.updateTrain(updatedTrain3905)) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers.find(_.contains(train3905)) shouldBe Some(Track(1, updatedTrain3905))
-            updatedRoute.containers shouldBe Seq(
-              Track(1, updatedTrain3905),
-              Track(2)
-            )
-          case None => fail()
-
-      "not be updated if not present" in:
-        route.updateTrain(train3905) shouldBe None
-
-    "remove from a route" should:
-      "be removed if present" in:
-        route.putTrain(train3905, Forward).flatMap(_.removeTrain(train3905)) match
-          case Some(updatedRoute) =>
-            updatedRoute.containers.find(_.contains(train3905)) shouldBe None
-            updatedRoute.containers shouldBe Seq(
-              Track(1),
-              Track(2)
-            )
-          case None => fail()
-
-      "not be removed if not present" in:
-        route.removeTrain(train3905) shouldBe None
+//  private val defaultWagon                        = Wagon(UseType.Passenger, 50)
+//  private val defaultWagonNumber                  = 5
+//  private val train3905 =
+//    TrainAgent.apply(Train("3905", defaultTechnology, defaultWagon, defaultWagonNumber))
+//  private val train3906 =
+//    TrainAgent.apply(Train("3906", defaultTechnology, defaultWagon, defaultWagonNumber))
+//  private val train3907 =
+//    TrainAgent.apply(Train("3907", defaultTechnology, defaultWagon, defaultWagonNumber))
+//
+//  def route: RouteEnvironmentElement =
+//    // Create a route with 2 tracks and a length of 200.0 + trainLength, min distance between two train is 100.0 with this factory method
+//    validateRoute.flatMap(_.withLength(2 * minPermittedDistanceBetweenTrains + train3905.length)) match
+//      case Left(errors) => fail()
+//      case Right(route) => RouteEnvironmentElement(route, minPermittedDistanceBetweenTrains)
+//
+//  "RouteEnvironmentElement" should:
+//    "have two empty tracks" in:
+//      route.containers.size shouldBe 2
+//      route.containers.forall(_.isEmpty) shouldBe true
+//
+//    "have a default minPermittedDistanceBetweenTrains of 100.0" in:
+//      route.containers.forall(_.minPermittedDistanceBetweenTrains == 100.0) shouldBe true
+//
+//    "have a length greater or equal than minPermittedDistanceBetweenTrains + train3905 length" in:
+//      route.containers.forall(
+//        route.length >= _.minPermittedDistanceBetweenTrains + train3905.length
+//      ) shouldBe true
+//
+//  "A trainAgent" when:
+//    "take a route" should:
+//      "be place in a track if it empty" in:
+//        route.putTrain(train3905, Forward) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers.find(_.contains(train3905)) shouldBe Some(Track(1, train3905))
+//            updatedRoute.containers shouldBe Seq(Track(1, train3905), Track(2))
+//          case None => fail()
+//
+//      "not be place in a track if not available" in:
+//        route.putTrain(train3905, Forward).flatMap(_.putTrain(train3906, Forward)) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers.find(_.contains(train3906)) shouldBe Some(Track(2, train3906))
+//            updatedRoute.containers shouldBe Seq(Track(1, train3905), Track(2, train3906))
+//            updatedRoute.putTrain(train3907, Forward) shouldBe None
+//          case None => fail()
+//
+//      "not be place if it's already in the route" in:
+//        route.putTrain(train3905, Forward).flatMap(_.putTrain(train3905, Forward)) shouldBe None
+//        route.putTrain(train3905, Forward).flatMap(
+//          _.updateTrain(train3905.updateDistanceTravelled(100.0 + train3905.length))
+//        ).flatMap(_.putTrain(train3905, Forward)) shouldBe None
+//
+//      "be place in a track if available" in:
+//        val train3905Updated = train3905.updateDistanceTravelled(100.0 + train3905.length)
+//        route.putTrain(train3905, Forward).flatMap(_.updateTrain(train3905Updated)).flatMap(_.putTrain(
+//          train3906,
+//          Forward
+//        )) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers.find(_.contains(train3906)) shouldBe Some(Track(1, train3905Updated, train3906))
+//          case None => fail()
+//
+//    "put in a route" should:
+//      "be placed in the first available track" in:
+//        route.putTrain(train3905, Forward) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers shouldBe Seq(
+//              Track(1, train3905),
+//              Track(2)
+//            )
+//          case None => fail()
+//
+//        route.putTrain(train3905, Forward).flatMap(_.putTrain(
+//          train3906,
+//          Forward
+//        )) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers shouldBe Seq(
+//              Track(1, train3905),
+//              Track(2, train3906)
+//            )
+//          case None => fail()
+//
+//      "not be placed if it's already in the route" in:
+//        route.putTrain(train3905, Forward).flatMap(_.putTrain(
+//          train3905,
+//          Forward
+//        )) shouldBe None
+//
+//      "not be placed if it's not available" in:
+//        route.putTrain(train3905, Forward).flatMap(_.putTrain(
+//          train3906,
+//          Forward
+//        )).flatMap(_.putTrain(train3907, Forward)) shouldBe None
+//
+//      "be place behind if it is possible" in:
+//        val train3905Updated = train3905.updateDistanceTravelled(100.0 + train3905.length)
+//        route.putTrain(train3905, Forward).flatMap(_.updateTrain(train3905Updated)).flatMap(
+//          _.putTrain(train3906, Forward)
+//        ) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers shouldBe Seq(
+//              Track(1, train3905Updated, train3906),
+//              Track(2)
+//            )
+//          case None => fail()
+//
+//    "update in a route" should:
+//      "be updated if present" in:
+//        val updatedTrain3905 = train3905.updateDistanceTravelled(1)
+//        route.putTrain(train3905, Forward).flatMap(_.updateTrain(updatedTrain3905)) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers.find(_.contains(train3905)) shouldBe Some(Track(1, updatedTrain3905))
+//            updatedRoute.containers shouldBe Seq(
+//              Track(1, updatedTrain3905),
+//              Track(2)
+//            )
+//          case None => fail()
+//
+//      "not be updated if not present" in:
+//        route.updateTrain(train3905) shouldBe None
+//
+//    "remove from a route" should:
+//      "be removed if present" in:
+//        route.putTrain(train3905, Forward).flatMap(_.removeTrain(train3905)) match
+//          case Some(updatedRoute) =>
+//            updatedRoute.containers.find(_.contains(train3905)) shouldBe None
+//            updatedRoute.containers shouldBe Seq(
+//              Track(1),
+//              Track(2)
+//            )
+//          case None => fail()
+//
+//      "not be removed if not present" in:
+//        route.removeTrain(train3905) shouldBe None
--- src/test/scala/ulisse/entities/route/TrackTest.scala
+++ src/test/scala/ulisse/entities/route/TrackTest.scala
@@ -11,99 +11,99 @@ import ulisse.entities.train.Wagons.{UseType, Wagon}
 class TrackTest extends AnyWordSpec with Matchers:
   given minPermittedDistanceBetweenTrains: Double = 100.0
   private val defaultTechnology                   = TrainTechnology("HighSpeed", 300, 1.0, 0.5)
-  private val defaultWagon                        = Wagon(UseType.Passenger, 50)
-  private val defaultWagonNumber                  = 5
-  private val train3905 =
-    TrainAgent.apply(Train("3905", defaultTechnology, defaultWagon, defaultWagonNumber))
-  private val train3906 =
-    TrainAgent.apply(Train("3906", defaultTechnology, defaultWagon, defaultWagonNumber))
-  private val train3907 =
-    TrainAgent.apply(Train("3907", defaultTechnology, defaultWagon, defaultWagonNumber))
-
-  "A track" when:
-    "created" should:
-      "be created empty by default" in:
-        val track = Track(1)
-        track.isEmpty shouldBe true
-        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-
-      "be created with a list of trains" in:
-        val track = Track(1, train3905, train3906, train3907)
-        track.trains.size shouldBe 3
-        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-
-      "contain distinct trains" in:
-        val track = Track(1, train3905, train3905, train3905)
-        track.trains.size shouldBe 1
-        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-
-      "contain distinct trains considering the name" in:
-        val track = Track(1, train3905, train3905.updateDistanceTravelled(10))
-        track.trains.size shouldBe 1
-        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-
-    "created checked" should:
-      "be created if contains distinct trains" in:
-        Track.createCheckedTrack(1, train3905, train3906, train3907) shouldBe Right(Track(
-          1,
-          train3905,
-          train3906,
-          train3907
-        ))
-
-      "return errors when not contains distinct trains" in:
-        Track.createCheckedTrack(1, train3905, train3905, train3905) shouldBe Left(Chain(Track.Errors.DuplicateTrains))
-
-    "a new train is added" should:
-      "be updated with the new train" in:
-        val track = Track(1, train3905, train3906)
-        track :+ (train3907, Forward) match
-          case Left(_) => fail()
-          case Right(ut) =>
-            ut.trains.size shouldBe 3
-            ut.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-
-      "not be updated if the train is already moved" in:
-        Track(1, train3905) :+ (train3906.updateDistanceTravelled(10), Forward) shouldBe Left(
-          Chain(Track.Errors.TrainAlreadyMoved)
-        )
-
-      "not be updated if the train is already present" in:
-        Track(1, train3905) :+ (train3905, Forward) shouldBe Left(Chain(Track.Errors.DuplicateTrains))
-
-      "not be updated if the train is already present with the same name" in:
-        Track(1, train3905) :+ (train3905.updateDistanceTravelled(10), Forward) shouldBe Left(Chain(
-          Track.Errors.TrainAlreadyMoved,
-          Track.Errors.DuplicateTrains
-        ))
-
-    "a train is updated with a new train" should:
-      "be updated with the new train" in:
-        Track(1, train3905, train3906).updateTrain(train3905) match
-          case Some(ut) =>
-            ut.trains shouldBe Seq(train3905, train3906)
-            ut.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-          case None => fail()
-
-        Track(1, train3905, train3906).updateTrain(train3905.updateDistanceTravelled(10)) match
-          case Some(ut) =>
-            ut.trains shouldBe Seq(train3905.updateDistanceTravelled(10), train3906)
-            ut.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
-          case None => fail()
-
-      "not be updated if the train is not present" in:
-        Track(1, train3905, train3906).updateTrain(train3907) shouldBe None
-
-      "be available" in:
-        Track(1).isAvailable shouldBe true
-        Track(
-          1,
-          train3905.updateDistanceTravelled(minPermittedDistanceBetweenTrains + train3905.length)
-        ).isAvailable shouldBe true
-
-      "not be available" in:
-        Track(1, train3905).isAvailable shouldBe false
-        Track(
-          1,
-          train3905.updateDistanceTravelled(minPermittedDistanceBetweenTrains / 2)
-        ).isAvailable shouldBe false
+//  private val defaultWagon                        = Wagon(UseType.Passenger, 50)
+//  private val defaultWagonNumber                  = 5
+//  private val train3905 =
+//    TrainAgent.apply(Train("3905", defaultTechnology, defaultWagon, defaultWagonNumber))
+//  private val train3906 =
+//    TrainAgent.apply(Train("3906", defaultTechnology, defaultWagon, defaultWagonNumber))
+//  private val train3907 =
+//    TrainAgent.apply(Train("3907", defaultTechnology, defaultWagon, defaultWagonNumber))
+//
+//  "A track" when:
+//    "created" should:
+//      "be created empty by default" in:
+//        val track = Track(1)
+//        track.isEmpty shouldBe true
+//        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//
+//      "be created with a list of trains" in:
+//        val track = Track(1, train3905, train3906, train3907)
+//        track.trains.size shouldBe 3
+//        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//
+//      "contain distinct trains" in:
+//        val track = Track(1, train3905, train3905, train3905)
+//        track.trains.size shouldBe 1
+//        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//
+//      "contain distinct trains considering the name" in:
+//        val track = Track(1, train3905, train3905.updateDistanceTravelled(10))
+//        track.trains.size shouldBe 1
+//        track.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//
+//    "created checked" should:
+//      "be created if contains distinct trains" in:
+//        Track.createCheckedTrack(1, train3905, train3906, train3907) shouldBe Right(Track(
+//          1,
+//          train3905,
+//          train3906,
+//          train3907
+//        ))
+//
+//      "return errors when not contains distinct trains" in:
+//        Track.createCheckedTrack(1, train3905, train3905, train3905) shouldBe Left(Chain(Track.Errors.DuplicateTrains))
+//
+//    "a new train is added" should:
+//      "be updated with the new train" in:
+//        val track = Track(1, train3905, train3906)
+//        track :+ (train3907, Forward) match
+//          case Left(_) => fail()
+//          case Right(ut) =>
+//            ut.trains.size shouldBe 3
+//            ut.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//
+//      "not be updated if the train is already moved" in:
+//        Track(1, train3905) :+ (train3906.updateDistanceTravelled(10), Forward) shouldBe Left(
+//          Chain(Track.Errors.TrainAlreadyMoved)
+//        )
+//
+//      "not be updated if the train is already present" in:
+//        Track(1, train3905) :+ (train3905, Forward) shouldBe Left(Chain(Track.Errors.DuplicateTrains))
+//
+//      "not be updated if the train is already present with the same name" in:
+//        Track(1, train3905) :+ (train3905.updateDistanceTravelled(10), Forward) shouldBe Left(Chain(
+//          Track.Errors.TrainAlreadyMoved,
+//          Track.Errors.DuplicateTrains
+//        ))
+//
+//    "a train is updated with a new train" should:
+//      "be updated with the new train" in:
+//        Track(1, train3905, train3906).updateTrain(train3905) match
+//          case Some(ut) =>
+//            ut.trains shouldBe Seq(train3905, train3906)
+//            ut.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//          case None => fail()
+//
+//        Track(1, train3905, train3906).updateTrain(train3905.updateDistanceTravelled(10)) match
+//          case Some(ut) =>
+//            ut.trains shouldBe Seq(train3905.updateDistanceTravelled(10), train3906)
+//            ut.minPermittedDistanceBetweenTrains shouldBe minPermittedDistanceBetweenTrains
+//          case None => fail()
+//
+//      "not be updated if the train is not present" in:
+//        Track(1, train3905, train3906).updateTrain(train3907) shouldBe None
+//
+//      "be available" in:
+//        Track(1).isAvailable shouldBe true
+//        Track(
+//          1,
+//          train3905.updateDistanceTravelled(minPermittedDistanceBetweenTrains + train3905.length)
+//        ).isAvailable shouldBe true
+//
+//      "not be available" in:
+//        Track(1, train3905).isAvailable shouldBe false
+//        Track(
+//          1,
+//          train3905.updateDistanceTravelled(minPermittedDistanceBetweenTrains / 2)
+//        ).isAvailable shouldBe false
