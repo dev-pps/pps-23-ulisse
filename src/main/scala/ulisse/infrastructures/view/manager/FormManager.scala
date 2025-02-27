3499267f875a86094950d7b7db39895292657c26
Manuel Buizo <buizomanuel@gmail.com>
2025-02-27 23:06:53 +0100

    fix: exspansion of form buttons

--- src/main/scala/ulisse/infrastructures/view/manager/FormManager.scala
+++ src/main/scala/ulisse/infrastructures/view/manager/FormManager.scala
@@ -11,25 +11,25 @@ trait FormManager extends ComposedSwing:
   def scheduleForm: Form
 
 object FormManager:
-  def createMap(): MapController = MapController()
+  def createMap(): FormManagerImpl = FormManagerImpl()
 
-  private case class BaseCentralController(iconLabels: ComposedImageLabel*)(forms: Form*):
+  private case class BaseFormManager(iconLabels: ComposedImageLabel*)(forms: Form*):
     private val page: Map[ComposedImageLabel, Form]   = iconLabels.zip(forms).toMap
     private val tabbedPane: ComposedSwing.JTabbedPane = ComposedSwing.createTabbedPane(iconLabels: _*)
 
-    page.foreach(tabbedPane.paneOf(_).contents += _.component)
+    page.map(tabbedPane.paneOf(_).contents += _.component)
 
     def pageOf(label: ComposedImageLabel): Form = page(label)
     def component[T >: Component]: T            = tabbedPane.component
 
-  case class MapController() extends FormManager:
+  case class FormManagerImpl() extends FormManager:
     given orientation: Orientation.Value = Orientation.Horizontal
     private val station                  = ComposedImageLabel.createIcon("icons/station.svg", "station")
     private val route                    = ComposedImageLabel.createIcon("icons/route.svg", "route")
     private val schedule                 = ComposedImageLabel.createIcon("icons/menu/train.svg", "schedule")
 
-    private val menu: BaseCentralController =
-      BaseCentralController(station, route, schedule)(Form.createStation(), Form.createRoute(), Form.createSchedule())
+    private val menu: BaseFormManager =
+      BaseFormManager(station, route, schedule)(Form.createStation(), Form.createRoute(), Form.createSchedule())
 
     export menu._
 
