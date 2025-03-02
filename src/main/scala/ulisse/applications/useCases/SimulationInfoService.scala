3df8d6821547ac08c7093fc13c71dfd08fad1b99
Manuel Buizo <buizomanuel@gmail.com>
2025-03-02 11:49:54 +0100

    feat: add methods in event queue to read manager

--- src/main/scala/ulisse/applications/useCases/SimulationInfoService.scala
+++ src/main/scala/ulisse/applications/useCases/SimulationInfoService.scala
@@ -18,27 +18,24 @@ final case class SimulationInfoService(private val eventQueue: SimulationEventQu
 
   override def stationInfo(s: Station): Future[Option[StationEnvironmentElement]] =
     val p = Promise[Option[StationEnvironmentElement]]
-    eventQueue.addReadSimulationEvent(simulationManager => {
-      p.success(simulationManager.simulationData.simulationEnvironment.stations.find(_.name == s.name))
-      simulationManager
-    })
+    eventQueue.addReadSimulationEvent(simulationData =>
+      p.success(simulationData.simulationEnvironment.stations.find(_.name == s.name))
+    )
     p.future
 
   override def routeInfo(r: Route): Future[Option[RouteEnvironmentElement]] =
     val p = Promise[Option[RouteEnvironmentElement]]
-    eventQueue.addReadSimulationEvent(simulationManager => {
-      p.success(simulationManager.simulationData.simulationEnvironment.routes.find(_.id == r.id))
-      simulationManager
-    })
+    eventQueue.addReadSimulationEvent(simulationData =>
+      p.success(simulationData.simulationEnvironment.routes.find(_.id == r.id))
+    )
     p.future
 
   override def trainInfo(t: Train): Future[Option[TrainAgentInfo]] =
     val p = Promise[Option[TrainAgentInfo]]
     // TODO find timetables
-    eventQueue.addReadSimulationEvent(simulationManager => {
-      p.success(simulationManager.simulationData.simulationEnvironment.agents.collect({ case t: TrainAgent => t }).find(
+    eventQueue.addReadSimulationEvent(simulationData => {
+      p.success(simulationData.simulationEnvironment.agents.collect({ case t: TrainAgent => t }).find(
         _.name == t.name
       ).map(TrainAgentInfo(_, List.empty)))
-      simulationManager
     })
     p.future
