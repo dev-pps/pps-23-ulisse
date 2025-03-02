404717e96b87cbf0a62864df2c7193366fbedaa0
Manuel Buizo <buizomanuel@gmail.com>
2025-03-02 23:04:05 +0100

    refactor: updateMananger

--- src/main/scala/ulisse/applications/useCases/Services.scala
+++ src/main/scala/ulisse/applications/useCases/Services.scala
@@ -8,10 +8,10 @@ object Services:
   /** Function to update a manager and return the updated manager. */
   def updateManager[Error, Value, Manager](
       promise: Promise[Either[Error, Value]],
-      routeManager: Manager,
+      manager: Manager,
       updatedManager: Either[Error, Manager],
       values: Manager => Value
   ): Manager =
     updatedManager match
-      case Left(error)       => promise.success(Left(error)); routeManager
+      case Left(error)       => promise.success(Left(error)); manager
       case Right(newManager) => promise.success(Right(values(newManager))); newManager
