b8761a6bc47ac9e3382e6de782583e6a9ab862c1
Federico Bravetti <federicobravetti802@gmail.com>
2025-03-03 21:50:51 +0100

    feat(FieldsComparators): make a generic dsl to compare object fields

--- src/main/scala/ulisse/entities/simulation/data/EngineState.scala
+++ src/main/scala/ulisse/entities/simulation/data/EngineState.scala
@@ -1,42 +1,28 @@
 package ulisse.entities.simulation.data
 
+import ulisse.dsl.comparison.FieldsComparators.{Field, FieldComparator}
+
 object EngineState:
   def empty(): EngineState = EngineState(false, None, 0, None, 0, 0)
 
-  enum Field:
-    case Running, CyclesPerSecond, LastUpdate, LastDelta, ElapsedCycleTime
-
-  given Conversion[ComparisonBuilder, Boolean] with
-    def apply(builder: ComparisonBuilder): Boolean =
-      builder.compare
-
-  case class ComparisonBuilder(engines: List[EngineState], ignoredFields: Seq[Field]):
-    def ignoring(fields: Field*): ComparisonBuilder =
-      copy(ignoredFields = ignoredFields ++ fields)
-
-    def considering(fields: Field*): ComparisonBuilder =
-      copy(ignoredFields = Field.values.filterNot(fields.contains).toIndexedSeq)
-
-    def andTo(nextEngine: EngineState): ComparisonBuilder =
-      copy(engines = nextEngine +: engines)
-
-    def compare: Boolean =
-      val fieldsToCompare = Field.values.filterNot(ignoredFields.contains)
+  given FieldComparator[EngineStateField, EngineState] with
+    def compare(engines: List[EngineState], ignoredFields: Seq[EngineStateField]): Boolean =
+      val fieldsToCompare = EngineStateField.values.filterNot(ignoredFields.contains)
       engines match
-        case Nil => true
-        case firstEngine :: tail => tail.forall { otherEngine =>
-            fieldsToCompare.forall {
-              case Field.Running          => firstEngine.running == otherEngine.running
-              case Field.CyclesPerSecond  => firstEngine.cyclesPerSecond == otherEngine.cyclesPerSecond
-              case Field.LastUpdate       => firstEngine.lastUpdate == otherEngine.lastUpdate
-              case Field.LastDelta        => firstEngine.lastDelta == otherEngine.lastDelta
-              case Field.ElapsedCycleTime => firstEngine.elapsedCycleTime == otherEngine.elapsedCycleTime
-            }
-          }
+        case firstEngine :: tail => tail.forall: otherEngine =>
+            fieldsToCompare.forall:
+              case EngineStateField.Running          => firstEngine.running == otherEngine.running
+              case EngineStateField.CyclesPerSecond  => firstEngine.cyclesPerSecond == otherEngine.cyclesPerSecond
+              case EngineStateField.LastUpdate       => firstEngine.lastUpdate == otherEngine.lastUpdate
+              case EngineStateField.LastDelta        => firstEngine.lastDelta == otherEngine.lastDelta
+              case EngineStateField.ElapsedCycleTime => firstEngine.elapsedCycleTime == otherEngine.elapsedCycleTime
+        case _ => false
+
+  enum EngineStateField extends Field[EngineStateField, EngineState]:
+    case Running, CyclesPerSecond, LastUpdate, LastDelta, ElapsedCycleTime
+    def values: Seq[EngineStateField] = EngineStateField.values.toSeq
 
   extension (engineState: EngineState)
-    def compareTo(otherEngineState: EngineState): ComparisonBuilder =
-      ComparisonBuilder(List(engineState, otherEngineState), Seq[Field]())
     def decreaseElapsedCycleTimeBy(delta: Double): EngineState =
       engineState.copy(elapsedCycleTime = engineState.elapsedCycleTime - delta)
 
