33804f29bef6d849e11dcca8b5af1ef0e4bc924e
Manuel Buizo <buizomanuel@gmail.com>
2025-03-03 11:01:43 +0100

    feat: create services manager

--- src/main/scala/ulisse/Main.scala
+++ src/main/scala/ulisse/Main.scala
@@ -1,11 +1,14 @@
 package ulisse
 
-import ulisse.applications.{AppState, EventQueue}
+import ulisse.applications.{AppState, EventQueue, ServicesManager}
 import ulisse.infrastructures.view.GUIView
 
 object Main:
 
   @main def launchApp(): Unit =
-    val eventQueue   = EventQueue()
+    val eventQueue      = EventQueue()
+    val servicesManager = ServicesManager(eventQueue)
+    val map             = GUIView()
+
     val initialState = AppState()
-    val map          = GUIView()
+    eventQueue.startProcessing(initialState)
