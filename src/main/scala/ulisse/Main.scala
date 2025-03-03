2da3f2f3faad8ae757c225c130f30db840408d57
Manuel Buizo <buizomanuel@gmail.com>
2025-03-03 11:19:28 +0100

    feat: create input port manager

--- src/main/scala/ulisse/Main.scala
+++ src/main/scala/ulisse/Main.scala
@@ -1,14 +1,14 @@
 package ulisse
 
-import ulisse.applications.{AppState, EventQueue, ServicesManager}
+import ulisse.applications.{AppState, EventQueue, InputPortManager}
 import ulisse.infrastructures.view.GUIView
 
 object Main:
 
   @main def launchApp(): Unit =
-    val eventQueue      = EventQueue()
-    val servicesManager = ServicesManager(eventQueue)
-    val map             = GUIView()
+    val eventQueue       = EventQueue()
+    val inputPortManager = InputPortManager(eventQueue)
+    val map              = GUIView(inputPortManager)
 
     val initialState = AppState()
     eventQueue.startProcessing(initialState)
