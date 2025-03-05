c5919c1a0615ae480eec123888fd55de04e7ed2d
Federico Bravetti <federicobravetti802@gmail.com>
2025-03-05 21:03:44 +0100

    refactor(StationPort): improve code

--- src/test/scala/ulisse/applications/useCases/StationServiceTest.scala
+++ src/test/scala/ulisse/applications/useCases/StationServiceTest.scala
@@ -1,9 +1,12 @@
 package ulisse.applications.useCases
 
 import cats.data.Chain
+import org.mockito.Mockito.when
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar.mock
 import ulisse.Runner.runAll
+import ulisse.applications.managers.RouteManagers.RouteManager
 import ulisse.applications.{AppState, EventQueue}
 import ulisse.applications.managers.StationManager
 import ulisse.entities.Coordinate
@@ -18,10 +21,28 @@ import scala.util.Right
 
 class StationServiceTest extends AnyWordSpec with Matchers:
 
-  private val initialState  = AppState()
+  private val mockedRouteManager = mock[RouteManager]
+  when(mockedRouteManager.routes).thenReturn(List())
+  private val initialState  = AppState().updateRoute(_ => mockedRouteManager)
   private val eventQueue    = EventQueue()
   private val inputPort     = StationService(eventQueue)
   private def updateState() = runAll(initialState, eventQueue.events)
+
+  private def removeStation(): Unit =
+    val addStationResult = inputPort.addStation(stationA)
+    val removeStationResult = inputPort.removeStation(stationA)
+    val stationMapResult = inputPort.stationMap
+    updateState()
+    Await.result(addStationResult, Duration.Inf) shouldBe Right(List(stationA))
+    Await.result(removeStationResult, Duration.Inf) shouldBe Right((List(), List()))
+    Await.result(stationMapResult, Duration.Inf) shouldBe List()
+
+  private def erroneousRemoveStation(): Unit =
+    val removeStationResult = inputPort.removeStation(stationA)
+    val stationMapResult = inputPort.stationMap
+    updateState()
+    Await.result(removeStationResult, Duration.Inf) shouldBe Left(Chain(StationManager.Error.StationNotFound))
+    Await.result(stationMapResult, Duration.Inf) shouldBe List()
 
   "StationService" should:
     "add a valid station to the station manager" in:
@@ -45,28 +66,27 @@ class StationServiceTest extends AnyWordSpec with Matchers:
       ))
       Await.result(stationMapResult, Duration.Inf) shouldBe List(stationA)
 
-    "remove a present station from the station manager" in:
-      val addStationResult    = inputPort.addStation(stationA)
-      val removeStationResult = inputPort.removeStation(stationA)
-      val stationMapResult    = inputPort.stationMap
+    "remove a present station from the station manager and the route manager" in:
+      when(mockedRouteManager.deleteByStation(stationA)).thenReturn(Right(mockedRouteManager))
+      removeStation()
 
-      updateState()
-      Await.result(addStationResult, Duration.Inf) shouldBe Right(List(stationA))
-      Await.result(removeStationResult, Duration.Inf) shouldBe Right((List(), List()))
-      Await.result(stationMapResult, Duration.Inf) shouldBe List()
+    "remove a present station from the station manager" in :
+      when(mockedRouteManager.deleteByStation(stationA)).thenReturn(Left(Chain()))
+      removeStation()
 
-    "return error when is removed an absent station from the station manager" in:
-      val removeStationResult = inputPort.removeStation(stationA)
-      val stationMapResult    = inputPort.stationMap
-      updateState()
+    "return error when is removed an absent station from the station manager and the route manager" in:
+      when(mockedRouteManager.deleteByStation(stationA)).thenReturn(Left(Chain()))
+      erroneousRemoveStation()
 
-      Await.result(removeStationResult, Duration.Inf) shouldBe Left(Chain(StationManager.Error.StationNotFound))
-      Await.result(stationMapResult, Duration.Inf) shouldBe List()
+    "return error when is removed an absent station from the station manager" in :
+      when(mockedRouteManager.deleteByStation(stationA)).thenReturn(Right(mockedRouteManager))
+      erroneousRemoveStation()
 
     "update a present station in the station manager" in:
       val addStationResult    = inputPort.addStation(stationA)
       val updateStationResult = inputPort.updateStation(stationA, stationB)
       val stationMapResult    = inputPort.stationMap
+      when(mockedRouteManager.modifyAutomaticByStation(stationA, stationB)).thenReturn(mockedRouteManager)
 
       updateState()
       Await.result(addStationResult, Duration.Inf) shouldBe Right(List(stationA))
