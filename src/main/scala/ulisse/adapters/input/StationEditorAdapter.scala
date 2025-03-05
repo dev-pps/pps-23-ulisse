9ec7c3c61f57a254e2bf54ae63ab945bb7190c36
Federico Bravetti <federicobravetti802@gmail.com>
2025-03-05 22:00:34 +0100

    refactor(StationEditorAdapter): improve code

--- src/main/scala/ulisse/adapters/input/StationEditorAdapter.scala
+++ src/main/scala/ulisse/adapters/input/StationEditorAdapter.scala
@@ -2,6 +2,7 @@ package ulisse.adapters.input
 
 import cats.data.{Chain, NonEmptyChain}
 import cats.syntax.all.*
+import ulisse.adapters.input.StationEditorAdapter.StationCreationInfo
 import ulisse.applications.ports.StationPorts
 import ulisse.applications.ports.StationPorts.Input
 import ulisse.entities.Coordinate
@@ -11,65 +12,89 @@ import ulisse.utils.Errors.BaseError
 
 import scala.concurrent.Future
 
-object StationEditorAdapter:
-
-  /** Represents errors that can occur during view input validation. */
-  enum Error extends BaseError:
-    case InvalidFirstCoordinateComponentFormat, InvalidSecondCoordinateComponentFormat, InvalidNumberOfTrackFormat
-
-/** Controller for the `StationEditorView`, managing interactions between the view and the application. */
-final case class StationEditorAdapter(
-    private val appPort: StationPorts.Input
-):
-
-  def addStation(
-      stationName: String,
-      x: String,
-      y: String,
-      numberOfTrack: String
-  ): Future[Either[NonEmptyChain[BaseError], StationPorts.Input#SM]] =
-    createStation(stationName, x, y, numberOfTrack) match
-      case Left(error)    => Future.successful(Left(error))
-      case Right(station) => appPort.addStation(station)
-
-  def updateStation(
-      stationName: String,
-      x: String,
-      y: String,
-      numberOfTrack: String,
-      oldStation: Station
-  ): Future[Either[NonEmptyChain[BaseError], (StationPorts.Input#SM, List[Route])]] =
-    createStation(stationName, x, y, numberOfTrack) match
-      case Left(error)    => Future.successful(Left(error))
-      case Right(station) => appPort.updateStation(oldStation, station)
-
-  /** Handles the click event when the "OK" button is pressed.
-    *
-    * This method attempts to create and add a new station using the provided information.
-    * If an `oldStation` is given, the station is instead updated.
-    * If the input data or it's format is invalid errors are returned.
-    */
+/** Adapter for the `StationEditorView` that handles interactions between the view and the Station input port. */
+trait StationEditorAdapter:
+  // TODO remove
   def onOkClick(
       stationName: String,
       x: String,
       y: String,
-      numberOfTrack: String,
+      numberOfPlatforms: String,
       oldStation: Option[Station]
-  ): Future[Either[NonEmptyChain[BaseError], StationPorts.Input#SM]] =
-    createStation(stationName, x, y, numberOfTrack) match
-      case Left(error)    => Future.successful(Left(error))
-      case Right(station) => appPort.addStation(station)
+  ): Future[Either[NonEmptyChain[BaseError], StationPorts.Input#SM]]
 
-  private def createStation(
-      name: String,
+  /** Adds a new station to the application. */
+  def addStation(
+      stationCreationInfo: StationCreationInfo
+  ): Future[Either[NonEmptyChain[BaseError], StationPorts.Input#SM]]
+
+  /** Updates a station in the application. */
+  def updateStation(
+      stationCreationInfo: StationCreationInfo,
+      oldStation: Station
+  ): Future[Either[NonEmptyChain[BaseError], (StationPorts.Input#SM, List[Route])]]
+
+  /** Removes a station from the application. */
+  def removeStation(station: Station): Future[Either[NonEmptyChain[BaseError], (StationPorts.Input#SM, List[Route])]]
+
+  /** Finds a station at the given coordinate. */
+  def findStationAt(coordinate: Coordinate): Future[Option[Station]]
+
+/** Factory for [[StationEditorAdapter]] instances. */
+object StationEditorAdapter:
+  /** Information required to create a new station. */
+  case class StationCreationInfo(
+      stationName: String,
       x: String,
       y: String,
-      numberOfTrack: String
-  ): Either[NonEmptyChain[BaseError], Station] =
-    (
-      x.toIntOption.toValidNec(StationEditorAdapter.Error.InvalidFirstCoordinateComponentFormat),
-      y.toIntOption.toValidNec(StationEditorAdapter.Error.InvalidSecondCoordinateComponentFormat),
-      numberOfTrack.toIntOption.toValidNec(StationEditorAdapter.Error.InvalidNumberOfTrackFormat)
-    ).mapN((_, _, _)).toEither.flatMap((x, y, nt) => Station.createCheckedStation(name, Coordinate(x, y), nt))
+      numberOfPlatforms: String
+  )
 
-  export appPort.{findStationAt, removeStation}
+  /** Creates a new StationEditorAdapter. */
+  def apply(appPort: StationPorts.Input): StationEditorAdapter = StationEditorAdapterImpl(appPort)
+
+  /** Represents errors that can occur during view input validation. */
+  enum Error extends BaseError:
+    case InvalidFirstCoordinateComponentFormat, InvalidSecondCoordinateComponentFormat, InvalidNumberOfPlatformsFormat
+
+  private final case class StationEditorAdapterImpl(private val appPort: StationPorts.Input)
+      extends StationEditorAdapter:
+    override def addStation(
+        stationCreationInfo: StationCreationInfo
+    ): Future[Either[NonEmptyChain[BaseError], StationPorts.Input#SM]] =
+      stationCreationInfo.createStation(appPort.addStation)
+
+    override def updateStation(
+        stationCreationInfo: StationCreationInfo,
+        oldStation: Station
+    ): Future[Either[NonEmptyChain[BaseError], (StationPorts.Input#SM, List[Route])]] =
+      stationCreationInfo.createStation(appPort.updateStation(oldStation, _))
+
+    extension (stationCreationInfo: StationCreationInfo)
+      private def createStation[R](
+          update: Station => Future[Either[NonEmptyChain[BaseError], R]]
+      ): Future[Either[NonEmptyChain[BaseError], R]] =
+        createStationFrom(stationCreationInfo) match
+          case Left(error)    => Future.successful(Left(error))
+          case Right(station) => update(station)
+
+    private def createStationFrom(
+        stationCreationInfo: StationCreationInfo
+    ): Either[NonEmptyChain[BaseError], Station] = stationCreationInfo match
+      case StationCreationInfo(name, x, y, numberOfPlatforms) =>
+        (
+          x.toIntOption.toValidNec(StationEditorAdapter.Error.InvalidFirstCoordinateComponentFormat),
+          y.toIntOption.toValidNec(StationEditorAdapter.Error.InvalidSecondCoordinateComponentFormat),
+          numberOfPlatforms.toIntOption.toValidNec(StationEditorAdapter.Error.InvalidNumberOfPlatformsFormat)
+        ).mapN((_, _, _)).toEither.flatMap((x, y, nt) => Station.createCheckedStation(name, Coordinate(x, y), nt))
+
+    export appPort.{findStationAt, removeStation}
+
+    def onOkClick(
+        stationName: String,
+        x: String,
+        y: String,
+        numberOfPlatforms: String,
+        oldStation: Option[Station]
+    ): Future[Either[NonEmptyChain[BaseError], StationPorts.Input#SM]] =
+      StationCreationInfo(stationName, x, y, numberOfPlatforms).createStation(appPort.addStation)
