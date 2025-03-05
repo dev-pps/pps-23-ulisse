db7f2db7380c77c17f151626014d3c7bd91ebabf
Federico Bravetti <federicobravetti802@gmail.com>
2025-03-05 22:43:08 +0100

    refactor(StationEnvironmentElement): refactor file structure

--- src/main/scala/ulisse/entities/simulation/environments/EnvironmentElements.scala
+++ src/main/scala/ulisse/entities/simulation/environments/EnvironmentElements.scala
@@ -12,7 +12,6 @@ object EnvironmentElements:
   /** A data structure that contains trains */
   trait TrainAgentsContainer[TAC <: TrainAgentsContainer[TAC]]:
     self: TAC =>
-
     /** The id of the container */
     def id: Int
 
