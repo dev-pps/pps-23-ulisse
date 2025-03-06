0b90b78842623f9db6e399e5a52cd8e65cfb2284
Manuel Buizo <buizomanuel@gmail.com>
2025-03-06 17:15:03 +0100

    feat: connected station form at gui

--- src/main/scala/ulisse/infrastructures/view/page/forms/StationForm.scala
+++ src/main/scala/ulisse/infrastructures/view/page/forms/StationForm.scala
@@ -1,16 +1,16 @@
 package ulisse.infrastructures.view.page.forms
 
 import ulisse.adapters.input.StationEditorAdapter
+import ulisse.adapters.input.StationEditorAdapter.StationCreationInfo
 import ulisse.entities.station.Station
 import ulisse.infrastructures.view.common.Observers
 import ulisse.infrastructures.view.common.Observers.ClickObserver
 import ulisse.infrastructures.view.components.ExtendedSwing
 import ulisse.infrastructures.view.components.composed.ComposedSwing
 import ulisse.infrastructures.view.components.styles.Styles
-import ulisse.infrastructures.view.manager.FormManager
-import ulisse.infrastructures.view.map.{MapElement, MapPanel}
+import ulisse.infrastructures.view.map.MapElement
 import ulisse.infrastructures.view.page.forms.Form.BaseForm
-import ulisse.infrastructures.view.page.forms.StationForm.StationFormData
+import ulisse.infrastructures.view.page.workspaces.MapWorkspace
 
 import scala.concurrent.ExecutionContext
 import scala.swing.Swing
@@ -34,10 +34,17 @@ trait StationForm extends Form:
   val tracks: ComposedSwing.InfoTextField
 
   /** Attach the creation observer to the form. */
-  def attachCreation(observer: ClickObserver[StationFormData]): Unit
+  def attachCreation(observer: ClickObserver[StationCreationInfo]): Unit
 
   /** Attach the deletion observer to the form. */
-  def attachDeletion(observer: ClickObserver[StationFormData]): Unit
+  def attachDeletion(observer: ClickObserver[StationCreationInfo]): Unit
+
+  /** Compiles the form. */
+  def compileForm(station: Station): Unit =
+    name.text = station.name
+    xField.text = station.coordinate.x.toString
+    yField.text = station.coordinate.y.toString
+    tracks.text = station.numberOfPlatforms.toString
 
 /** Companion object of the [[StationForm]]. */
 object StationForm:
@@ -45,26 +52,29 @@ object StationForm:
   /** Creates a new instance of station form. */
   def apply(): StationForm = StationFormImpl()
 
-  /** Represents the station form data. */
-  final case class StationFormData(name: String, x: String, y: String, tracks: String)
-
   /** Represents the creation station event. */
-  final case class CreationStationEvent(adapter: StationEditorAdapter, forms: FormManager, map: MapPanel)
-      extends ClickObserver[StationFormData]:
-    override def onClick(data: StationFormData): Unit =
-      val future = adapter.onOkClick(data.name, data.x, data.y, data.tracks, Option.empty)
-      future.onComplete(_ map:
-        case Left(error) => println(error)
-        case Right(stations) =>
-          map.uploadStation(stations)
-          map.attachClickStation(StationForm.TakeStationFromMapEvent(forms.stationForm))
-          map.attachClickStation(RouteForm.TakeStationFromMapEvent(forms.routeForm))
-      )
+  final case class CreationStationEvent(adapter: StationEditorAdapter, workspace: MapWorkspace)
+      extends ClickObserver[StationCreationInfo]:
+
+    private def createStation(data: StationCreationInfo): Unit =
+      adapter addStation data onComplete (_ fold (println, _ fold (println, workspace.updateStations)))
+
+    private def updateStation(data: StationCreationInfo, oldStation: Station): Unit =
+      adapter updateStation (data, oldStation) onComplete (_ fold (println, _ fold (println, workspace.update)))
+      workspace.resetSelectedStation()
+
+    override def onClick(data: StationCreationInfo): Unit =
+      workspace.selectedStation.fold(createStation(data))(updateStation(data, _))
 
   /** Represents the deletion station event. */
-  final case class DeletionStationEvent(adapter: StationEditorAdapter, form: StationForm, map: MapPanel)
-      extends ClickObserver[StationFormData]:
-    override def onClick(data: StationFormData): Unit = ()
+  final case class DeletionStationEvent(adapter: StationEditorAdapter, workspace: MapWorkspace)
+      extends ClickObserver[StationCreationInfo]:
+
+    override def onClick(data: StationCreationInfo): Unit =
+      workspace.selectedStation.fold(println("Error not found"))(
+        adapter removeStation _ onComplete (_ fold (println, _ fold (println, workspace.update)))
+      )
+      workspace.resetSelectedStation()
 
   /** Represents the take point from map event. */
   final case class TakePointFomMapEvent(stationForm: StationForm) extends ClickObserver[MouseEvent]:
@@ -73,35 +83,34 @@ object StationForm:
       stationForm.yField.text = data.point.y.toString
 
   /** Represents the take station from map event. */
-  final case class TakeStationFromMapEvent(stationForm: StationForm) extends ClickObserver[MapElement[Station]]:
+  final case class TakeStationFromMapEvent(workspace: MapWorkspace) extends ClickObserver[MapElement[Station]]:
     override def onClick(data: MapElement[Station]): Unit =
-      stationForm.name.text = data.element.name
-      stationForm.xField.text = data.element.coordinate.x.toString
-      stationForm.yField.text = data.element.coordinate.y.toString
-      stationForm.tracks.text = data.element.numberOfPlatforms.toString
+      workspace.selectedStation = Option(data.element)
+      workspace.compileStationForm(data.element)
 
+  @SuppressWarnings(Array("org.wartremover.warts.Var"))
   private case class StationFormImpl() extends StationForm:
-    override val name: ComposedSwing.InfoTextField   = ComposedSwing.createInfoTextField("Name")
-    override val xField: ComposedSwing.InfoTextField = ComposedSwing.createInfoTextField("x")
-    override val yField: ComposedSwing.InfoTextField = ComposedSwing.createInfoTextField("y")
-    override val tracks: ComposedSwing.InfoTextField = ComposedSwing.createInfoTextField("Tracks")
+    override val name: ComposedSwing.InfoTextField   = ComposedSwing createInfoTextField "Name"
+    override val xField: ComposedSwing.InfoTextField = ComposedSwing createInfoTextField "x"
+    override val yField: ComposedSwing.InfoTextField = ComposedSwing createInfoTextField "y"
+    override val tracks: ComposedSwing.InfoTextField = ComposedSwing createInfoTextField "Tracks"
 
-    private val saveButton   = ExtendedSwing.createFormButtonWith("Save", Styles.formTrueButtonRect)
-    private val deleteButton = ExtendedSwing.createFormButtonWith("Delete", Styles.formFalseButtonRect)
+    private val saveButton   = ExtendedSwing createFormButtonWith ("Save", Styles.formTrueButtonRect)
+    private val deleteButton = ExtendedSwing createFormButtonWith ("Delete", Styles.formFalseButtonRect)
 
     private val form = BaseForm("Station", name, xField, yField, tracks)
 
-    private val creationObservable = Observers.createObservable[StationFormData]
-    private val deletionObservable = Observers.createObservable[StationFormData]
+    private val creationObservable = Observers.createObservable[StationCreationInfo]
+    private val deletionObservable = Observers.createObservable[StationCreationInfo]
 
     buttonPanel.contents += saveButton
     buttonPanel.contents += deleteButton
 
-    saveButton.attach(creationObservable.toObserver(_ =>
-      StationFormData(name.text, xField.text, yField.text, tracks.text)
+    saveButton.attach(creationObservable toObserver (_ =>
+      StationCreationInfo(name.text, xField.text, yField.text, tracks.text)
     ))
-    deleteButton.attach(deletionObservable.toObserver(_ =>
-      StationFormData(name.text, xField.text, yField.text, tracks.text)
+    deleteButton.attach(deletionObservable toObserver (_ =>
+      StationCreationInfo(name.text, xField.text, yField.text, tracks.text)
     ))
 
     export form._, creationObservable.attachClick as attachCreation, deletionObservable.attachClick as attachDeletion
--- src/main/scala/ulisse/infrastructures/view/page/workspaces/MapWorkspace.scala
+++ src/main/scala/ulisse/infrastructures/view/page/workspaces/MapWorkspace.scala
@@ -1,6 +1,9 @@
 package ulisse.infrastructures.view.page.workspaces
 
 import ulisse.adapters.InputAdapterManager
+import ulisse.applications.ports.StationPorts
+import ulisse.entities.route.Routes.Route
+import ulisse.entities.station.Station
 import ulisse.infrastructures.view.manager.FormManager
 import ulisse.infrastructures.view.map.MapPanel
 import ulisse.infrastructures.view.page.forms.StationForm.{CreationStationEvent, DeletionStationEvent}
@@ -11,14 +14,28 @@ import scala.swing.BorderPanel.Position
 
 /** Represents the map workspace of the application. */
 trait MapWorkspace extends Workspace:
-  /** The map panel of the workspace. */
-  val mapPanel: MapPanel
+  /** The selected station of the form. */
+  def selectedStation: Option[Station]
 
-  /** The station form of the workspace. */
-  val stationForm: StationForm
+  /** Set the selected station of the form. */
+  def selectedStation_=(station: Option[Station]): Unit
 
-  /** The route form of the workspace. */
-  val routeForm: RouteForm
+  /** Reset selected station. */
+  def resetSelectedStation(): Unit = selectedStation = Option.empty
+
+  /** Compile station form. */
+  def compileStationForm(station: Station): Unit
+
+  /** Updates the stations of the map. */
+  def updateStations(stations: StationPorts.Input#SM): Unit
+
+  /** Updates the routes of the map. */
+  def updateRoutes(route: List[Route]): Unit
+
+  /** Updates the stations and routes of the map. */
+  def update(stations: StationPorts.Input#SM, routes: List[Route]): Unit =
+    updateStations(stations)
+    updateRoutes(routes)
 
 /** Companion object of the [[MapWorkspace]]. */
 object MapWorkspace:
@@ -28,21 +45,35 @@ object MapWorkspace:
     MapWorkspaceImpl(adapterManager)
 
   /** Represents the map workspace of the application. */
+  @SuppressWarnings(Array("org.wartremover.warts.Var"))
   private final case class MapWorkspaceImpl(adapterManager: InputAdapterManager) extends MapWorkspace:
     private val workspace   = BaseWorkspace()
     private val formManager = FormManager.createMap()
 
-    override val mapPanel: MapPanel       = MapPanel()
-    override val stationForm: StationForm = formManager.stationForm
-    override val routeForm: RouteForm     = formManager.routeForm
+    private val mapPanel: MapPanel       = MapPanel()
+    private val stationForm: StationForm = formManager.stationForm
+    private val routeForm: RouteForm     = formManager.routeForm
+
+    private var _selectedStation: Option[Station] = Option.empty
 
     workspace.workPanel.layout(mapPanel) = Position.Center
     workspace.menuPanel.layout(formManager.component) = Position.East
     workspace.revalidate()
 
-    formManager.stationForm.attachCreation(CreationStationEvent(adapterManager.stationAdapter, formManager, mapPanel))
-    formManager.stationForm.attachDeletion(DeletionStationEvent(adapterManager.stationAdapter, stationForm, mapPanel))
+    formManager.stationForm.attachCreation(CreationStationEvent(adapterManager.station, this))
+    formManager.stationForm.attachDeletion(DeletionStationEvent(adapterManager.station, this))
 
     mapPanel.attachClick(StationForm.TakePointFomMapEvent(stationForm))
 
-    export workspace.{component, revalidate}
+    export workspace.{component, revalidate}, stationForm.compileForm as compileStationForm
+
+    override def selectedStation: Option[Station] = _selectedStation
+
+    override def selectedStation_=(station: Option[Station]): Unit = _selectedStation = station
+
+    override def updateStations(stations: StationPorts.Input#SM): Unit =
+      mapPanel.uploadStation(stations)
+      mapPanel.attachClickStation(StationForm.TakeStationFromMapEvent(this))
+      mapPanel.attachClickStation(RouteForm.TakeStationFromMapEvent(routeForm))
+
+    override def updateRoutes(route: List[Route]): Unit = ()
