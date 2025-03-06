57b0746ae5a900864efe7570777a3852639c7408
Manuel Buizo <buizomanuel@gmail.com>
2025-03-06 17:15:03 +0100

    feat: connected station form at gui

--- src/main/scala/ulisse/adapters/InputAdapterManager.scala
+++ src/main/scala/ulisse/adapters/InputAdapterManager.scala
@@ -1,11 +1,15 @@
 package ulisse.adapters
 
-import ulisse.adapters.input.StationEditorAdapter
+import ulisse.adapters.input.{RouteAdapter, StationEditorAdapter}
 import ulisse.applications.InputPortManager
 
 /** Represents the input adapter manager of the application. */
 trait InputAdapterManager:
-  val stationAdapter: StationEditorAdapter
+  /** The station editor adapter. */
+  val station: StationEditorAdapter
+
+  /** The route adapter. */
+  val route: RouteAdapter
 
 /** Companion object of the input adapter manager. */
 object InputAdapterManager:
@@ -13,5 +17,7 @@ object InputAdapterManager:
   /** Creates a new instance of the input adapter manager. */
   def apply(ports: InputPortManager): InputAdapterManager = new InputAdapterManagerImpl(ports)
 
-  private final case class InputAdapterManagerImpl(stationAdapter: StationEditorAdapter) extends InputAdapterManager:
-    def this(ports: InputPortManager) = this(StationEditorAdapter(ports.station))
+  private final case class InputAdapterManagerImpl(station: StationEditorAdapter, route: RouteAdapter)
+      extends InputAdapterManager:
+    def this(ports: InputPortManager) =
+      this(StationEditorAdapter(ports.station), RouteAdapter(ports.route))
