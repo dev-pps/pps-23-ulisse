7a23cf406d369ccb25c5baa4fa91928b9b6246d5
Federico Bravetti <federicobravetti802@gmail.com>
2025-03-06 22:47:19 +0100

    build(Scaladoc): test deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,49 +1,53 @@
 import {defineConfig} from 'vitepress'
+import { withMermaid } from "vitepress-plugin-mermaid";
 
 let reportPath = '/report'
 let processPath = '/process'
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-    base: '/pps-23-ulisse',
-    title: "PPS-23-Ulisse",
-    description: "Ulisse an Train Infrastructure Similator",
-    themeConfig: {
-        // https://vitepress.dev/reference/default-theme-config
-        nav: [
-            {text: 'Home', link: '/'},
-        ],
 
-        sidebar: [
-            {text: 'Report',
-                items: [
-                    {text: 'Intro', link: `${reportPath}/0-intro`},
-                    {text: 'Develop process', link: `${reportPath}/1-dev-process`},
-                    {text: 'Requirements', link: `${reportPath}/2-requirements`},
-                    {text: 'Architectural Design', link: `${reportPath}/3-arch-design`},
-                    {text: 'Detailed Design', link: `${reportPath}/4-details-design`},
-                    {text: 'Implementation',
-                        link: `${reportPath}/5-implementation`,
-                        items: [
-                            {text: 'Bravetti Federico', link: `${reportPath}/5-impl-bravetti`},
-                            {text: 'Montesinos Buizo Julio Manuel', link: `${reportPath}/5-impl-buizo`},
-                            {text: 'Violani Matteo', link: `${reportPath}/5-impl-violani`},
-                        ]
-                    },
-                    {text: 'Testing', link: `${reportPath}/5-testing`},
-                ]
-            },
-            {text: 'Development process artifacts',
-                items: [
-                    {text: 'Sprint 1', link: `${processPath}/sprint-1`},
-                    {text: 'Sprint 2', link: `${processPath}/sprint-2`},
-                    {text: 'Sprint 3', link: `${processPath}/sprint-3`},
-                    {text: 'Sprint 4', link: `${processPath}/sprint-4`},
-                ]
-            },
-        ],
+export default withMermaid(
+    defineConfig({
+        base: '/pps-23-ulisse',
+        title: "PPS-23-Ulisse",
+        description: "Ulisse an Train Infrastructure Similator",
+        themeConfig: {
+            // https://vitepress.dev/reference/default-theme-config
+            nav: [
+                {text: 'Home', link: '/'},
+            ],
 
-        socialLinks: [
-            {icon: 'github', link: 'https://github.com/dev-pps/pps-23-ulisse'}
-        ]
-    }
-})
+            sidebar: [
+                {text: 'Report',
+                    items: [
+                        {text: 'Intro', link: `${reportPath}/0-intro`},
+                        {text: 'Develop process', link: `${reportPath}/1-dev-process`},
+                        {text: 'Requirements', link: `${reportPath}/2-requirements`},
+                        {text: 'Architectural Design', link: `${reportPath}/3-arch-design`},
+                        {text: 'Detailed Design', link: `${reportPath}/4-details-design`},
+                        {text: 'Implementation',
+                            link: `${reportPath}/5-implementation`,
+                            items: [
+                                {text: 'Bravetti Federico', link: `${reportPath}/5-impl-bravetti`},
+                                {text: 'Montesinos Buizo Julio Manuel', link: `${reportPath}/5-impl-buizo`},
+                                {text: 'Violani Matteo', link: `${reportPath}/5-impl-violani`},
+                            ]
+                        },
+                        {text: 'Testing', link: `${reportPath}/5-testing`},
+                    ]
+                },
+                {text: 'Development process artifacts',
+                    items: [
+                        {text: 'Sprint 1', link: `${processPath}/sprint-1`},
+                        {text: 'Sprint 2', link: `${processPath}/sprint-2`},
+                        {text: 'Sprint 3', link: `${processPath}/sprint-3`},
+                        {text: 'Sprint 4', link: `${processPath}/sprint-4`},
+                    ]
+                },
+            ],
+
+            socialLinks: [
+                {icon: 'github', link: 'https://github.com/dev-pps/pps-23-ulisse'}
+            ]
+        }
+    })
+)
--- docs/report/5-impl-bravetti.md
+++ docs/report/5-impl-bravetti.md
@@ -1,1 +1,13 @@
+# Example of a Class Diagram
 
+```mermaid
+classDiagram
+    class Animal {
+        +String name
+        +int age
+        +eat()
+    }
+    class Dog {
+        +bark()
+    }
+    Animal <|-- Dog
