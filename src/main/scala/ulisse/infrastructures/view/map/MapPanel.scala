b3ce114a3ddb789223000c98c1ec12db69b8ad92
Manuel Buizo <buizomanuel@gmail.com>
2025-03-07 03:56:10 +0100

    feat: connect click route at form

--- src/main/scala/ulisse/infrastructures/view/map/MapPanel.scala
+++ src/main/scala/ulisse/infrastructures/view/map/MapPanel.scala
@@ -36,8 +36,8 @@ object MapPanel:
     private val stations = MapElements[Station](observable)
     private val routes   = MapElements[Route](observable)
 
-    private val test = DrawImageTiled.createAt("route.png", new Point(400, 200), new Point(600, 500))
-    observable.attachMove(test)
+//    private val test = DrawImageTiled.createAt("route.png", new Point(400, 200), new Point(600, 500))
+//    observable.attachMove(test)
 
     def attachClickStation(event: ClickObserver[MapElement[Station]]): Unit = stations attachClick event
 
@@ -55,7 +55,7 @@ object MapPanel:
       routes update (newRoutes map (MapElement createRoute (_, ImagePath.route)))
 
     override protected def paintLook(g: Graphics2D): Unit =
-      test draw (g, peer)
+//      test draw (g, peer)
       routes draw (g, peer)
       stations draw (g, peer)
       super.paintLook(g)
