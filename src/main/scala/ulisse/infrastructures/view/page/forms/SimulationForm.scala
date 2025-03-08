213d8f385d03ebb9ad6c9a13334374ed6034c0f5
Manuel Buizo <buizomanuel@gmail.com>
2025-03-08 12:06:35 +0100

    feat: create listener to start simulation

--- src/main/scala/ulisse/infrastructures/view/page/forms/SimulationForm.scala
+++ src/main/scala/ulisse/infrastructures/view/page/forms/SimulationForm.scala
@@ -1,37 +1,64 @@
 package ulisse.infrastructures.view.page.forms
 
+import ulisse.adapters.input.SimulationPageAdapter
+import ulisse.infrastructures.view.common.Observers
+import ulisse.infrastructures.view.common.Observers.ClickObserver
 import ulisse.infrastructures.view.components.ExtendedSwing
 import ulisse.infrastructures.view.components.ExtendedSwing.SBorderPanel
 import ulisse.infrastructures.view.components.composed.ComposedSwing
 import ulisse.infrastructures.view.components.styles.Styles
 import ulisse.infrastructures.view.page.forms.Form.BaseForm
+import ulisse.infrastructures.view.page.forms.SimulationForm.SimulationInfo
 
 import scala.swing.BorderPanel.Position
 import scala.swing.Component
 
 /** Represents the simulation form of the application. */
-trait SimulationForm extends Form
+trait SimulationForm extends Form:
+
+  /** Attach the start simulation observer to the form of type [[SimulationInfo]]. */
+  def attachStartSimulation(observer: ClickObserver[SimulationInfo]): Unit
+
+  /** Attach the reset simulation observer to the form of type [[Unit]]. */
+  def attachResetSimulation(observer: ClickObserver[Unit]): Unit
 
 /** Companion object of the [[SimulationForm]]. */
 object SimulationForm:
   /** Creates a new instance of simulation form. */
   def apply(): SimulationForm = SimulationFormImpl()
 
+  /** Represents the simulation form data. */
+  final case class SimulationInfo(step: String, cyclePerSecond: String)
+
+  /** Represents the start simulation event. */
+  private final case class PlaySimulationEvent(adapter: SimulationPageAdapter) extends ClickObserver[SimulationInfo]:
+    override def onClick(info: SimulationInfo): Unit = ()
+
   private case class SimulationFormImpl() extends SimulationForm:
     private val mainPanel: SBorderPanel                     = SBorderPanel()
     private val stepSize: ComposedSwing.InfoTextField       = ComposedSwing createInfoTextField "Step"
     private val cyclePerSecond: ComposedSwing.InfoTextField = ComposedSwing createInfoTextField "Cycle"
-
     private val playButton  = ExtendedSwing createFormButtonWith ("Play", Styles.formButtonRect)
     private val resetButton = ExtendedSwing createFormButtonWith ("Delete", Styles.formButtonRect)
+    private val form        = BaseForm("Simulation", stepSize, cyclePerSecond)
 
-    private val form = BaseForm("Simulation", stepSize, cyclePerSecond)
     buttonPanel.contents += playButton
     buttonPanel.contents += resetButton
 
     mainPanel.layout(form.component) = Position.South
-    mainPanel.updateGraphics()
+
+    private val playObservable  = Observers.createObservable[SimulationInfo]
+    private val resetObservable = Observers.createObservable[Unit]
+
+    playButton attachClick (playObservable toObserver (_ => SimulationInfo(stepSize.text, cyclePerSecond.text)))
+    resetButton attachClick (resetObservable toObserver (_ => ()))
 
     export form.{component => _, _}
+
+    override def attachStartSimulation(observer: ClickObserver[SimulationInfo]): Unit =
+      playObservable attachClick observer
+
+    override def attachResetSimulation(observer: ClickObserver[Unit]): Unit =
+      resetObservable attachClick observer
 
     override def component[T >: Component]: T = mainPanel
