a1025e6aa50bc195c36f21e3a484069d6492f306
Manuel Buizo <buizomanuel@gmail.com>
2025-03-09 02:08:03 +0100

    feat: create station dsl

--- src/test/scala/ulisse/RailwayDslTest.scala
+++ src/test/scala/ulisse/RailwayDslTest.scala
@@ -1,13 +1,18 @@
 package ulisse
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers.mustBe
 import org.scalatest.matchers.should.Matchers
 import ulisse.applications.AppState
-import ulisse.dsl.RailwayDsl.*
+import ulisse.dsl.RailwayDsl.CreateStation._
+import ulisse.dsl.RailwayDsl.CreateStation
+import ulisse.entities.Coordinate
+import ulisse.entities.station.Station
 
 class RailwayDslTest extends AnyFlatSpec with Matchers:
   private val appState = AppState()
 
-//  "create station" should "push a station in the app state" in:
-//    appState
-//
+  "create station with dsl" should "create a station" in:
+    val stationTest = Station("test", Coordinate(0, 0), 1)
+    val station     = CreateStation -> "test" at (0, 0) platform 1
+    stationTest mustBe station
