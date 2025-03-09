a13cfef08a0d28fa1b36774680a9ec40fb16f513
Manuel Buizo <buizomanuel@gmail.com>
2025-03-09 02:08:03 +0100

    feat: create station dsl

--- src/main/scala/ulisse/dsl/RailwayDsl.scala
+++ src/main/scala/ulisse/dsl/RailwayDsl.scala
@@ -1,11 +1,22 @@
 package ulisse.dsl
 
-import ulisse.applications.AppState
+import ulisse.entities.Coordinate
+import ulisse.entities.station.Station
 
-object RailwayDsl
+/** DSL for creating railway entities. */
+object RailwayDsl:
 
-//  type StationDsl = (String, )
-//
-//  extension (appState: AppState)
-//
-//    def create
+  /** Create a station. */
+  object CreateStation:
+
+    /** Create a station with a name. */
+    case class StationBuilder(name: String):
+      def at(coord: (Int, Int)): StationWithCoord = StationWithCoord(name, Coordinate(coord._1, coord._2))
+
+    /** Create a station with a name and a coordinate. */
+    case class StationWithCoord(name: String, coordinate: Coordinate):
+      def platform(capacity: Int): Station = Station(name, coordinate, capacity)
+
+    /** Create a station with a name and a coordinate. */
+    implicit class StationOps(start: CreateStation.type):
+      def ->(name: String): StationBuilder = StationBuilder(name)
