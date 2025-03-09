31175a1f6f9b058febabb0acb8473ba9768e743b
Manuel Buizo <buizomanuel@gmail.com>
2025-03-09 02:55:39 +0100

    feat: create route  dsl

--- src/main/scala/ulisse/dsl/RailwayDsl.scala
+++ src/main/scala/ulisse/dsl/RailwayDsl.scala
@@ -1,10 +1,16 @@
 package ulisse.dsl
 
 import ulisse.entities.Coordinate
+import ulisse.entities.route.Routes
+import ulisse.entities.route.Routes.Route
 import ulisse.entities.station.Station
+import ulisse.entities.train.Trains.{Train, TrainTechnology}
+import ulisse.entities.train.Wagons
 
 /** DSL for creating railway entities. */
 object RailwayDsl:
+
+  export CreateStation._, CreateTrain._, CreateRoute._
 
   /** Create a station. */
   object CreateStation:
@@ -15,8 +21,51 @@ object RailwayDsl:
 
     /** Create a station with a name and a coordinate. */
     case class StationWithCoord(name: String, coordinate: Coordinate):
-      def platform(capacity: Int): Station = Station(name, coordinate, capacity)
+      def platforms(capacity: Int): Station = Station(name, coordinate, capacity)
 
     /** Create a station with a name and a coordinate. */
     implicit class StationOps(start: CreateStation.type):
       def ->(name: String): StationBuilder = StationBuilder(name)
+
+  /** Create a train. */
+  object CreateTrain:
+
+    /** Create a train with a name. */
+    case class TrainBuilder(name: String):
+      def technology(technology: TrainTechnology): TrainWithTechnology = TrainWithTechnology(name, technology)
+
+    /** Create a train with a name and a technology. */
+    case class TrainWithTechnology(name: String, technology: TrainTechnology):
+      def wagon(wagon: Wagons.UseType): TrainWithWagon = TrainWithWagon(name, technology, wagon)
+
+    /** Create a train with a name, a technology, and a wagon. */
+    case class TrainWithWagon(name: String, technology: TrainTechnology, wagon: Wagons.UseType):
+      def numbers(number: Int): Train = Train(name, technology, Wagons.Wagon(wagon, number), number)
+
+    /** Create a train with a name, a technology, a wagon, and a number. */
+    implicit class TrainOps(start: CreateTrain.type):
+      def ->(name: String): TrainBuilder = TrainBuilder(name)
+
+  /** Create a route. */
+  object CreateRoute:
+
+    /** Create a route with a departure station. */
+    case class RouteBuilder(departure: Station):
+      def ->(arrival: Station): RouteWithArrival = RouteWithArrival(departure, arrival)
+
+    /** Create a route with a departure and an arrival station. */
+    case class RouteWithArrival(departure: Station, arrival: Station):
+      def on(routeType: Routes.RouteType): RouteWithType = RouteWithType(departure, arrival, routeType)
+
+    /** Create a route with a departure, an arrival station, and a route type. */
+    case class RouteWithType(departure: Station, arrival: Station, routeType: Routes.RouteType):
+      def tracks(platform: Int): RouteWithPlatform = RouteWithPlatform(departure, arrival, routeType, platform)
+
+    /** Create a route with a departure, an arrival station, a route type, and a platform. */
+    case class RouteWithPlatform(departure: Station, arrival: Station, routeType: Routes.RouteType, platform: Int):
+      def length(length: Double): Either[Routes.RouteError, Route] =
+        Route(departure, arrival, routeType, platform, length)
+
+    /** Create a route with a departure, an arrival station, a route type, a platform, and a length. */
+    implicit class RouteOps(start: CreateRoute.type):
+      def ->(departure: Station): RouteBuilder = RouteBuilder(departure)
--- src/test/scala/ulisse/RailwayDslTest.scala
+++ src/test/scala/ulisse/RailwayDslTest.scala
@@ -4,15 +4,30 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers.mustBe
 import org.scalatest.matchers.should.Matchers
 import ulisse.applications.AppState
-import ulisse.dsl.RailwayDsl.CreateStation._
-import ulisse.dsl.RailwayDsl.CreateStation
+import ulisse.dsl.RailwayDsl.*
 import ulisse.entities.Coordinate
+import ulisse.entities.route.Routes
+import ulisse.entities.route.Routes.{Route, RouteType}
 import ulisse.entities.station.Station
+import ulisse.entities.train.Trains.{Train, TrainTechnology}
+import ulisse.entities.train.Wagons.{UseType, Wagon}
 
 class RailwayDslTest extends AnyFlatSpec with Matchers:
-  private val appState = AppState()
+  private val appState  = AppState()
+  private val highSpeed = TrainTechnology("HighSpeed", 300, 1.0, 0.5)
+  private val departure = Station("departure", Coordinate(0, 0), 1)
+  private val arrival   = Station("arrival", Coordinate(100, 100), 1)
+  private val trainTest = Train("test", highSpeed, Wagon(UseType.Passenger, 1), 1)
+  private val routeTest = Route(departure, arrival, Routes.RouteType.Normal, 1, 100.0)
 
   "create station with dsl" should "create a station" in:
-    val stationTest = Station("test", Coordinate(0, 0), 1)
-    val station     = CreateStation -> "test" at (0, 0) platform 1
-    stationTest mustBe station
+    val station = CreateStation -> "departure" at (0, 0) platforms 1
+    departure mustBe station
+
+  "create train with dsl" should "create a train" in:
+    val train = CreateTrain -> "test" technology highSpeed wagon UseType.Passenger numbers 1
+    trainTest mustBe train
+
+  "create route with dsl" should "create a route" in:
+    val route = CreateRoute -> departure -> arrival on RouteType.Normal tracks 1 length 100.0
+    route mustBe routeTest
