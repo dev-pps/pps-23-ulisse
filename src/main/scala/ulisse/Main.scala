14892f1a2135a9dd4e3ab8f2c3292b4fb673b873
Manuel Buizo <buizomanuel@gmail.com>
2025-03-09 22:51:29 +0100

    feat: set default railway

--- src/main/scala/ulisse/Main.scala
+++ src/main/scala/ulisse/Main.scala
@@ -6,6 +6,7 @@ import ulisse.adapters.output.{SimulationNotificationAdapter, SimulationNotifica
 import ulisse.applications.ports.{SimulationInfoPorts, SimulationPorts}
 import ulisse.applications.useCases.{SimulationInfoService, SimulationService}
 import ulisse.applications.{AppState, EventQueue, InputPortManager}
+import ulisse.dsl.ConfigRailway
 import ulisse.infrastructures.utilty.SimulationNotificationBridge
 import ulisse.infrastructures.view.GUI
 import ulisse.infrastructures.view.page.workspaces.SimulationWorkspace
@@ -35,70 +36,4 @@ object Main:
 
     val gui = GUI(inputAdapterManager, simulationSetup.workspace)
 
-//    // TRAIN
-//    val defaultTechnology     = TrainTechnology("HighSpeed", 300, 1.0, 0.5)
-//    val normalTrainTechnology = TrainTechnology("Normal", 100, 0.5, 0.25)
-//    val technologyManager     = TechnologyManager(List(defaultTechnology, normalTrainTechnology))
-//    val defaultWagon          = Wagon(UseType.Passenger, 5)
-//    val defaultWagonNumber    = 1
-//    val train3905             = Train("3905", normalTrainTechnology, defaultWagon, defaultWagonNumber)
-//    val train3906             = Train("3906", normalTrainTechnology, defaultWagon, defaultWagonNumber)
-//
-//    // STATION
-//    val stationA = Station("A", Coordinate(409, 188), 1)
-//    val stationB = Station("B", Coordinate(758, 377), 1)
-//    val stationC = Station("C", Coordinate(505, 672), 1)
-//
-//    // ROUTE
-//    val routeA = Route(stationA, stationB, RouteType.Normal, 1, 450).toOption
-//    val routeB = Route(stationB, stationC, RouteType.Normal, 1, 450).toOption
-//
-//    // RAIL
-//    val routeTEOa: RailInfo = RailInfo(length = 450, typeRoute = RouteType.Normal)
-//    val routeTEOb: RailInfo = RailInfo(length = 450, typeRoute = RouteType.Normal)
-//
-//    // TIME-TABLE
-//    val timetable1: Timetable =
-//      TimetableBuilder(
-//        train = train3906,
-//        startStation = stationC,
-//        departureTime = h(8).m(0).getOrDefault
-//      ).transitIn(stationB)(routeTEOa)
-//        .arrivesTo(stationA)(routeTEOb)
-//
-//    // TIME-TABLE
-//    val timetable2: Timetable =
-//      TimetableBuilder(
-//        train = train3905,
-//        startStation = stationA,
-//        departureTime = h(8).m(0).getOrDefault
-//      ).stopsIn(stationB, waitTime = 5)(routeTEOa)
-//        .arrivesTo(stationC)(routeTEOb)
-//
-//    val initialState = AppState.withTechnology(technologyManager)
-//
-//    val newState1 = initialState.updateStationManager(manager =>
-//      manager.addStation(stationA).flatMap(_.addStation(stationB).flatMap(_.addStation(stationC))).getOrElse(manager)
-//    )
-//
-//    val newState2 = newState1.updateRoute(manager =>
-//      // RICORDA FOLD LEFT
-//      val routes = (routeA, routeB)
-//      routes match
-//        case (Some(a), Some(b)) => manager.save(a).flatMap(_.save(b)).getOrElse(manager)
-//        case _                  => manager
-//    )
-//
-//    val newState3 = newState2.updateTrain((trainManager, technologyManager) =>
-//      trainManager.addTrain(train3905).getOrElse(trainManager).addTrain(train3906).getOrElse(trainManager)
-//    )
-//
-//    val newState4 = newState3.updateTimetable(manager =>
-//      val e = manager.save(timetable2)
-//      println(e)
-//      val r = e.getOrElse(manager).save(timetable1)
-//      println(r)
-//      r.getOrElse(manager)
-//    )
-
-    eventQueue.startProcessing(AppState())
+    eventQueue.startProcessing(ConfigRailway.simpleRailwayConfig)
