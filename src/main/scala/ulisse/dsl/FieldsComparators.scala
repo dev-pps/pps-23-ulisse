c8bca04cb1824a3cbdbf3ec4fe5261f2a90aec8f
Federico Bravetti <federicobravetti802@gmail.com>
2025-03-10 04:09:59 +0100

    docs(Implementation): ComparisonDSL

--- src/main/scala/ulisse/dsl/FieldsComparators.scala
+++ src/main/scala/ulisse/dsl/FieldsComparators.scala
@@ -3,19 +3,19 @@ package ulisse.dsl
 /** DSL for comparing fields of objects. */
 object FieldsComparators:
   /** Field of an object. */
-  trait Field[T <: Field[T, O], O <: Any]:
-    self: T =>
+  trait Field[F <: Field[F, O], O <: Any]:
+    self: F =>
 
     /** Seq of all the field of the object. */
-    def values: Seq[T]
+    def values: Seq[F]
 
   /** Comparator for fields of objects. */
-  trait FieldComparator[T <: Field[T, O], O <: Any]:
+  trait FieldComparator[F <: Field[F, O], O <: Any]:
     /** Seq of all the field of the object. */
-    def fields: Seq[T]
+    def fields: Seq[F]
 
     /** Compare the fields of the objects. */
-    final def compare(objects: List[O], ignoredFields: Seq[T]): Boolean =
+    final def compare(objects: List[O], ignoredFields: Seq[F]): Boolean =
       val fieldsToCompare = fields.filterNot(ignoredFields.contains)
       objects match
         case firstObject :: tail => tail.forall: otherObject =>
@@ -23,34 +23,34 @@ object FieldsComparators:
         case _ => false
 
     /** Compare the given field for the two objects. */
-    protected def _compare(obj: O, otherObj: O, field: T): Boolean
+    protected def _compare(obj: O, otherObj: O, field: F): Boolean
 
   /** Conversion from a comparison builder to a boolean. */
-  given [T <: Field[T, O], O <: Any]: Conversion[ComparisonBuilder[T, O], Boolean] with
-    def apply(builder: ComparisonBuilder[T, O]): Boolean =
+  given [F <: Field[F, O], O <: Any]: Conversion[ComparisonBuilder[F, O], Boolean] with
+    def apply(builder: ComparisonBuilder[F, O]): Boolean =
       builder.compare
 
   /** Builder for structure comparison. */
-  case class ComparisonBuilder[T <: Field[T, O], O <: Any](objects: List[O], ignoredFields: Seq[T])(using
-      fieldComparator: FieldComparator[T, O]
+  case class ComparisonBuilder[F <: Field[F, O], O <: Any](objects: List[O], ignoredFields: Seq[F])(using
+      fieldComparator: FieldComparator[F, O]
   ):
     /** Ignore the given fields. */
-    def ignoring(field: T, fields: T*): ComparisonBuilder[T, O] =
+    def ignoring(field: F, fields: F*): ComparisonBuilder[F, O] =
       copy(ignoredFields = ignoredFields ++ (fields :+ field))
 
     /** Consider only the given fields. */
-    def considering(field: T, fields: T*): ComparisonBuilder[T, O] =
+    def considering(field: F, fields: F*): ComparisonBuilder[F, O] =
       copy(ignoredFields = field.values.filterNot((fields :+ field).contains).toIndexedSeq)
 
     /** Add the next object to compare. */
-    def andTo(nextObj: O): ComparisonBuilder[T, O] =
+    def andTo(nextObj: O): ComparisonBuilder[F, O] =
       copy(objects = nextObj +: objects)
 
     /** Compare the objects. */
     def compare: Boolean =
       fieldComparator.compare(objects, ignoredFields)
 
-  extension [T <: Field[T, O], O <: Any](obj: O)
+  extension [F <: Field[F, O], O <: Any](obj: O)
     /** Extension for comparing two objects. */
-    def compareTo(otherObj: O)(using fieldComparator: FieldComparator[T, O]): ComparisonBuilder[T, O] =
-      ComparisonBuilder(List(obj, otherObj), Seq[T]())
+    def compareTo(otherObj: O)(using fieldComparator: FieldComparator[F, O]): ComparisonBuilder[F, O] =
+      ComparisonBuilder(List(obj, otherObj), Seq[F]())
