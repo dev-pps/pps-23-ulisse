472ad7f92ff0d4fdc6ca74f45558d155905893b3
Federico Bravetti <federicobravetti802@gmail.com>
2025-04-02 00:15:07 +0200

    docs(Titles): adjust names

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -22,9 +22,9 @@ export default withMermaid(
                         {text: 'Introduzione', link: `${reportPath}/0-intro`},
                         {text: 'Processo di sviluppo', link: `${reportPath}/1-dev-process`},
                         {text: 'Requisiti', link: `${reportPath}/2-requirement`},
-                        {text: 'Design Architetturale', link: `${reportPath}/3-arch-design`},
-                        {text: 'Detailed Design', link: `${reportPath}/4-details-design`},
-                        {text: 'Implementation',
+                        {text: 'Design architetturale', link: `${reportPath}/3-arch-design`},
+                        {text: 'Design di dettaglio', link: `${reportPath}/4-details-design`},
+                        {text: 'Implementazione',
                             items: [
                                 {text: 'Bravetti Federico', link: `${reportPath}/5-impl-bravetti`},
                                 {text: 'Montesinos Buizo Julio Manuel', link: `${reportPath}/5-impl-buizo`},
@@ -35,7 +35,7 @@ export default withMermaid(
                         {text: 'Retrospettiva', link: `${reportPath}/7-retrospective`},
                     ]
                 },
-                {text: 'Development process artifacts',
+                {text: 'Artefatti del processo di sviluppo',
                     items: [
                         {text: 'Sprint 1', link: `${processPath}/sprint-1`},
                         {text: 'Sprint 2', link: `${processPath}/sprint-2`},
