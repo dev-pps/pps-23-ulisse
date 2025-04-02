07db68bdeacb95ef225cc010b2cb47da25ef2cb3
Federico Bravetti <federicobravetti802@gmail.com>
2025-04-02 17:42:06 +0200

    chore(Example): add rail preferences example

--- src/main/scala/ulisse/applications/configs/RailwayExamplesConfig.scala
+++ src/main/scala/ulisse/applications/configs/RailwayExamplesConfig.scala
@@ -22,29 +22,43 @@ object RailwayExamplesConfig extends RailwayConfig:
   private val railN400                 = RailInfo(length = 400, typeRoute = RouteType.Normal)
   private val railAV400                = RailInfo(length = 400, typeRoute = RouteType.AV)
 
-  private val stationA  = CreateStation -> "A" at (300, 200) platforms 2
-  private val stationB  = CreateStation -> "B" at (300, 500) platforms 2
+  private val stationA  = CreateStation -> "A" at (400, 50) platforms 2
+  private val stationB  = CreateStation -> "B" at (400, 350) platforms 2
   private val trainA_AV = CreateTrain   -> "AV1" technology highSpeed wagon UseType.Passenger capacity 2 count 1
   private val trainB_NR = CreateTrain   -> "NR1" technology normal wagon UseType.Passenger capacity 2 count 1
   private val routeAB   = CreateRoute   -> stationA -> stationB on railN400.typeRoute tracks 2 length railN400.length
 
-  private val stationC  = CreateStation -> "C" at (500, 200) platforms 2
-  private val stationD  = CreateStation -> "D" at (500, 500) platforms 2
+  private val stationC  = CreateStation -> "C" at (600, 50) platforms 2
+  private val stationD  = CreateStation -> "D" at (600, 350) platforms 2
   private val trainC_AV = CreateTrain   -> "AV2" technology highSpeed wagon UseType.Passenger capacity 2 count 1
   private val trainD_NR = CreateTrain   -> "NR2" technology normal wagon UseType.Passenger capacity 2 count 1
   private val routeCD   = CreateRoute   -> stationC -> stationD on railN400.typeRoute tracks 1 length railN400.length
 
-  private val stationE  = CreateStation -> "E" at (700, 200) platforms 1
-  private val stationF  = CreateStation -> "F" at (700, 500) platforms 1
+  private val stationE  = CreateStation -> "E" at (800, 50) platforms 1
+  private val stationF  = CreateStation -> "F" at (800, 350) platforms 1
   private val trainE_AV = CreateTrain   -> "AV3" technology highSpeed wagon UseType.Passenger capacity 2 count 1
   private val trainF_NR = CreateTrain   -> "NR3" technology normal wagon UseType.Passenger capacity 2 count 1
   private val routeEF   = CreateRoute   -> stationE -> stationF on railN400.typeRoute tracks 1 length railN400.length
 
-  private val stationG  = CreateStation -> "G" at (900, 200) platforms 2
-  private val stationH  = CreateStation -> "H" at (900, 500) platforms 2
+  private val stationG  = CreateStation -> "G" at (400, 450) platforms 2
+  private val stationH  = CreateStation -> "H" at (400, 750) platforms 2
   private val trainG_AV = CreateTrain   -> "AV4" technology highSpeed wagon UseType.Passenger capacity 2 count 1
   private val trainH_NR = CreateTrain   -> "NR4" technology normal wagon UseType.Passenger capacity 2 count 1
-  private val routeGH   = CreateRoute   -> stationG -> stationH on railAV400.typeRoute tracks 2 length railAV400.length
+  private val routeGH   = CreateRoute   -> stationG -> stationH on railAV400.typeRoute tracks 1 length railAV400.length
+
+  private val stationI   = CreateStation -> "I" at (600, 450) platforms 2
+  private val stationJ   = CreateStation -> "J" at (600, 750) platforms 2
+  private val trainI_AV  = CreateTrain   -> "AVI" technology highSpeed wagon UseType.Passenger capacity 2 count 1
+  private val routeIJ_N  = CreateRoute   -> stationI -> stationJ on railN400.typeRoute tracks 1 length railN400.length
+  private val routeIJ_AV = CreateRoute   -> stationI -> stationJ on railAV400.typeRoute tracks 1 length railAV400.length
+
+  private val stationK   = CreateStation -> "K" at (800, 450) platforms 2
+  private val stationL   = CreateStation -> "L" at (800, 750) platforms 2
+  private val trainK_AV  = CreateTrain   -> "AVK" technology highSpeed wagon UseType.Passenger capacity 2 count 1
+  private val trainL_AV  = CreateTrain   -> "AVL" technology highSpeed wagon UseType.Passenger capacity 2 count 1
+  private val routeKL_N  = CreateRoute   -> stationK -> stationL on railN400.typeRoute tracks 1 length railN400.length
+  private val routeKL_AV = CreateRoute   -> stationK -> stationL on railAV400.typeRoute tracks 1 length railAV400.length
+
   import ulisse.dsl.TimetableDSL.*
 
   private val table1 = trainA_AV at h(0).m(0).getOrDefault startFrom stationA thenOnRail
@@ -67,7 +81,16 @@ object RailwayExamplesConfig extends RailwayConfig:
   private val table8 = trainH_NR at h(0).m(0).getOrDefault startFrom stationH thenOnRail
     railAV400 arrivesTo stationG
 
+  private val table9 = trainI_AV at h(0).m(0).getOrDefault startFrom stationI thenOnRail
+    railAV400 arrivesTo stationJ
+
+  private val table10 = trainK_AV at h(0).m(0).getOrDefault startFrom stationK thenOnRail
+    railN400 arrivesTo stationL
+
+  private val table11 = trainL_AV at h(0).m(0).getOrDefault startFrom stationK thenOnRail
+    railAV400 arrivesTo stationL
+
   examplesConfig =
-    CreateAppState || examplesConfig set stationA set stationB set stationC set stationD set stationE set stationF set stationG set stationH link routeAB link routeCD link routeEF link routeGH put trainA_AV put trainB_NR put trainC_AV put trainD_NR put trainE_AV put trainF_NR put trainG_AV put trainH_NR scheduleA table1 scheduleA table2 scheduleA table3 scheduleA table4 scheduleA table5 scheduleA table6 scheduleA table7 scheduleA table8
+    CreateAppState || examplesConfig set stationA set stationB set stationC set stationD set stationE set stationF set stationG set stationH set stationI set stationJ set stationK set stationL link routeAB link routeCD link routeEF link routeGH link routeIJ_N link routeIJ_AV link routeKL_N link routeKL_AV put trainA_AV put trainB_NR put trainC_AV put trainD_NR put trainE_AV put trainF_NR put trainG_AV put trainH_NR put trainI_AV put trainK_AV put trainL_AV scheduleA table1 scheduleA table2 scheduleA table3 scheduleA table4 scheduleA table5 scheduleA table6 scheduleA table7 scheduleA table8 scheduleA table9 scheduleA table10 scheduleA table11
 
   def config: AppState = examplesConfig
